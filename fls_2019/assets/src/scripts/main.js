4662f7446e9c61494047d621f819183172e7e5fe
Ron Madriz <ron@firstcreative.com>
2019-08-21 15:21:42 -0500

    SVG Click Issue - xlink - jquery solution

--- fls_2019/assets/src/scripts/main.js
+++ fls_2019/assets/src/scripts/main.js
@@ -1,5 +1,4 @@
 var j = jQuery.noConflict();
-
 j(document).ready(function(){
 	j('a').not('[href*="mailto:"]').each(function () {
 		var isInternalLink = new RegExp('/' + window.location.host + '/');
@@ -7,15 +6,10 @@ j(document).ready(function(){
 		  j(this).attr('target', '_blank');
 		};
 	});
-
 	j('#myModal').on('shown.bs.modal', function () {
 		j('#myInput').trigger('focus')
 	})	
-
-
 	!function(t){t.fn.bcSwipe=function(e){var n={threshold:50};return e&&t.extend(n,e),this.each(function(){function e(t){1==t.touches.length&&(u=t.touches[0].pageX,c=!0,this.addEventListener("touchmove",o,!1))}function o(e){if(c){var o=e.touches[0].pageX,i=u-o;Math.abs(i)>=n.threshold&&(h(),t(this).carousel(i>0?"next":"prev"))}}function h(){this.removeEventListener("touchmove",o),u=null,c=!1}var u,c=!1;"ontouchstart"in document.documentElement&&this.addEventListener("touchstart",e,!1)}),this}}(jQuery);
-	 
-	// Swipe functions for Bootstrap Carousel
 	j('.carousel').bcSwipe({ threshold: 50 });
 
 	j(window).scroll(function(){
@@ -25,8 +19,6 @@ j(document).ready(function(){
 	       j('section').removeClass('focus');
 	    }
 	});
-
-    // Gravity form fixes 
     j("form#gform_3 .medium").addClass('form-control');
     j("form#gform_2 .medium").addClass('form-control');
     j("form#gform_2 input").removeClass('medium');
@@ -34,8 +26,11 @@ j(document).ready(function(){
     j("form#gform_2 select").addClass('form-control');
     j("form#gform_2 textarea").addClass('form-control');
     j(".phone").text(function(i, text) {text = text.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3"); return text; });
+    // change SVG Target to TOP
+    j('svg a').click(function(event){
+      j('a').attr("target", "_top");
+    });
 });
-
 j(document).on('click', '#searchToggle', function(event) {
    j('form#searchform').removeClass('d-none');
    j('form#searchform').addClass('d-block');
@@ -48,14 +43,6 @@ j(document).on('click', '[data-toggle="lightbox"]', function(event) {
 	event.preventDefault();
 	j(this).ekkoLightbox();
 });
-/*
-j('.navbar .dropdown > a').click(function() {
-		if (!j(this).hasClass("parent-clicked")) {
-			j(this).addClass("parent-clicked");
-		} else {
-			location.href = this.href;
-		}
-}); */
 function normalizeSlideHeights() {
     j('.carousel').each(function(){
       var items = j('.carousel-item img.background', this);
@@ -80,39 +67,12 @@ function normalizeSlideHeights() {
       items.css('min-height', maxHeight + 'px');
     })
 }
-
 jQuery(
   function(j) {
-  if (j(window).width() > 769) {
-    j('.navbar .dropdown > a').click(function() {
-      location.href = this.href;
-    });
+    if (j(window).width() > 769) {
+      j('.navbar .dropdown > a').click(function() {
+        location.href = this.href;
+      });
+    }
   }
-}
 );
-// j(window).on('load resize orientationchange', normalizeSlideHeights);
-// Listens for screen width change to apply bootstrap dropdown menus
-// function myFunction(x) {
-//   if (x.matches) { // If media query matches
-// document.body.style.backgroundColor = "rgb(248,248,248)";
-//   } else {
-// document.body.style.backgroundColor = "rgb(238,238,238)";
-//     j('.navbar .dropdown > a').click(function() {
-//       location.href = this.href;
-//     });
-//   }
-// }
-//  var x = window.matchMedia("(max-width: 786px)")
-// myFunction(x) // Call listener function at run time
-// x.addListener(myFunction) // Attach listener function on state changes
-// Solutions Filter
-// var $btns = j('.btn_filter').click(function() {
-// if (this.id == 'all') {
-// j('#solutions_results > .item').fadeIn(450);
-//   } else {
-// var $el = j('.' + this.id).fadeIn(450);
-// j('#solutions_results > .item').not($el).hide();
-// }
-// $btns.removeClass('active');
-// j(this).addClass('active');
-// })
