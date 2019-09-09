f85365a79dcc1acfdb75e8e2a515d905d5aaf623
Ron Madriz <ron@firstcreative.com>
2019-09-09 11:41:46 -0500

    fluid layout for industries test

--- fls_2019/single-industries-fluid.php
+++ fls_2019/single-industries-fluid.php
@@ -42,6 +42,41 @@ echo '</section>'.PHP_EOL;
 endwhile;
 endif;
 
+// GALLERY
+
+$industry_gallery = get_field('industry_gallery');
+if ($industry_gallery) {
+	while (have_rows('industry_gallery')):the_row();
+	$gallery_title = get_sub_field('title');
+	$gallery_pics  = get_sub_field('gallery_pics');
+	echo '<section id="gallery">'.PHP_EOL;
+	echo '<div class="container">'.PHP_EOL;
+	echo '<div class="row">'.PHP_EOL;
+	echo '<div class="section_title col-12">'.PHP_EOL;
+	echo '<h1>'.$gallery_title.'</h1>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '<div class="row">'.PHP_EOL;
+	echo '<div class="content col-12">'.PHP_EOL;
+	if ($gallery_pics):
+	echo '<ul class="gallery list-unstyled list-inline">'.PHP_EOL;
+	foreach ($gallery_pics as $gallery_pic):
+	echo '<li class="list-inline-item col-6 col-md-3 col-xl-2">'.PHP_EOL;
+	echo '<a href="'.$gallery_pic['url'].'" data-gallery="fls-gallery" data-toggle="lightbox">'.PHP_EOL;
+	echo '<img src="'.$gallery_pic['sizes']['thumbnail'].'" alt="'.$gallery_pic['alt'].'" class="img-fluid">'.PHP_EOL;
+	echo '</a>'.PHP_EOL;
+	echo '</li>'.PHP_EOL;
+	endforeach;
+	echo '</ul>'.PHP_EOL;
+	endif;
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
+	endwhile;
+	;
+}
+
 // INDUSTRY SPECIFIC SOLUTIONS
 $solutions_group = get_field('solutions_group');
 if ($solutions_group) {
@@ -167,42 +202,6 @@ wp_reset_query();
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
 endif;
-// GALLERY
-
-// GALLERY
-
-$industry_gallery = get_field('industry_gallery');
-if ($industry_gallery) {
-	while (have_rows('industry_gallery')):the_row();
-	$gallery_title = get_sub_field('title');
-	$gallery_pics  = get_sub_field('gallery_pics');
-	echo '<section id="gallery">'.PHP_EOL;
-	echo '<div class="container">'.PHP_EOL;
-	echo '<div class="row">'.PHP_EOL;
-	echo '<div class="section_title col-12">'.PHP_EOL;
-	echo '<h1>'.$gallery_title.'</h1>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	echo '<div class="row">'.PHP_EOL;
-	echo '<div class="content col-12">'.PHP_EOL;
-	if ($gallery_pics):
-	echo '<ul class="gallery list-unstyled list-inline">'.PHP_EOL;
-	foreach ($gallery_pics as $gallery_pic):
-	echo '<li class="list-inline-item col-6 col-md-3 col-xl-2">'.PHP_EOL;
-	echo '<a href="'.$gallery_pic['url'].'" data-gallery="fls-gallery" data-toggle="lightbox">'.PHP_EOL;
-	echo '<img src="'.$gallery_pic['sizes']['thumbnail'].'" alt="'.$gallery_pic['alt'].'" class="img-fluid">'.PHP_EOL;
-	echo '</a>'.PHP_EOL;
-	echo '</li>'.PHP_EOL;
-	endforeach;
-	echo '</ul>'.PHP_EOL;
-	endif;
-	echo '</div>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	echo '</section>'.PHP_EOL;
-	endwhile;
-	;
-}
 
 // FREQUENTLY ASKED QUESTIONS
 
