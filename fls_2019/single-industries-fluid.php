e61c3042dc3cce21fc893673fa0903977c2cf5a3
Ron Madriz <ron@firstcreative.com>
2019-09-09 11:03:32 -0500

    fluid layout for industries test

--- fls_2019/single-industries-fluid.php
+++ fls_2019/single-industries-fluid.php
@@ -20,11 +20,6 @@ echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
-
-echo '<div id="pagewrapper" class="container-fluid">'.PHP_EOL;
-echo '<div class="row">'.PHP_EOL;
-echo '<div id="columns_2" class="col-12 col-md-9">'.PHP_EOL;
-
 // INDUSTRY CONTENT
 if (have_posts()):while (have_posts()):the_post();
 echo '<section id="main-content">'.PHP_EOL;
@@ -265,27 +260,6 @@ if (have_rows('download_literature')) {
 	echo '</div>'.PHP_EOL;
 	echo '</section>'.PHP_EOL;
 }
-
-// End Industry Main Column
-
-// Sidebar
-echo '</div>'.PHP_EOL;
-
-echo '<aside class="sidebar d-none d-sm-block col-md-3">'.PHP_EOL;
-$secondary_logo = get_field('secondary_logo');
-echo (!empty($secondary_logo)?'<div id="sidebar_logo" class="d-block text-center"><img src="'.$secondary_logo['url'].'" alt="'.$secondary_logo['alt'].'"></div>':'').PHP_EOL;
-$side_nav = get_field('has_sidebar_menu');
-if ($side_nav == 1) {
-	if (get_field('navmenu')) {
-		$menu = get_field('navmenu')->slug;
-	}
-	echo wp_nav_menu(['container' => 'nav', "menu" => $menu]);
-}
-get_sidebar();
-echo '</aside>'.PHP_EOL;
-
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
 
 // Solutions footer
 $solutions_footercs = get_field('solutions_footer');
