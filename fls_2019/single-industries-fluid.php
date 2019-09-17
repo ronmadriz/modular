260c6bc1317bd63575c580be06cf60315b3e6704
Ron Madriz <ron@firstcreative.com>
2019-09-16 21:10:21 -0500

    icons - structure

--- fls_2019/single-industries-fluid.php
+++ fls_2019/single-industries-fluid.php
@@ -93,8 +93,8 @@ if (have_rows('icon_row')) {
 			$fl_image   = get_sub_field('icon_image');
 			$fl_content = get_sub_field('icon_content');
 			echo '<div class="row h-100 justify-content-center align-items-center">'.PHP_EOL;
-			echo '<div class="col-3"><img src="'.$fl_image['url'].'" class="img-fluid" alt=""></div>'.PHP_EOL;
-			echo '<div class="col-9">'.$fl_content.'</div>'.PHP_EOL;
+			echo '<div class="fl_thumb col-12 col-md-3"><img src="'.$fl_image['url'].'" class="img-fluid" alt=""></div>'.PHP_EOL;
+			echo '<div class="fl_content col-12 col-md-9">'.$fl_content.'</div>'.PHP_EOL;
 			echo '</div>'.PHP_EOL;
 		}
 		echo '</div>'.PHP_EOL;
