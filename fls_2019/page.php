205ff6f267fa0e1c1ac820247a016a8037f27522
Ron Madriz <ron@firstcreative.com>
2019-09-16 23:13:15 -0500

    privacy policy

--- fls_2019/page.php
+++ fls_2019/page.php
@@ -5,7 +5,7 @@ get_header();
 $banner_img = get_field('banner');
 // BANNER
 echo '<section id="banner">'.PHP_EOL;
-echo '<div class="container">'.PHP_EOL;
+echo (!is_page('privacy-policy')?'<div class="container">':'<div class="container-fluid">').PHP_EOL;
 echo ($banner_img != null?'<div class="row w-image"><style type="text/css">section#banner{background-image:url('.$banner_img['url'].');}</style>':'<div class="row">').PHP_EOL;
 
 echo '<div class="page_title col-12 col-md-7">'.PHP_EOL;
