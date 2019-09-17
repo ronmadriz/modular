5edbacd3b89da9cb468da8393d603cc48530eec3
Ron Madriz <ron@firstcreative.com>
2019-09-16 21:08:42 -0500

    icons - styling

--- fls_2019/single-industries-fluid.php
+++ fls_2019/single-industries-fluid.php
@@ -85,7 +85,7 @@ if ($cf_contactForm) {
 // ICONS
 $icons = get_field('icon_row');
 if (have_rows('icon_row')) {
-	if (have_rows('icon_row') {
+	if (have_rows('icon_row')) {
 		echo '<section id="icons">'.PHP_EOL;
 		echo '<div class="container-fluid">'.PHP_EOL;
 		while (have_rows('icon_row')) {
