0421f7997a3ca015d3d300283b94bf3f30e4c7f0
Ron Madriz <ron@firstcreative.com>
2019-09-18 16:11:37 -0500

    fall protection 101

--- fls_2019/page-fall-protection-101.php
+++ fls_2019/page-fall-protection-101.php
@@ -34,18 +34,4 @@ echo '</section>'.PHP_EOL;
 
 // Sidebar
 echo '</div>'.PHP_EOL;
-
-echo '<aside class="sidebar d-none d-sm-block col-md-3">'.PHP_EOL;
-$secondary_logo = get_field('secondary_logo');
-echo ((!empty($secondary_logo)) && (!is_page('fall-protection-101'))?'<div id="sidebar_logo" class="d-block text-center"><img src="'.$secondary_logo['url'].'" alt="'.$secondary_logo['alt'].'"></div>':'').PHP_EOL;
-$side_nav = get_field('has_sidebar_menu');
-if ($side_nav == 1) {
-	if (get_field('navmenu')) {
-		$menu = get_field('navmenu')->slug;
-	}
-	echo wp_nav_menu(['container' => 'nav', "menu" => $menu]);
-}
-get_sidebar();
-echo '</aside>'.PHP_EOL;
-echo '</div></div>'.PHP_EOL;
 get_footer();?>
