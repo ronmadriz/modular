9bc9b4a24b5f6364a8e191c7b11145897cce1733
Ron Madriz <ron@firstcreative.com>
2019-10-04 12:56:03 -0500

    Aircraft Page

--- fls_2019/assets/dist/scripts/main.js
+++ fls_2019/assets/dist/scripts/main.js
@@ -256,6 +256,13 @@ jQuery(
         location.href = this.href;
       });
     }
+    function offsetAnchor() {
+        if(location.hash.length !== 0) {
+            window.scrollTo(window.scrollX, window.scrollY - 100);
+        }
+    }
+    window.addEventListener("hashchange", offsetAnchor);
+    window.setTimeout(offsetAnchor, 1)    
   }
 );
 
--- fls_2019/assets/src/scripts/main.js
+++ fls_2019/assets/src/scripts/main.js
@@ -87,6 +87,13 @@ jQuery(
         location.href = this.href;
       });
     }
+    function offsetAnchor() {
+        if(location.hash.length !== 0) {
+            window.scrollTo(window.scrollX, window.scrollY - 100);
+        }
+    }
+    window.addEventListener("hashchange", offsetAnchor);
+    window.setTimeout(offsetAnchor, 1)    
   }
 );
 
