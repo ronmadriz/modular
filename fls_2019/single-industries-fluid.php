91998e09004b1d295a4d306cc8aff2b602931d3e
Ron Madriz <ron@firstcreative.com>
2019-10-18 15:11:05 -0500

    fluid - add videos and literature

--- fls_2019/single-industries-fluid.php
+++ fls_2019/single-industries-fluid.php
@@ -132,6 +132,41 @@ if (have_rows('icon_row')) {
 	}
 }
 
+// DOWNLOADABLE FILES
+if (have_rows('download_literature')) {
+	echo '<section id="literature">'.PHP_EOL;
+	echo '<div class="container-fluid">'.PHP_EOL;
+	echo '<div class="row">'.PHP_EOL;
+	echo '<div class="section_title col-12 col-md-10">'.PHP_EOL;
+	echo '<h1>download literature</h1>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '<div class="row">'.PHP_EOL;
+	echo '<div class="content col-12 col-md-10">'.PHP_EOL;
+	echo '<ul class="list-inline literature_list">'.PHP_EOL;
+	while (have_rows('download_literature')) {
+		the_row();
+		$lit_file  = get_sub_field('file');
+		$lit_img   = get_sub_field('Image');
+		$lit_title = get_sub_field('title');
+		echo '<li class="list-inline-item col-6 col-md-4 text-center">';
+		echo ($lit_file != null?'<a href="'.$lit_file['url'].'" class="d-block">':'');
+		echo ($lit_img != null?'<img src="'.$lit_img['url'].'" alt="'.$lit_img['alt'].'" class="img-fluid">':'');
+		echo ($lit_file != null?'</a>'.PHP_EOL:'');
+		echo '<h4 class="text-uppercase">';
+		echo ($lit_file != null?'<a href="'.$lit_file['url'].'">':'');
+		echo $lit_title;
+		echo ($lit_file != null?'</a>':'');
+		echo '</h4>'.PHP_EOL;
+		echo '</li>'.PHP_EOL;
+	}
+	echo '</ul>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
+}
+
 // SOLUTIONS
 $solutions_footercs = get_field('solutions_footer');
 if ($solutions_footercs) {
@@ -254,41 +289,6 @@ if (have_rows('industry_faq')) {
 	echo '</dl>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	echo '</section>'.PHP_EOL;
-}
-
-// DOWNLOADABLE FILES
-if (have_rows('download_literature')) {
-	echo '<section id="literature">'.PHP_EOL;
-	echo '<div class="container-fluid">'.PHP_EOL;
-	echo '<div class="row">'.PHP_EOL;
-	echo '<div class="section_title col-12 col-md-10">'.PHP_EOL;
-	echo '<h1>download literature</h1>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	echo '<div class="row">'.PHP_EOL;
-	echo '<div class="content col-12 col-md-10">'.PHP_EOL;
-	echo '<ul class="list-inline literature_list">'.PHP_EOL;
-	while (have_rows('download_literature')) {
-		the_row();
-		$lit_file  = get_sub_field('file');
-		$lit_img   = get_sub_field('Image');
-		$lit_title = get_sub_field('title');
-		echo '<li class="list-inline-item col-6 col-md-4 text-center">';
-		echo ($lit_file != null?'<a href="'.$lit_file['url'].'" class="d-block">':'');
-		echo ($lit_img != null?'<img src="'.$lit_img['url'].'" alt="'.$lit_img['alt'].'" class="img-fluid">':'');
-		echo ($lit_file != null?'</a>'.PHP_EOL:'');
-		echo '<h4 class="text-uppercase">';
-		echo ($lit_file != null?'<a href="'.$lit_file['url'].'">':'');
-		echo $lit_title;
-		echo ($lit_file != null?'</a>':'');
-		echo '</h4>'.PHP_EOL;
-		echo '</li>'.PHP_EOL;
-	}
-	echo '</ul>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
