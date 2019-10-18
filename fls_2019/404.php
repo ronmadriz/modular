50dab3a3dfe663684c257a3e68691d20c12cd4ab
Ron Madriz <ron@firstcreative.com>
2019-10-18 11:43:13 -0500

    banner for 404

--- fls_2019/404.php
+++ fls_2019/404.php
@@ -4,7 +4,7 @@ $pageID = get_the_id();
 $pageCF = get_post_custom($pageID);
 echo '<section id="banner">'.PHP_EOL;
 echo '<div class="container">'.PHP_EOL;
-echo '<div class="row w-image"><style type="text/css">section#banner{background-image:url(/wp-content/uploads/2019/10/main_training2-2.jpg);}</style>'.PHP_EOL;
+echo '<div class="row w-image"><style type="text/css">section#banner{background-image:url(https://fall-arrest.com/wp-content/uploads/2019/10/main_training2-2.jpg);}</style>'.PHP_EOL;
 echo '<div class="page_title col-12 col-md-7">'.PHP_EOL;
 $alternate_page_title = get_field('alternate_page_title');
 echo '<h1></h1>'.PHP_EOL;
