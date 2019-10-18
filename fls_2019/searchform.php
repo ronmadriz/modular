8edde364b8c13a0595aaa4523304456f8a51906c
Ron Madriz <ron@firstcreative.com>
2019-10-18 12:04:54 -0500

    banner for 404

--- fls_2019/searchform.php
+++ fls_2019/searchform.php
@@ -5,6 +5,6 @@ echo '">'.PHP_EOL;
 echo '<label class="sr-only" for="s">Search for:</label>'.PHP_EOL;
 echo '<input type="text" value="" name="s" id="s" placeholder="Search...">'.PHP_EOL;
 echo '<button type="submit" id="searchsubmit"><i class="fas fa-search"></i><span class="sr-only">Search</span></button>'.PHP_EOL;
-echo '<button type="button" id="searchClose"><i class="fas fa-times"></i></button>'.PHP_EOL;
+echo (!is_404()?'<button type="button" id="searchClose"><i class="fas fa-times"></i></button>'.PHP_EOL:'');
 echo '</form>'.PHP_EOL;
 ?>
