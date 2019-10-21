e5f5c7aed93e072b0793b5530b90b2f82e026a91
Ron Madriz <ron@firstcreative.com>
2019-10-21 17:10:34 -0500

    pdf link open new tab

--- fls_2019/assets/src/scripts/main.js
+++ fls_2019/assets/src/scripts/main.js
@@ -11,7 +11,7 @@ j(document).ready(function(){
 	})	
 	!function(t){t.fn.bcSwipe=function(e){var n={threshold:50};return e&&t.extend(n,e),this.each(function(){function e(t){1==t.touches.length&&(u=t.touches[0].pageX,c=!0,this.addEventListener("touchmove",o,!1))}function o(e){if(c){var o=e.touches[0].pageX,i=u-o;Math.abs(i)>=n.threshold&&(h(),t(this).carousel(i>0?"next":"prev"))}}function h(){this.removeEventListener("touchmove",o),u=null,c=!1}var u,c=!1;"ontouchstart"in document.documentElement&&this.addEventListener("touchstart",e,!1)}),this}}(jQuery);
 	j('.carousel').bcSwipe({ threshold: 50 });
-
+  j("a[target!='_blank'][href$='.pdf']").attr("target", "_blank");
 	j(window).scroll(function(){
 	    if (j(this).scrollTop() > 50) {
 	       j('section').addClass('focus');
