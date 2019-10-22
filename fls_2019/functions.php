38d962f337b5f3a8f60d7249ef6a31e7ee65a44c
Ron Madriz <ron@firstcreative.com>
2019-10-22 11:19:10 -0500

    pagination

--- fls_2019/functions.php
+++ fls_2019/functions.php
@@ -645,4 +645,17 @@ add_filter('acf/settings/remove_wp_meta_box', '__return_false');
 if (function_exists('add_image_size')) {
 	add_image_size('gallery-th', 256, 144, true);//(cropped)
 }
+// pagination
+if (!function_exists('post_pagination')):
+function post_pagination() {
+	global $wp_query;
+	$pager = 999999999;// need an unlikely integer
+	echo paginate_links(array(
+			'base'    => str_replace($pager, '%#%', esc_url(get_pagenum_link($pager))),
+			'format'  => '?paged=%#%',
+			'current' => max(1, get_query_var('paged')),
+			'total'   => $wp_query->max_num_pages
+		));
+}
+endif;
 ?>
