a880aebc77f8de8854d1728edc70d241b2048bea
Ron Madriz <ron@firstcreative.com>
2019-10-22 14:04:46 -0500

    case studies - restructure

--- fls_2019/single-industries-fluid.php
+++ fls_2019/single-industries-fluid.php
@@ -238,6 +238,7 @@ if (have_rows('case_study_groups')) {
 		}
 		wp_reset_postdata();
 	} elseif ($studies && $studies_count > 3) {
+		echo '<div class="justify-content-center row">'.PHP_EOL;
 		while (have_rows('studies')) {
 			the_row();
 			$study_item = get_sub_field('case_study');
@@ -246,7 +247,6 @@ if (have_rows('case_study_groups')) {
 			setup_postdata($study);
 			$study_title   = get_field('case_study_title', $study->ID);
 			$study_summary = get_field('summary', $study->ID);
-			echo '<div class="justify-content-center row">'.PHP_EOL;
 			echo '<div class="item col-12 col-md-3">'.PHP_EOL;
 			echo '<span class="d-block img">'.PHP_EOL;
 			echo get_the_post_thumbnail($study->ID, 'medium', array('class' => 'img-fluid')).PHP_EOL;
@@ -257,9 +257,9 @@ if (have_rows('case_study_groups')) {
 			echo '<span class="d-block buttons"><a href="'.get_permalink($study->ID).'" class="btn btn-dark">View Case Study</a></span>';
 			echo '</span>'.PHP_EOL;
 			echo '</div>'.PHP_EOL;
-			echo '</div>'.PHP_EOL;
 			endif;
 		}
+		echo '</div>'.PHP_EOL;
 		wp_reset_postdata();
 	}
 	endwhile;
