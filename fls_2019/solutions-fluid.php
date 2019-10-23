52675c2a872c6454c041b1b9e65aa75a6388efa8
Ron Madriz <ron@firstcreative.com>
2019-10-23 13:28:46 -0500

    fluid fix

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -13,7 +13,7 @@ echo '<section id="banner">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
 echo ($banner_img != null?'<div class="row w-image"><style type="text/css">section#banner{background-image:url('.$banner_img['url'].');}</style>':'<div class="row">').PHP_EOL;
 
-echo '<div class="page_title col-12 col-md-7">'.PHP_EOL;
+echo '<div class="page_title col-12">'.PHP_EOL;
 echo '<h1>'.(!empty($alternate_page_title)?$alternate_page_title:get_the_title()).'</h1>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
