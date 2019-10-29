fb7cb39362043f29a3c8fd9413631391e65a62e0
Ron Madriz <ron@firstcreative.com>
2019-10-29 18:32:35 -0500

    solutions - measurement tables

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -134,6 +134,43 @@ if (have_rows('icon_row')) {
 	}
 }
 
+// TABLES
+if (have_rows('measurement_tables')) {
+	while (have_rows('measurement_tables')) {
+		the_row();
+		$table = get_sub_field('table');
+		if (!empty($table)) {
+			echo '<table border="0">';
+			if (!empty($table['caption'])) {
+				echo '<caption>'.$table['caption'].'</caption>';
+			}
+			if (!empty($table['header'])) {
+				echo '<thead>';
+				echo '<tr>';
+				foreach ($table['header'] as $th) {
+					echo '<th>';
+					echo $th['c'];
+					echo '</th>';
+				}
+				echo '</tr>';
+				echo '</thead>';
+			}
+			echo '<tbody>';
+			foreach ($table['body'] as $tr) {
+				echo '<tr>';
+				foreach ($tr as $td) {
+					echo '<td>';
+					echo $td['c'];
+					echo '</td>';
+				}
+				echo '</tr>';
+			}
+			echo '</tbody>';
+			echo '</table>';
+		}
+	}
+}
+
 // LITERATURE
 
 if (have_rows('download_literature')) {
