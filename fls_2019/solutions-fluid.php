3a259c2d7635bb4ad8c57d4e407029e1a60cf4c5
Ron Madriz <ron@firstcreative.com>
2019-10-31 00:15:04 -0500

    portables - gallery fix

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -48,7 +48,7 @@ if ($solutions_gallery['gallery_pics']) {
 		echo '<section id="carousel">'.PHP_EOL;
 		echo '<div class="container-fluid">'.PHP_EOL;
 		echo '<div class="row justify-content-center mb-md-3"><div class="content text-md-center col-12">'.PHP_EOL;
-		echo '<div id="flsCarousel" class="carousel slide" data-ride="carousel">'.PHP_EOL;
+		echo '<div id="solutionsCarousel" class="carousel slide" data-ride="carousel">'.PHP_EOL;
 		$gallery_pics = get_sub_field('gallery_pics');
 		if ($gallery_pics) {
 			echo '<div class="carousel-outer"><div class="carousel-inner">'.PHP_EOL;
@@ -61,16 +61,18 @@ if ($solutions_gallery['gallery_pics']) {
 			endforeach;
 			$gall_count = 0;
 			echo '</div>';
-			echo '<a class="carousel-control left" href="#flsCarousel" data-slide="prev"><i class="fa fa-chevron-left"></i></a>'.PHP_EOL;
-			echo '<a class="carousel-control right" href="#flsCarousel" data-slide="next"><i class="fa fa-chevron-right"></i></a>'.PHP_EOL;
+			echo '<a class="carousel-control left" href="#solutionsCarousel" data-slide="prev"><i class="fa fa-chevron-left"></i></a>'.PHP_EOL;
+			echo '<a class="carousel-control right" href="#solutionsCarousel" data-slide="next"><i class="fa fa-chevron-right"></i></a>'.PHP_EOL;
 			echo '</div>';
 		}
 		$gallery_photos = get_sub_field('gallery_pics');
 		if ($gallery_photos) {
 			echo '<ol class="carousel-indicators mCustomScrollbar">'.PHP_EOL;
 			foreach ($gallery_photos as $gallery_photo):
-			echo '<li data-target="#flsCarousel"'.($gall_count == 0?' active':'').'"" data-slide-to="'.$gall_count.'">'.PHP_EOL;
+			echo '<li data-target="#solutionsCarousel"'.($gall_count == 0?' active':'').'"" data-slide-to="'.$gall_count.'">'.PHP_EOL;
+			echo '<a id="carousel-selector-'.$gall_count.'"'.($gall_count == 0?' class="selected"':'').'>'.PHP_EOL;
 			echo '<img src="'.$gallery_photo['sizes']['gallery-th'].'" alt="'.$gallery_photo['alt'].'" class="img-fluid">'.PHP_EOL;
+			echo '</a>'.PHP_EOL;
 			echo '</li>'.PHP_EOL;
 			$gall_count++;
 			endforeach;
