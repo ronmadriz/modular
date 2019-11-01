a8efadee30a6b8529fcf5aacd2504ae883f85afd
Ron Madriz <ron@firstcreative.com>
2019-11-01 12:19:02 -0500

    testimonials - quotes

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -99,7 +99,16 @@ if (have_rows('quotes')) {
 	echo '<div class="row justify-content-center">'.PHP_EOL;
 	while (have_rows('quotes')) {
 		the_row();
-		echo '<!-- Im a row -->'.PHP_EOL;
+		$testimonials = get_sub_field('testimonial');
+		if ($testimonials) {
+			$testimonial = $testimonials;
+			setup_postdata($testimonial);
+			echo '<div class="content col-12">'.PHP_EOL;
+			echo '<span class="sr-only">customer testimonial</span>'.PHP_EOL;
+			echo get_the_content();
+			echo '<p class="text-right">~ '.get_the_title($testimonials).'</p>'.PHP_EOL;
+			wp_reset_postdata();
+		}
 	}
 	echo '</div>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
