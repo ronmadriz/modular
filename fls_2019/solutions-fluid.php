45a416aac416bfbf38a66a341aae0623e5e45347
Ron Madriz <ron@firstcreative.com>
2019-11-01 12:00:17 -0500

    testimonials - quotes

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -93,25 +93,25 @@ if ($solutions_gallery['gallery_pics']) {
 // QUOTE
 $quotes = get_field('quotes');
 if (have_rows('quotes')) {
+	echo '<section id="testimonial">'.PHP_EOL;
+	echo '<div class="container-fluid">'.PHP_EOL;
+	echo '<div class="row justify-content-center">'.PHP_EOL;
 	// loop through the rows of data
 	while (have_rows('quotes')) {
 		$testimonials = get_sub_field('testimonial');
 		if ($testimonials) {
 			$testimonial = $testimonials;
 			setup_postdata($testimonial);
-			echo '<section id="testimonial">'.PHP_EOL;
-			echo '<div class="container-fluid">'.PHP_EOL;
-			echo '<div class="row justify-content-center">'.PHP_EOL;
 			echo '<div class="content col-12">'.PHP_EOL;
 			echo '<span class="sr-only">customer testimonial</span>'.PHP_EOL;
 			echo get_the_content();
 			echo '<p class="text-right">~ '.get_the_title($testimonials).'</p>'.PHP_EOL;
-			echo '</div>'.PHP_EOL;
-			echo '</div>'.PHP_EOL;
-			echo '</section>'.PHP_EOL;
 			wp_reset_postdata();
 		}
 	}
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
 }
 
 // CTA
