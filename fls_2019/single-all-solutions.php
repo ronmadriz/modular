28570c62ac078a75f1b72dc6a3201f5b28e360dd
Ron Madriz <ron@firstcreative.com>
2019-12-04 16:09:58 -0600

    fluid all solutions page

--- fls_2019/single-all-solutions.php
+++ fls_2019/single-all-solutions.php
@@ -53,7 +53,8 @@ if (have_rows('all_solutions')) {
 				$callout_image   = get_sub_field('image');
 				$callout_title   = get_sub_field('title');
 				$callout_content = get_sub_field('content');
-				echo (!empty($callout_image)?'<div class="img col-12 col-md-4 align-self-center"><img src="'.$callout_image['url'].'" class="img-fluid"></div><div class="content text-center text-md-left col-12 col-md-8 align-self-center">'.PHP_EOL:'<div class="content text-center text-md-left col-12 align-self-center">');
+				$callout_urls    = get_sub_field('urls');
+				echo (!empty($callout_image)?'<div class="img col-12 col-md-4"><img src="'.$callout_image['url'].'" class="img-fluid"></div><div class="content text-center text-md-left col-12 col-md-8">'.PHP_EOL:'<div class="content text-center text-md-left col-12">');
 				echo (!empty($callout_title)?'<h3>'.$callout_title.'</h3>'.PHP_EOL:'');
 				echo (!empty($callout_content)?$callout_content.PHP_EOL:'');
 				echo '</div>'.PHP_EOL;
