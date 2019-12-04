af0795e623b9d1e157a5309859223bdb816fcb97
Ron Madriz <ron@firstcreative.com>
2019-12-04 14:45:39 -0600

    callouts

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -130,11 +130,11 @@ if (have_rows('image_callout')) {
 		if (have_rows('callout')) {
 			while (have_rows('callout')) {
 				the_row();
-				echo '<div class="row item">'.PHP_EOL;
+				echo '<div class="row justify-content-center item">'.PHP_EOL;
 				$callout_image   = get_sub_field('image');
 				$callout_title   = get_sub_field('title');
 				$callout_content = get_sub_field('content');
-				echo (!empty($callout_image)?'<div class="img col-12 col-md-4"><img src="'.$callout_image['url'].'" class="img-fluid"></div><div class="content col-12 col-md-8">'.PHP_EOL:'<div class="content col-12">');
+				echo (!empty($callout_image)?'<div class="img col-12 col-md-4 align-self-center"><img src="'.$callout_image['url'].'" class="img-fluid"></div><div class="content text-center text-md-left col-12 col-md-8 align-self-center">'.PHP_EOL:'<div class="content text-center text-md-left col-12 align-self-center">');
 				echo (!empty($callout_title)?'<h3>'.$callout_title.'</h3>'.PHP_EOL:'');
 				echo (!empty($callout_content)?$callout_content.PHP_EOL:'');
 				echo '</div>'.PHP_EOL;
