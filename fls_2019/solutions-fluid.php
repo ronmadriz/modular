dafba6e90cade687963ffaec37b39b9d32ebb0a7
Ron Madriz <ron@firstcreative.com>
2019-12-04 14:37:49 -0600

    callouts

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -134,7 +134,7 @@ if (have_rows('image_callout')) {
 				$callout_image   = get_sub_field('image');
 				$callout_title   = get_sub_field('title');
 				$callout_content = get_sub_field('content');
-				echo (!empty($callout_image)?'<div class="col-12 col-md-4"><img src="'.$callout_image['url'].'" class="img-fluid"></div><div class="col-12 col-md-8">'.PHP_EOL:'<div class="col-12">');
+				echo (!empty($callout_image)?'<div class="img col-12 col-md-4"><img src="'.$callout_image['url'].'" class="img-fluid"></div><div class="content col-12 col-md-8">'.PHP_EOL:'<div class="content col-12">');
 				echo (!empty($callout_title)?'<h3>'.$callout_title.'</h3>'.PHP_EOL:'');
 				echo (!empty($callout_content)?$callout_content.PHP_EOL:'');
 				echo '</div>'.PHP_EOL;
