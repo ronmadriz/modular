52b09c63315768949e309024f64a9577a33f7dd4
Ron Madriz <ron@firstcreative.com>
2019-12-06 16:09:27 -0600

    icon fix

--- fls_2019/assets/src/scripts/main.js
+++ fls_2019/assets/src/scripts/main.js
@@ -9,8 +9,8 @@ j(document).ready(function(){
 	j('#myModal').on('shown.bs.modal', function () {
 		j('#myInput').trigger('focus')
 	})	
-	!function(t){t.fn.bcSwipe=function(e){var n={threshold:50};return e&&t.extend(n,e),this.each(function(){function e(t){1==t.touches.length&&(u=t.touches[0].pageX,c=!0,this.addEventListener("touchmove",o,!1))}function o(e){if(c){var o=e.touches[0].pageX,i=u-o;Math.abs(i)>=n.threshold&&(h(),t(this).carousel(i>0?"next":"prev"))}}function h(){this.removeEventListener("touchmove",o),u=null,c=!1}var u,c=!1;"ontouchstart"in document.documentElement&&this.addEventListener("touchstart",e,!1)}),this}}(jQuery);
-	j('.carousel').bcSwipe({ threshold: 50 });
+//	!function(t){t.fn.bcSwipe=function(e){var n={threshold:50};return e&&t.extend(n,e),this.each(function(){function e(t){1==t.touches.length&&(u=t.touches[0].pageX,c=!0,this.addEventListener("touchmove",o,!1))}function o(e){if(c){var o=e.touches[0].pageX,i=u-o;Math.abs(i)>=n.threshold&&(h(),t(this).carousel(i>0?"next":"prev"))}}function h(){this.removeEventListener("touchmove",o),u=null,c=!1}var u,c=!1;"ontouchstart"in document.documentElement&&this.addEventListener("touchstart",e,!1)}),this}}(jQuery);
+//	j('.carousel').bcSwipe({ threshold: 50 });
   j('#flsCarousel').carousel('pause');
   j("a[target!='_blank'][href$='.pdf']").attr("target", "_blank");
 	j(window).scroll(function(){
