c635ab347f96834b7e53db46a307b3c3b6edf4e8
Ron Madriz <ron@firstcreative.com>
2019-12-16 15:52:09 -0600

    section title fix - h2

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -376,7 +376,7 @@ if (have_rows('case_study_groups')) {
 			echo '</a>'.PHP_EOL;
 			echo '</span>'.PHP_EOL;
 			echo '<span class="d-block content text-center text-md-left align-self-center">'.PHP_EOL;
-			echo (!empty($case_study_title)?'<h4>'.$case_study_title.'</h4>'.PHP_EOL:'<h4>'.get_the_title($study->ID).'</h4>'.PHP_EOL);
+			echo (!empty($case_study_title)?'<h3>'.$case_study_title.'</h3>'.PHP_EOL:'<h3>'.get_the_title($study->ID).'</h3>'.PHP_EOL);
 			echo '<p>'.$study_summary.'</p>'.PHP_EOL;
 			echo '<span class="d-block buttons"><a href="'.get_permalink($study->ID).'" class="btn btn-dark">View Case Study</a></span>';
 			echo '</span>'.PHP_EOL;
