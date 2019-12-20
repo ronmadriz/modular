85871bbfc76582221ba153020ed65329fd457444
Ron Madriz <ron@firstcreative.com>
2019-12-20 11:46:02 -0600

    title issues - inline solutions fix

--- fls_2019/single-solutions.php
+++ fls_2019/single-solutions.php
@@ -59,7 +59,7 @@ if (have_rows('case_study_groups')) {
 			echo get_the_post_thumbnail($study->ID, 'medium', array('class' => 'img-fluid')).PHP_EOL;
 			echo '</a>'.PHP_EOL;
 			echo '</div>'.PHP_EOL;
-			echo '<div class="content text-center text-md-left col-12 col-md-8 align-self-center">'.PHP_EOL;
+			echo '<div class="content text-center text-md-left col-12 col-md-8">'.PHP_EOL;
 			echo (!empty($case_study_title)?'<h3>'.$case_study_title.'</h3>'.PHP_EOL:'<h3>'.get_the_title($study->ID).'</h3>'.PHP_EOL);
 			echo '<p>'.$study_summary.'</p>'.PHP_EOL;
 			echo '<div class="buttons"><a href="'.get_permalink($study->ID).'" class="btn btn-dark">View Case Study</a></div>';
