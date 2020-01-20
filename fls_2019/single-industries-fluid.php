75d233d8616c989194f0f069ccb70e2f6965ac0b
Ron Madriz <ron@firstcreative.com>
2020-01-20 15:16:43 -0600

    post title shortcode

--- fls_2019/single-industries-fluid.php
+++ fls_2019/single-industries-fluid.php
@@ -459,9 +459,9 @@ if ($contact_form_code) {
 	<div class="container-fluid">
 		<div class="row">
 			<div id="cfCTA_form" class="col-12 col-md-6"><?php echo do_shortcode('[contact-form-7 id="117830" title="Contact CTA"]');?></div>
-			<div id="cfCTA_Img" class="col-12 col-md-6">
+			<div id="cfCTA_Img" class="d-flex justify-content-end align-items-end col-12 col-md-6">
 				<img src="https://simplifiedsafety.com/media/wysiwyg/staff/ssi/staff-at-desk-michael.jpg" alt="" class="img-fluid d-md-none">
-				<div class="align-self-baseline"><h3>Michael Hannah <small>Fall Protection Specialist</small></h3></div>
+				<div><h3>Michael Hannah <small>Fall Protection Specialist</small></h3></div>
 			</div>
 		</div>
 	</div>
