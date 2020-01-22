b03a839157c7e5769bb6087dd440669cb52954f2
Ron Madriz <ron@firstcreative.com>
2020-01-22 12:13:08 -0600

    contact form cta - wordpress integration

--- fls_2019/single-industries-fluid.php
+++ fls_2019/single-industries-fluid.php
@@ -377,7 +377,7 @@ if ($videos && $video_count == 1) {
 		echo '<div class="videos multi col-12 col-md-6">'.PHP_EOL;
 		echo '<span class="section_title"><h2>'.$v_title.'</h2></span>'.PHP_EOL;
 		echo '<span class="video">'.$v_embed.'</span>'.PHP_EOL;
-		echo ($v_desc?'<span class="content">'.$v_desc.'</span>'.PHP_EOL:'');
+		echo (!empty($v_desc)?'<span class="content">'.$v_desc.'</span>'.PHP_EOL:'');
 		echo '</div>'.PHP_EOL;
 		wp_reset_query();
 	}
@@ -443,7 +443,16 @@ if (have_rows('contact_form_cta')) {
 	echo '<div class="row">'.PHP_EOL;
 	while (have_rows('contact_form_cta')) {
 		the_row();
-
+		$cfcta_form          = get_sub_field('form');
+		$cfcta_img           = get_sub_field('image');
+		$cfcta_employee_name = get_sub_field('employee_name');
+		$cfcta_employee_role = get_sub_field('employee_role');
+		echo '<style>section#contactFormCTA {background-image: url('.(!empty($cfcta_img)?$ctcta_img['url']:'https://sandbox.fall-arrest.com/wp-content/uploads/2020/01/staff-at-desk-michael.jpg').');}</style>'.PHP_EOL;
+		echo '<div id="cfCTA_form" class="col-12 col-md-6">'.do_shortcode(''.$ctcta_form.'').'></div>'.PHP_EOL;
+		echo '<div id="cfCTA_Img" class="d-md-flex justify-content-center align-items-md-end col-12 col-md-6">'.PHP_EOL;
+		echo '<img src="'.(!empty($cfcta_img)?$ctcta_img['url']:'https://sandbox.fall-arrest.com/wp-content/uploads/2020/01/staff-at-desk-michael.jpg').'" alt="" class="img-fluid d-md-none">'.PHP_EOL;
+		// echo '<div><h3>'.$cfcta_employee_name.' <small>'.$cfcta_employee_role.'</small></h3></div>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
 	}
 	echo '</div>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
