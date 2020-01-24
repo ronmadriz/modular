b0b535dabb5637667a118f17b9d54e84730e595d
Ron Madriz <ron@firstcreative.com>
2020-01-24 12:27:30 -0600

    solutions - format first

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -141,7 +141,7 @@ if ($image_callout['callout']) {
 				if ($callout_link) {
 					echo '<div class="col-12 col-md-6">'.PHP_EOL;
 					echo '<a href="'.esc_url($callout_link).'"><img src="'.$callout_image['url'].'" alt="" class="img-fluid"></a>'.PHP_EOL;
-					echo '<div class="content text-center text-md-left">'.PHP_EOL;
+					echo '<div class="text-center text-md-left">'.PHP_EOL;
 					echo '<h3><a href="'.esc_url($callout_link).'">'.$callout_title.'</a></h3>'.PHP_EOL;
 					echo (!empty($callout_content)?'<p>'.$callout_content.'</p>'.PHP_EOL:'');
 					echo '</div>'.PHP_EOL;
@@ -149,7 +149,7 @@ if ($image_callout['callout']) {
 				} else {
 					echo '<div class="col-12 col-md-6">'.PHP_EOL;
 					echo '<img src="'.$callout_image['url'].'" alt="" class="img-fluid">'.PHP_EOL;
-					echo '<div class="content text-center text-md-left">'.PHP_EOL;
+					echo '<div class="text-center text-md-left">'.PHP_EOL;
 					echo '<h3>'.$callout_title.'</h3>'.PHP_EOL;
 					echo (!empty($callout_content)?'<p>'.$callout_content.'</p>'.PHP_EOL:'');
 					echo '</div>'.PHP_EOL;
