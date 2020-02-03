b789a4460a8ac210eb0ff2e6ed34103c76017d8e
Ron Madriz <ron@firstcreative.com>
2020-02-03 14:43:01 -0600

    update repo

--- fls_2019/assets/src/scripts/main.js
+++ fls_2019/assets/src/scripts/main.js
@@ -46,6 +46,24 @@ j(document).ready(function(){
       enableDrag:true,
       enableTouch:true,
       currentPagerPosition:'left',
+      speed:600,
+      responsive : [
+          {
+              breakpoint:800,
+              settings: {
+                  item:3,
+                  slideMove:1,
+                  slideMargin:6,
+                }
+          },
+          {
+              breakpoint:480,
+              settings: {
+                  item:2,
+                  slideMove:1
+                }
+          }
+      ]      
       onSliderLoad: function(el) {
         el.lightGallery({
             selector: '#lightSlider .lslide'
