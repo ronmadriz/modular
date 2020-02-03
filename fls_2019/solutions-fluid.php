5121985002d6a14aecb3925cd9be46dcbb7c3cda
Ron Madriz <ron@firstcreative.com>
2020-02-03 12:07:53 -0600

    solutions - if statement not working

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -43,12 +43,16 @@ if ($solutions_gallery['gallery_pics']) {
 	echo '<section id="gallery">'.PHP_EOL;
 	echo '<div class="container-fluid">'.PHP_EOL;
 	echo '<div class="row justify-content-center mb-md-3"><div class="content text-md-center col-12">'.PHP_EOL;
-	while (have_rows('industry_gallery')) {
+	while (have_rows('solutions_gallery')) {
 		the_row();
 		$gallery_pics = get_sub_field('gallery_pics');
-		?>
-		<!-- Hi -->
-		<?
+		if ($gallery_pics) {
+			echo '<ul id="lightSlider" class="gallery list-unstyled cS-hidden">'.PHP_EOL;
+			foreach ($gallery_pics as $gallery_pic):
+			echo '<li data-thumb="'.$gallery_pic['sizes']['medium'].'"><img src="'.$gallery_pic['sizes']['full'].'" alt="'.$gallery_pic['alt'].'" class="img-fluid" /></li>'.PHP_EOL;
+			endforeach;
+			echo '</ul>'.PHP_EOL;
+		}
 	}
 	echo '</div></div>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
