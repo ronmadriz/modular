4ebae85f8fad28de64858533e600a8c2e97e74b6
Ron Madriz <ron@firstcreative.com>
2020-02-06 13:37:13 -0600

    fluid footer reposition

--- fls_2019/single-industries-fluid.php
+++ fls_2019/single-industries-fluid.php
@@ -131,7 +131,7 @@ if (have_rows('testimonials')) {
 
 // Ugly CTA formerly cta_blue
 
-echo '<section id="cta_speak"><div class="container-fluid"><div class="row"><div class="col-12"><h2 class="text-center text-uppercase"><a href="#cf_contactFormTitle">Speak with a fall protection specialist <i class="im im-angle-right-circle"></i></a></h2></div></div></div></section>'.PHP_EOL;
+echo '<section id="cta_speak"><div class="container-fluid"><div class="row"><div class="col-12"><h2 class="text-center text-uppercase"><a href="#contactFormCTA">Speak with a fall protection specialist <i class="im im-angle-right-circle"></i></a></h2></div></div></div></section>'.PHP_EOL;
 
 // IMAGE CALLOUTS
 
