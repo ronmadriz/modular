255865144494a6d9a76d216a1c5be71e3762c7dd
Ron Madriz <ron@firstcreative.com>
2020-02-06 12:30:01 -0600

    cta grow on hover

--- fls_2019/footer-fluid.php
+++ fls_2019/footer-fluid.php
@@ -7,7 +7,6 @@ $phone              = get_theme_mod('footer_phone');
 $fax                = get_theme_mod('footer_fax');
 $email              = get_theme_mod('footer_email');
 $footer_newsletter  = get_theme_mod('footer_newsletter');
-echo '<footer class="fluid">'.PHP_EOL;
 if (is_front_page()) {
 	echo '<section id="foot_contact">'.PHP_EOL;
 	echo '<div class="container-fluid">'.PHP_EOL;
@@ -24,6 +23,7 @@ if (is_front_page()) {
 	echo '</section>'.PHP_EOL;
 }
 echo '</main>'.PHP_EOL;
+echo '<footer class="fluid">'.PHP_EOL;
 echo '<section id="foot_call_cta">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
 echo '<div class="row align-content-center justify-content-center">'.PHP_EOL;
