11cb57ce7a7ba4433492a29057663914a82d6fcc
Ron Madriz <ron@firstcreative.com>
2020-02-10 15:49:04 -0600

    Testimonials - Site Reviews - Loop Work

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -493,7 +493,7 @@ if (have_rows('contact_form_cta')) {
 		$cfcta_employee_name = get_sub_field('employee_name');
 		$cfcta_employee_role = get_sub_field('employee_role');
 		echo '<style>section#contactFormCTA {background-image: url("'.(!empty($cfcta_img)?$cfcta_img['url']:get_stylesheet_directory_uri().'/images/cfImage.jpg').'");}</style>'.PHP_EOL;
-		echo '<div id="cfCTA_form" class="col-12 col-sm-6 col-md-8 col-lg-7 col-xl-6">'.do_shortcode('[contact-form-7 id="1117830" title="Contact CTA"]').'</div>'.PHP_EOL;
+		echo '<div id="cfCTA_form" class="col-12 col-sm-6 col-md-7 col-lg-7 col-xl-7">'.do_shortcode('[contact-form-7 id="1117830" title="Contact CTA"]').'</div>'.PHP_EOL;
 		echo (!empty($cfcta_img)?'<div id="cfCTA_Img" class="d-flex d-md-none justify-content-center align-items-md-end col-12"><img src="'.$cfcta_img['url'].'" alt="" class="img-fluid"></div>':'').PHP_EOL;
 		echo (!empty($cfcta_employee_name) || !empty($cfcta_employee_role)?'<div class="col-12 col-md-7 text-right align-self-end" id="cfCTA_employeeInfo"><h3>'.$cfcta_employee_name.' <small>'.$cfcta_employee_role.'</small></h3></div>':'').PHP_EOL;
 	}
