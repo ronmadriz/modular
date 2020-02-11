bc63039c860743dabe73867a920f7d199cc82c61
Ron Madriz <ron@firstcreative.com>
2020-02-10 19:33:44 -0600

    contact form footer fix

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -481,11 +481,11 @@ echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
 }
  */
-echo '<section id="cta_blue"><h2 class="text-md-center">Tell Us About Your Fall Hazard</h2></div></section>'.PHP_EOL;
-echo '<section id="contactFormCTA">'.PHP_EOL;
-echo '<div class="container-fluid">'.PHP_EOL;
-echo '<div class="row">'.PHP_EOL;
 if (have_rows('contact_form_cta')) {
+	echo '<section id="cta_blue"><h2 class="text-md-center">Tell Us About Your Fall Hazard</h2></div></section>'.PHP_EOL;
+	echo '<section id="contactFormCTA">'.PHP_EOL;
+	echo '<div class="container-fluid">'.PHP_EOL;
+	echo '<div class="row">'.PHP_EOL;
 	while (have_rows('contact_form_cta')) {
 		the_row();
 		$cfcta_form          = get_sub_field('form');
@@ -497,9 +497,9 @@ if (have_rows('contact_form_cta')) {
 		echo (!empty($cfcta_img)?'<div id="cfCTA_Img" class="d-flex d-md-none justify-content-center align-items-md-end col-12"><img src="'.(!empty($cfcta_img)?$cfcta_img['url']:get_stylesheet_directory_uri().'/images/cfImage.jpg').'" alt="" class="img-fluid"></div>':'').PHP_EOL;
 		echo (!empty($cfcta_employee_name) || !empty($cfcta_employee_role)?'<div class="col-12 col-md-5 text-right align-self-end" id="cfCTA_employeeInfo"><h3>'.$cfcta_employee_name.' <small>'.$cfcta_employee_role.'</small></h3></div>':'').PHP_EOL;
 	}
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
 }
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</section>'.PHP_EOL;
 include 'footer-fluid.php';
 ?>
