7ff1e7a829d5e70e51b716723b027665b569cc19
Ron Madriz <ron@firstcreative.com>
2020-02-11 11:16:58 -0600

    footer call cta - text adjustment

--- fls_2019/single-industries-fluid.php
+++ fls_2019/single-industries-fluid.php
@@ -100,7 +100,7 @@ if (have_rows('testimonials')) {
 
 // Ugly CTA formerly cta_blue
 
-echo (!empty(get_the_content())?'<section id="cta_speak"><div class="container-fluid"><div class="row"><div class="col-12"><h2 class="text-center text-uppercase"><a href="#contactFormCTA">Speak with a fall protection specialist <i class="im im-angle-right-circle"></i></a></h2></div></div></div></section>'.PHP_EOL:'');
+echo (!empty(get_the_content())?'<section id="cta_speak"><div class="container-fluid"><div class="row"><div class="col-12"><h2 class="text-center text-uppercase"><a href="#cta_blue">Speak with a fall protection specialist <i class="im im-angle-right-circle"></i></a></h2></div></div></div></section>'.PHP_EOL:'');
 
 // IMAGE CALLOUTS
 
