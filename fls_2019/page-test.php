c8a4e167d90edd5b339193dc3c337fd7a8064e81
Ron Madriz <ron@firstcreative.com>
2020-02-17 11:12:14 -0600

    test fluid template

--- fls_2019/page-test.php
+++ fls_2019/page-test.php
@@ -1,29 +1,41 @@
 <?php
-$pageID = get_the_id();
-$pageCF = get_post_custom($pageID);
-get_header();
 /**
  * Template Name: Test
  */
-$banner_img = get_field('banner');
-echo '<section id="banner">'.PHP_EOL;
-echo '<div class="container">'.PHP_EOL;
-echo ($banner_img != null?'<div class="row w-image"><style type="text/css">section#banner{background-image:url('.$banner_img['url'].');}</style>':'<div class="row">').PHP_EOL;
-echo '<div class="page_title col-12 col-md-7">'.PHP_EOL;
+$pageID = get_the_id();
+$pageCF = get_post_custom($pageID);
+include 'header-fluid.php';
+
+$banner_img           = get_field('banner');
 $alternate_page_title = get_field('alternate_page_title');
+// BANNER
+echo '<section id="banner">'.PHP_EOL;
+echo '<div class="container-fluid">'.PHP_EOL;
+echo ($banner_img != null?'<div class="row w-image"><style type="text/css">section#banner{background-image:url('.$banner_img['url'].');}</style>':'<div class="row">').PHP_EOL;
+echo '<div class="page_title col-12">'.PHP_EOL;
 echo '<h1>'.(!empty($alternate_page_title)?$alternate_page_title:get_the_title()).'</h1>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
+
+the_breadcrumb();
+
 // MAIN CONTENT
-echo '<section id="main-content">'.PHP_EOL;
-echo '<div class="container">'.PHP_EOL;
-the_content();
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</section>'.PHP_EOL;
+if (have_posts()) {
+	echo '<section id="main-content">'.PHP_EOL;
+	echo '<div class="container-fluid">'.PHP_EOL;
+	echo '<div class="row">'.PHP_EOL;
+	while (have_posts()) {
+		the_post();
+		echo '<div class="col-12">';
+		the_content();
+		echo '</div>'.PHP_EOL;
+	}
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
+}
 // End Industry Main Column
-get_footer();
+include 'footer-fluid.php';
 ?>
