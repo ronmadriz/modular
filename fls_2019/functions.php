7774700b7709bcfe53ea2116eefa4eb89c891cdc
Ron Madriz <ron@firstcreative.com>
2020-02-17 16:40:15 -0600

    gateway fix - phone number masking

--- fls_2019/functions.php
+++ fls_2019/functions.php
@@ -5,7 +5,6 @@ function enqueue_my_scripts() {
 	$pathTojQuery  = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
 	$pathToScripts = get_stylesheet_directory_uri()."/assets/dist/scripts/main.min.js";
 	wp_enqueue_script('jquery', $pathTojQuery, array(), true, true);
-	wp_enqueue_script('site_script', $pathToScripts, array('jquery'), '1.0', true);
 	wp_enqueue_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', array('jquery'), '', true, true);
 	wp_enqueue_script('bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', array('jquery'), true, true);
 	wp_enqueue_script('lightbox', 'https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.js', array('jquery'), '', true, true);
@@ -13,6 +12,7 @@ function enqueue_my_scripts() {
 		wp_enqueue_script('lightgallery_js', 'https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.6.12/js/lightgallery.min.js', array('jquery'), '', true, true);
 		wp_enqueue_script('lightslide_js', 'https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/js/lightslider.min.js', array('jquery'), '', true, true);
 	}
+	wp_enqueue_script('site_script', $pathToScripts, array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'enqueue_my_scripts');
 // Enqueue Font Awesome.
