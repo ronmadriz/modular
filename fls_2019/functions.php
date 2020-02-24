5ef39f8a89f5a03bdb9b6317b43f19e634d21efb
Ron Madriz <ron@firstcreative.com>
2020-02-24 11:53:06 -0600

    attempt to retrieve user fields

--- fls_2019/functions.php
+++ fls_2019/functions.php
@@ -705,7 +705,7 @@ add_filter('wpcf7_form_tag', 'industryDropDown', 10, 2);
 //89 "New user" email to john@snow.com instead of admin.
 add_filter('wp_new_user_notification_email_admin', 'fls_new_admin_email', 10, 3);
 function fls_new_admin_email($notification, $user, $blogname) {
-	$notification['to']      = 'lyle@firstcreative.com';// , lyle@firstcreative.com
+	$notification['to']      = 'ron@firstcreative.com';// , lyle@firstcreative.com
 	$notification['message'] = '<p>A new user has registered with the following information <br>Username: '.$user['first_name'].' '.$user['last_name'].' at '.$user['email'].' '.$user['phone'].'</p>';
 	return $notification;
 }
