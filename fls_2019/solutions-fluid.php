eb5c2b4b20e81511610da642a1665fcc958bbfbe
Ron Madriz <ron@firstcreative.com>
2020-03-02 14:38:21 -0600

    video title

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -447,8 +447,10 @@ if ($videos) {
 				$v_embed = get_sub_field('v_embed');
 				$v_desc  = get_sub_field('description');
 				echo '<div class="row justify-content-center">'.PHP_EOL;
+				echo '<div class="videos single col-12"><span class="section_title"><h2>'.$v_title.'</h2></span></div>'.PHP_EOL;
+				echo '</div>'.PHP_EOL;
+				echo '<div class="row justify-content-center">'.PHP_EOL;
 				echo '<div class="videos single col-12 col-md-9">'.PHP_EOL;
-				echo '<span class="section_title"><h2>'.$v_title.'</h2></span>'.PHP_EOL;
 				echo '<span class="video">'.$v_embed.'</span>'.PHP_EOL;
 				echo '<span class="content">'.PHP_EOL;
 				echo $v_desc.PHP_EOL;
