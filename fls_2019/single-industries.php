118f43a9913e31068b33faa9d29b4f2364de745b
Ron Madriz <ron@firstcreative.com>
2020-03-02 15:18:10 -0600

    solutions breadcrumbs

--- fls_2019/single-industries.php
+++ fls_2019/single-industries.php
@@ -241,7 +241,7 @@ if (have_rows('download_literature')) {
 	echo '</div>'.PHP_EOL;
 	echo '<div class="row">'.PHP_EOL;
 	echo '<div class="content col-12 col-md-10">'.PHP_EOL;
-	echo '<ul class="list-inline literature_list">'.PHP_EOL;
+	echo '<ul class="d-flex align-items-center justify-content-center literature_list">'.PHP_EOL;
 	while (have_rows('download_literature')) {
 		the_row();
 		$lit_file  = get_sub_field('file');
