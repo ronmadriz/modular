f1682ab62fa8db78cf4c2c68f644360ebe125ef6
Ron Madriz <ron@firstcreative.com>
2020-03-02 15:46:52 -0600

    literature fixes

--- fls_2019/single-solutions.php
+++ fls_2019/single-solutions.php
@@ -141,7 +141,7 @@ if (have_rows('download_literature')) {
 	echo '</div>'.PHP_EOL;
 	echo '<div class="row">'.PHP_EOL;
 	echo '<div class="content col-12">'.PHP_EOL;
-	echo '<ul class="d-flex align-items-start literature_list">'.PHP_EOL;
+	echo '<ul class="d-flex align-items-start flex-wrap literature_list">'.PHP_EOL;
 	while (have_rows('download_literature')) {
 		the_row();
 		$lit_file  = get_sub_field('file');
