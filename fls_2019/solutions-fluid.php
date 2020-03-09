946dc26ce0c5bb86efd55c694f5d4b093f03036d
Ron Madriz <ron@firstcreative.com>
2020-03-09 13:06:21 -0500

    fixed case studies count

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -366,75 +366,77 @@ if ($solutions_footercs['solutions']) {
 // CASE STUDIES
 
 $case_studies = get_field('case_study_groups');
-if (have_rows('case_study_groups')) {
-	echo '<section id="case_study">'.PHP_EOL;
-	echo '<div class="container-fluid">'.PHP_EOL;
-	while (have_rows('case_study_groups')):the_row();
-	$study_section_title = get_sub_field('study_section_title');
-	$studies             = get_sub_field('studies');
-	$studies_count       = count($studies);
-	echo (!empty($study_section_title)?'<div class="row"><div class="section_title col-12"><h2>'.$study_section_title.'</h2></div></div>'.PHP_EOL:'');
-	if ($studies && $studies_count <= 3) {
-		while (have_rows('studies')) {
-			the_row();
-			$study_item = get_sub_field('case_study');
-			if ($study_item):
-			$study = $study_item;
-			setup_postdata($study);
-			$study_title   = get_field('case_study_title', $study->ID);
-			$study_summary = get_field('summary', $study->ID);
-			echo '<div class="item row">'.PHP_EOL;
-			echo '<div class="img col-12 col-md-4 align-self-center">'.PHP_EOL;
-			echo '<a href="'.get_permalink($study->ID).'">';
-			echo get_the_post_thumbnail($study->ID, 'medium', array('class' => 'img-fluid')).PHP_EOL;
-			echo '</a>'.PHP_EOL;
+if ($case_studies) {
+	$cs_count = 0;
+	if (is_array($case_studies)) {
+		$cs_count = count($case_studies);
+		echo '<section id="case_study">'.PHP_EOL;
+		echo '<div class="container-fluid">'.PHP_EOL;
+		while (have_rows('case_study_groups')):the_row();
+		$study_section_title = get_sub_field('study_section_title');
+		$studies             = get_sub_field('studies');
+		$studies_count       = count($studies);
+		echo (!empty($study_section_title)?'<div class="row"><div class="section_title col-12"><h2>'.$study_section_title.'</h2></div></div>'.PHP_EOL:'');
+		if ($studies && $studies_count <= 3) {
+			while (have_rows('studies')) {
+				the_row();
+				$study_item = get_sub_field('case_study');
+				if ($study_item):
+				$study = $study_item;
+				setup_postdata($study);
+				$study_title   = get_field('case_study_title', $study->ID);
+				$study_summary = get_field('summary', $study->ID);
+				echo '<div class="item row">'.PHP_EOL;
+				echo '<div class="img col-12 col-md-4 align-self-center">'.PHP_EOL;
+				echo '<a href="'.get_permalink($study->ID).'">';
+				echo get_the_post_thumbnail($study->ID, 'medium', array('class' => 'img-fluid')).PHP_EOL;
+				echo '</a>'.PHP_EOL;
+				echo '</div>'.PHP_EOL;
+				echo '<div class="content text-center text-md-left col-12 col-md-8 align-self-center">'.PHP_EOL;
+				echo (!empty($case_study_title)?'<h3>'.$case_study_title.'</h3>'.PHP_EOL:'<h3>'.get_the_title($study->ID).'</h3>'.PHP_EOL);
+				echo '<p>'.$study_summary.'</p>'.PHP_EOL;
+				echo '<div class="buttons"><a href="'.get_permalink($study->ID).'" class="btn btn-dark">View Case Study</a></div>';
+				echo '</div>'.PHP_EOL;
+				echo '</div>'.PHP_EOL;
+				endif;
+			}
+			wp_reset_postdata();
+		} elseif ($studies && $studies_count > 3) {
+			echo '<div class="row">'.PHP_EOL;
+			while (have_rows('studies')) {
+				the_row();
+				$study_item = get_sub_field('case_study');
+				if ($study_item):
+				$study = $study_item;
+				setup_postdata($study);
+				$study_title   = get_field('case_study_title', $study->ID);
+				$study_summary = get_field('summary', $study->ID);
+				echo '<div class="item col-12 col-md-3">'.PHP_EOL;
+				echo '<span class="d-block img">'.PHP_EOL;
+				echo '<a href="'.get_permalink($study->ID).'">';
+				echo get_the_post_thumbnail($study->ID, 'medium', array('class' => 'img-fluid')).PHP_EOL;
+				echo '</a>'.PHP_EOL;
+				echo '</span>'.PHP_EOL;
+				echo '<span class="d-block content text-center text-md-left align-self-center">'.PHP_EOL;
+				echo (!empty($case_study_title)?'<h3>'.$case_study_title.'</h3>'.PHP_EOL:'<h3>'.get_the_title($study->ID).'</h3>'.PHP_EOL);
+				echo '<p>'.$study_summary.'</p>'.PHP_EOL;
+				echo '<span class="d-block buttons"><a href="'.get_permalink($study->ID).'" class="btn btn-dark">View Case Study</a></span>';
+				echo '</span>'.PHP_EOL;
+				echo '</div>'.PHP_EOL;
+				endif;
+			}
 			echo '</div>'.PHP_EOL;
-			echo '<div class="content text-center text-md-left col-12 col-md-8 align-self-center">'.PHP_EOL;
-			echo (!empty($case_study_title)?'<h3>'.$case_study_title.'</h3>'.PHP_EOL:'<h3>'.get_the_title($study->ID).'</h3>'.PHP_EOL);
-			echo '<p>'.$study_summary.'</p>'.PHP_EOL;
-			echo '<div class="buttons"><a href="'.get_permalink($study->ID).'" class="btn btn-dark">View Case Study</a></div>';
-			echo '</div>'.PHP_EOL;
-			echo '</div>'.PHP_EOL;
-			endif;
+			wp_reset_postdata();
 		}
+		endwhile;
 		wp_reset_postdata();
-	} elseif ($studies && $studies_count > 3) {
-		echo '<div class="row">'.PHP_EOL;
-		while (have_rows('studies')) {
-			the_row();
-			$study_item = get_sub_field('case_study');
-			if ($study_item):
-			$study = $study_item;
-			setup_postdata($study);
-			$study_title   = get_field('case_study_title', $study->ID);
-			$study_summary = get_field('summary', $study->ID);
-			echo '<div class="item col-12 col-md-3">'.PHP_EOL;
-			echo '<span class="d-block img">'.PHP_EOL;
-			echo '<a href="'.get_permalink($study->ID).'">';
-			echo get_the_post_thumbnail($study->ID, 'medium', array('class' => 'img-fluid')).PHP_EOL;
-			echo '</a>'.PHP_EOL;
-			echo '</span>'.PHP_EOL;
-			echo '<span class="d-block content text-center text-md-left align-self-center">'.PHP_EOL;
-			echo (!empty($case_study_title)?'<h3>'.$case_study_title.'</h3>'.PHP_EOL:'<h3>'.get_the_title($study->ID).'</h3>'.PHP_EOL);
-			echo '<p>'.$study_summary.'</p>'.PHP_EOL;
-			echo '<span class="d-block buttons"><a href="'.get_permalink($study->ID).'" class="btn btn-dark">View Case Study</a></span>';
-			echo '</span>'.PHP_EOL;
-			echo '</div>'.PHP_EOL;
-			endif;
-		}
 		echo '</div>'.PHP_EOL;
-		wp_reset_postdata();
+		echo '</section>'.PHP_EOL;
 	}
-	endwhile;
-	wp_reset_postdata();
-	echo '</div>'.PHP_EOL;
-	echo '</section>'.PHP_EOL;
 }
-
 // VIDEOS
 $videos = get_field('videos');
 if ($videos) {
-	// echo '<!-- hi -->';
 	$video_count = 0;
 	if (is_array($videos)) {
 		$video_count = count($videos);
