8fac15688cdf4baad1e00e21fe3b87dbbeff0eb9
Ron Madriz <ron@firstcreative.com>
2020-04-22 04:26:18 -0500

    new global banner view and view folder

---
+++ fls_2019/views/global/banner.php
@@ -0,0 +1,14 @@
+<?php
+$banner_img = get_field('banner');
+// BANNER
+echo '<section id="banner"'.(empty($banner_img)?' class="no_img"':'').'>'.PHP_EOL;
+echo '<div class="container">'.PHP_EOL;
+echo ($banner_img != null?'<div class="row w-image"><style type="text/css">section#banner{background-image:url('.$banner_img['url'].');}</style>':'<div class="row">').PHP_EOL;
+
+echo '<div class="page_title col-12 col-md-7">'.PHP_EOL;
+$alternate_page_title = get_field('alternate_page_title');
+echo '<h1>'.(!empty($alternate_page_title)?$alternate_page_title:get_the_title()).'</h1>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</section>'.PHP_EOL;
