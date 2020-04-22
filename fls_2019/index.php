7777c40e4b940a836d9914a6658833dcb29549fd
Ron Madriz <ron@firstcreative.com>
2020-04-22 01:45:19 -0500

    Blog Main - Fix

--- fls_2019/index.php
+++ fls_2019/index.php
@@ -9,8 +9,7 @@ echo '<div class="container">'.PHP_EOL;
 echo ($banner_img != null?'<div class="row w-image"><style type="text/css">section#banner{background-image:url('.$banner_img['url'].');}</style>':'<div class="row">').PHP_EOL;
 
 echo '<div class="page_title col-12 col-md-7">'.PHP_EOL;
-$alternate_page_title = get_field('alternate_page_title');
-echo '<h1>'.(!empty($alternate_page_title)?$alternate_page_title:get_the_title()).'</h1>'.PHP_EOL;
+echo '<h1>'.get_the_title($pageID).'</h1>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
