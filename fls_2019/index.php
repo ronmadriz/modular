dc441e2a1600206e178e775e2872a821c9f26ce3
Ron Madriz <ron@firstcreative.com>
2020-04-24 00:21:11 -0500

    Blog Page - Title Bar

--- fls_2019/index.php
+++ fls_2019/index.php
@@ -3,7 +3,7 @@ $pageID = get_the_id();
 $pageCF = get_post_custom($pageID);
 get_header();
 $banner_img           = get_field('banner');
-$alternate_page_title = get_field('alternate_page_title', $post->ID);
+$alternate_page_title = get_field('alternate_page_title', 'fls-blog');
 // BANNER
 echo '<section id="banner"'.(empty($banner_img)?' class="no_img"':'').'>'.PHP_EOL;
 echo '<div class="container">'.PHP_EOL;
