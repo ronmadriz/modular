d37c4e6070fb0d103f725646d125f4737feb3e2f
Ron Madriz <ron@firstcreative.com>
2020-05-06 22:23:35 -0500

    blog listing - cta on first page only

--- fls_2019/home.php
+++ fls_2019/home.php
@@ -34,7 +34,7 @@ $wp_query   = $fls_blog;
 // MAIN CONTENT
 if ($fls_blog) {
 	// are we on page one?
-	if (1 == $paged) {
+	if (1 == $fls_blog) {
 		//true
 		echo '<section id="cta_speak" class="blog__cta"><div class="container-fluid"><div class="row"><div class="col-12"><h2 class="text-center text-uppercase"><a href="#cta_blue">Speak with a fall protection specialist <i class="im im-angle-right-circle"></i></a></h2></div></div></div></section>'.PHP_EOL;
 	}
