ddbc69d52f2bc83aba54859353b34d3a761c168e
Ron Madriz <ron@firstcreative.com>
2020-05-07 13:34:20 -0500

    blog listing - fix

--- fls_2019/home.php
+++ fls_2019/home.php
@@ -6,14 +6,11 @@ $page_for_posts       = get_option('page_for_posts');
 $banner_img           = get_field('banner');
 $alternate_page_title = get_field('alternate_page_title', $page_for_posts);
 
-echo '<!-- home -->';
 // BANNER
-echo '<section id="banner"'.(empty($banner_img)?' class="no_img"':'').'>'.PHP_EOL;
+echo '<section id="banner" class="no_img">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
-echo ($banner_img != null?'<div class="row w-image"><style type="text/css">section#banner{background-image:url('.$banner_img['url'].');}</style>':'<div class="row">').PHP_EOL;
-echo '<div class="page_title col-12">'.PHP_EOL;
-echo '<h1>'.(!empty($alternate_page_title)?$alternate_page_title:get_the_title()).'</h1>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
+echo '<div class="row w-image"><style type="text/css">section#banner{background-image:url(/wp-content/uploads/2018/03/drawings-and-specs.jpg);}</style>'.PHP_EOL;
+echo '<div class="page_title col-12"><h1>'.(!empty($alternate_page_title)?$alternate_page_title:get_the_title()).'</h1></div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
