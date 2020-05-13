4ce372b410453b3caf235566710ee50f9a3401da
Ron Madriz <ron@firstcreative.com>
2020-05-13 09:24:35 -0500

    tables added to regular solutions

--- fls_2019/single-solutions.php
+++ fls_2019/single-solutions.php
@@ -87,40 +87,35 @@ if (have_rows('measurement_tables')) {
 		echo '<div class="row">'.PHP_EOL;
 		echo ($m_image != null?'<div class="col-12 col-md-4"><img src="'.$m_image['url'].'" alt="'.$m_image['alt'].'" class="img-fluid"></div>':'');
 		echo '<div class="col-12 col-md-8">'.PHP_EOL;
-		if (have_rows('tables')) {
-			while (have_rows('tables')) {
-				the_row();
-				$m_table = get_sub_field('table');
-				if (!empty($m_table)) {
-					echo '<table border="0">'.PHP_EOL;
-					if (!empty($m_table['caption'])) {
-						echo '<caption>'.$m_table['caption'].'</caption>';
-					}
-					if (!empty($m_table['header'])) {
-						echo '<thead>';
-						echo '<tr>';
-						foreach ($m_table['header'] as $th) {
-							echo '<th scope="col">';
-							echo $th['c'];
-							echo '</th>';
-						}
-						echo '</tr>';
-						echo '</thead>';
-					}
-					echo '<tbody>';
-					foreach ($m_table['body'] as $tr) {
-						echo '<tr>';
-						foreach ($tr as $key => $td) {
-							echo '<td data-label="'.$m_table['header'][$key]['c'].'">';
-							echo $td['c'];
-							echo '</td>';
-						}
-						echo '</tr>';
-					}
-					echo '</tbody>';
-					echo '</table>';
-				}
+		$m_table = get_sub_field('table');
+		if (!empty($m_table)) {
+			echo '<table border="0">'.PHP_EOL;
+			if (!empty($m_table['caption'])) {
+				echo '<caption>'.$m_table['caption'].'</caption>';
 			}
+			if (!empty($m_table['header'])) {
+				echo '<thead>';
+				echo '<tr>';
+				foreach ($m_table['header'] as $th) {
+					echo '<th scope="col">';
+					echo $th['c'];
+					echo '</th>';
+				}
+				echo '</tr>';
+				echo '</thead>';
+			}
+			echo '<tbody>';
+			foreach ($m_table['body'] as $tr) {
+				echo '<tr>';
+				foreach ($tr as $key => $td) {
+					echo '<td data-label="'.$m_table['header'][$key]['c'].'">';
+					echo $td['c'];
+					echo '</td>';
+				}
+				echo '</tr>';
+			}
+			echo '</tbody>';
+			echo '</table>';
 		}
 		echo '</div>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
