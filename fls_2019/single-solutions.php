38263b74b9244d3e20cc85662229c3a9f46c20c8
Ron Madriz <ron@firstcreative.com>
2020-05-14 15:42:27 -0500

    measurement tables caption fix

--- fls_2019/single-solutions.php
+++ fls_2019/single-solutions.php
@@ -75,6 +75,24 @@ if (have_rows('case_study_groups')) {
 	echo '</section>'.PHP_EOL;
 }
 
+// Additional Content
+if (have_rows('additional_content')) {
+	echo '<section id="additional_content" class="additional_content">'.PHP_EOL;
+	echo '<div class="container">'.PHP_EOL;
+	while (have_rows('additional_content')) {
+		the_row();
+		$ac_title     = get_sub_field('title');
+		$ac_content   = get_sub_field('content');
+		$ac_has_title = get_sub_field('has_title');
+		echo ($ac_has_title == 1?'<div class="row"><div class="col-12 section_title"><h2>'.$ac_title.'</h2></div></div>':'').PHP_EOL;
+		echo '<div class="row">'.PHP_EOL;
+		echo '<div class="content col-12">'.$ac_content.'</div>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
+	}
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
+}
+
 // TABLES
 if (have_rows('measurement_tables')) {
 	echo '<section id="measurement_tables">'.PHP_EOL;
@@ -129,25 +147,6 @@ if (have_rows('measurement_tables')) {
 	}
 	echo '</section>'.PHP_EOL;
 }
-
-// Additional Content
-if (have_rows('additional_content')) {
-	echo '<section id="additional_content" class="additional_content">'.PHP_EOL;
-	echo '<div class="container">'.PHP_EOL;
-	while (have_rows('additional_content')) {
-		the_row();
-		$ac_title     = get_sub_field('title');
-		$ac_content   = get_sub_field('content');
-		$ac_has_title = get_sub_field('has_title');
-		echo ($ac_has_title == 1?'<div class="row"><div class="col-12 section_title"><h2>'.$ac_title.'</h2></div></div>':'').PHP_EOL;
-		echo '<div class="row">'.PHP_EOL;
-		echo '<div class="content col-12">'.$ac_content.'</div>'.PHP_EOL;
-		echo '</div>'.PHP_EOL;
-	}
-	echo '</div>'.PHP_EOL;
-	echo '</section>'.PHP_EOL;
-}
-
 // VIDEOS
 $videos = get_field('videos');
 if ($videos):
