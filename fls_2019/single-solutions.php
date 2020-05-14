537f47abed91a3a3bf5bee8ffefe450e6f944749
Ron Madriz <ron@firstcreative.com>
2020-05-14 07:53:53 -0500

    class added additional_content

--- fls_2019/single-solutions.php
+++ fls_2019/single-solutions.php
@@ -132,7 +132,7 @@ if (have_rows('measurement_tables')) {
 
 // Additional Content
 if (have_rows('additional_content')) {
-	echo '<section id="additional_content">'.PHP_EOL;
+	echo '<section id="additional_content" class="additional_content">'.PHP_EOL;
 	echo '<div class="container">'.PHP_EOL;
 	while (have_rows('additional_content')) {
 		the_row();
