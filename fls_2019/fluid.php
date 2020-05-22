60b33a4abe3fba7fa4ea2da448d5a212f16e7214
Ron Madriz <ron@firstcreative.com>
2020-05-22 14:31:26 -0500

    styles overhaul - subnav intro to pages fluid

--- fls_2019/fluid.php
+++ fls_2019/fluid.php
@@ -235,44 +235,46 @@ if (have_rows('download_literature')) {
 	echo '</section>'.PHP_EOL;
 }
 
-// SOLUTIONS
+// Sub Navigation
 
-$solutions_footercs = get_field('solutions_footer');
-if ($solutions_footercs['solutions']) {
-	while (have_rows('solutions_footer', $post_id)):the_row();
-	$solution_footers_title = get_sub_field('title');
-	$solution_footers_text  = get_sub_field('text');
-	echo '<section id="inline_solutions">'.PHP_EOL;
+$subnav = get_field('subnav');
+if (have_rows('subnav')) {
+	echo '<section id="subnav">'.PHP_EOL;
 	echo '<div class="container-fluid">'.PHP_EOL;
-	echo '<div class="row">'.PHP_EOL;
-	echo (!empty($solution_footers_title)?'<div class="section_title col-12"><h2>'.$solution_footers_title.'</h2></div>'.PHP_EOL:'');
-	echo '</div>'.PHP_EOL;
-	$solutions_repeater = get_sub_field('solutions');
-	if ($solutions_repeater) {
-		echo '<div class="row">'.PHP_EOL;
-		while (have_rows('solutions')) {
-			the_row();
-			$solutions_item = get_sub_field('solution');
-			if ($solutions_item):
-			$solution = $solutions_item;
-			setup_postdata($solution);
-			$solution_summary = get_field('summary', $solution->ID);
-			echo '<div class="item col-12 col-md-3">'.PHP_EOL;
-			echo '<a href="'.get_permalink($solution->ID).'">';
-			echo get_the_post_thumbnail($solution->ID, 'full', array('class' => 'img-fluid')).PHP_EOL;
-			echo '</a>'.PHP_EOL;
-			echo '<div class="title"><h3><a href="'.get_permalink($solution->ID).'">'.get_the_title($solution->ID).'</a></h3></div>'.PHP_EOL;
-			echo '<div class="desc"><p><a href="'.get_the_permalink($solution->ID).'">'.$solution_summary.'</a></p></div>'.PHP_EOL;
-			echo '</div>'.PHP_EOL;
-			endif;
-		}
-		wp_reset_query();
+	while (have_rows('subnav')) {
+		the_row();
+		$subnav_title           = get_sub_field('title');
+		$subnav_has_description = get_sub_field('has_description');
+		$subnav_desc            = get_sub_field('desc');
+		$subnav_links           = get_sub_field('links');
+		echo ''.PHP_EOL;
+		echo (!empty($subnav_title)?'<div class="row"><div class="section_title col-12"><h2>'.$subnav_title.'</h2></div>'.PHP_EOL:'');
+		echo ($subnav_has_description == 1?'<div class="row"><div class="content col-12">'.$subnav_desc.'</div></div>'.PHP_EOL:'');
 		echo '</div>'.PHP_EOL;
+		if ($subnav_links) {
+			echo '<div class="row">'.PHP_EOL;
+			while (have_rows('solutions')) {
+				the_row();
+				$subnav_link = get_sub_field('link');
+				if ($subnav_link):
+				$subLink = $subnav_link;
+				setup_postdata($subLink);
+				$subLink_summary = get_field('summary', $subLink->ID);
+				echo '<div class="item col-12 col-md-3">'.PHP_EOL;
+				echo '<a href="'.get_permalink($subLink->ID).'">';
+				echo get_the_post_thumbnail($subLink->ID, 'full', array('class' => 'img-fluid')).PHP_EOL;
+				echo '</a>'.PHP_EOL;
+				echo '<div class="title"><h3><a href="'.get_permalink($subLink->ID).'">'.get_the_title($subLink->ID).'</a></h3></div>'.PHP_EOL;
+				echo '<div class="desc"><p><a href="'.get_the_permalink($subLink->ID).'">'.$subLink_summary.'</a></p></div>'.PHP_EOL;
+				echo '</div>'.PHP_EOL;
+				endif;
+			}
+			wp_reset_query();
+		}
 	}
+	wp_reset_query();
 	echo '</div>'.PHP_EOL;
 	echo '</section>'.PHP_EOL;
-	endwhile;
-	wp_reset_query();
 }
 
 // CASE STUDIES
