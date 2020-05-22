fe05eb8b36ded93f3be5ff4f9095056984ec27e4
Ron Madriz <ron@firstcreative.com>
2020-05-22 14:52:08 -0500

    subnav - fix

--- fls_2019/fluid.php
+++ fls_2019/fluid.php
@@ -202,6 +202,25 @@ if (have_rows('icons')) {
 	echo '</section>'.PHP_EOL;
 }
 
+// Additional Content
+
+if (have_rows('additional_content')) {
+	echo '<section id="additional_content" class="additional_content">'.PHP_EOL;
+	echo '<div class="container-fluid">'.PHP_EOL;
+	while (have_rows('additional_content')) {
+		the_row();
+		$ac_title     = get_sub_field('title');
+		$ac_content   = get_sub_field('content');
+		$ac_has_title = get_sub_field('has_title');
+		echo ($ac_has_title == 1?'<div class="row"><div class="col-12 section_title"><h2>'.$ac_title.'</h2></div></div>':'').PHP_EOL;
+		echo '<div class="row">'.PHP_EOL;
+		echo '<div class="content col-12">'.$ac_content.'</div>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
+	}
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
+}
+
 $download_literature_title = get_field('download_literature_title');
 if (have_rows('download_literature')) {
 	echo '<section id="literature">'.PHP_EOL;
@@ -344,25 +363,6 @@ if (have_rows('case_study_groups')) {
 		}
 	}
 	wp_reset_postdata();
-	echo '</div>'.PHP_EOL;
-	echo '</section>'.PHP_EOL;
-}
-
-// Additional Content
-
-if (have_rows('additional_content')) {
-	echo '<section id="additional_content" class="additional_content">'.PHP_EOL;
-	echo '<div class="container-fluid">'.PHP_EOL;
-	while (have_rows('additional_content')) {
-		the_row();
-		$ac_title     = get_sub_field('title');
-		$ac_content   = get_sub_field('content');
-		$ac_has_title = get_sub_field('has_title');
-		echo ($ac_has_title == 1?'<div class="row"><div class="col-12 section_title"><h2>'.$ac_title.'</h2></div></div>':'').PHP_EOL;
-		echo '<div class="row">'.PHP_EOL;
-		echo '<div class="content col-12">'.$ac_content.'</div>'.PHP_EOL;
-		echo '</div>'.PHP_EOL;
-	}
 	echo '</div>'.PHP_EOL;
 	echo '</section>'.PHP_EOL;
 }
