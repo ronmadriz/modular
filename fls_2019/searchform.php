d507b062db2093ca8d43c1fdf60b187074a9086c
Ron Madriz <ron@firstcreative.com>
2020-07-21 15:25:39 -0500

    button test for search function in firefox

--- fls_2019/searchform.php
+++ fls_2019/searchform.php
@@ -4,7 +4,8 @@ bloginfo('url');
 echo '">'.PHP_EOL;
 echo '<label class="sr-only" for="s">Search for:</label>'.PHP_EOL;
 echo '<input type="text" value="" name="s" id="s" placeholder="Search...">'.PHP_EOL;
-echo '<button type="submit" id="searchsubmit"><i class="im im-magnifier"></i><span class="sr-only">Search</span></button>'.PHP_EOL;
+// echo '<button type="submit" id="searchsubmit"><i class="im im-magnifier"></i><span class="sr-only">Search</span></button>'.PHP_EOL;
+echo '<input type="submit" id="searchsubmit" value="Search">'.PHP_EOL;
 echo (!is_404()?'<button type="button" id="searchClose"><i class="im im-x-mark"></i></button>'.PHP_EOL:'');
 echo '</form>'.PHP_EOL;
 ?>
