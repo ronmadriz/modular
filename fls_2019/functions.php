4f01c3da003357d9edfbaebd4894dbdcf10bbbcd
Ron Madriz <ron@firstcreative.com>
2020-07-27 15:17:10 -0500

    organizing files in MVC structure

--- fls_2019/functions.php
+++ fls_2019/functions.php
@@ -93,15 +93,8 @@ function category_id_class($classes) {
 add_filter('post_class', 'category_id_class');
 add_filter('body_class', 'category_id_class');
 
-add_action('after_setup_theme', 'register_my_menu');
-function register_my_menu() {
-	register_nav_menu('new_menu', __('new_menu', 'new_menu'));
-	register_nav_menu('solution', __('solution-pages', 'solution-pages'));
-	register_nav_menu('industry', __('industry-pages', 'industry-pages'));
-	register_nav_menu('primary', __('company-pages', 'company-pages'));
-	register_nav_menu('sidenav', __('Side Navigation'));
-	register_nav_menu('footer__solution', __('footer__solution', 'footer__solution'));
-}
+require_once ('functions/menus.php');
+
 function my_mce_buttons_2($buttons) {
 	$buttons[] = 'sup';
 	$buttons[] = 'sub';
