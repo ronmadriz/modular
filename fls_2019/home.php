1fb78c8a02e3a658590e11ff6860a268ee790acc
Ron Madriz <ron@firstcreative.com>
2020-07-27 14:44:18 -0500

    organizing files in MVC structure

--- fls_2019/home.php
+++ fls_2019/home.php
@@ -5,7 +5,6 @@ include 'header-fluid.php';
 $page_for_posts       = get_option('page_for_posts');
 $banner_img           = get_field('banner');
 $alternate_page_title = get_field('alternate_page_title', $page_for_posts);
-
 // BANNER
 echo '<section id="banner" class="no_img">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
@@ -14,30 +13,22 @@ echo '<div class="page_title col-12"><h1>'.(!empty($alternate_page_title)?$alter
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
-
 the_breadcrumb();
-
-include 'views/blogs/featured.php';
-
+include 'views/components/blogs/featured.php';
 echo '<div id="pagewrapper" class="container">'.PHP_EOL;
 echo '<div class="row">'.PHP_EOL;
 echo '<div id="columns_2" class="col-12 col-md-9">'.PHP_EOL;
-
-include 'views/blogs/list.php';
-
+include 'views/components/blogs/list.php';
 // Sidebar
 echo '</div>'.PHP_EOL;
-
 echo '<aside class="sidebar d-none d-sm-block col-md-3">'.PHP_EOL;
 $secondary_logo = get_field('secondary_logo');
 echo (!empty($secondary_logo)?'<div id="sidebar_logo" class="d-block text-center"><img src="'.$secondary_logo['url'].'" alt="'.$secondary_logo['alt'].'"></div>':'<img src="'.get_bloginfo('url').'/wp-content/uploads/2019/02/fls-works.svg" alt="Fall Safety Protection - Works">').PHP_EOL;
 // latest Posts
 // include 'views/blogs/sidebar.php';
 echo '</aside>'.PHP_EOL;
-
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
-
 echo '<section id="cta_blue"><h2 class="text-md-center">Tell Us About Your Fall Hazard</h2></div></section>'.PHP_EOL;
 echo '<section id="contactFormCTA">'.PHP_EOL;
 echo '<div class="container-fluid">'.PHP_EOL;
