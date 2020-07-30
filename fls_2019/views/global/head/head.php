6f2a614f0ff5d5235487836682902c886f732192
Ron Madriz <ron@firstcreative.com>
2020-07-30 15:16:25 -0500

    topbar - email

--- fls_2019/views/global/head/head.php
+++ fls_2019/views/global/head/head.php
@@ -9,6 +9,7 @@ echo '>'.PHP_EOL;
 echo $ronmadriz_settings['ronmadriz_body_extra'];
 include (get_template_directory().'/views/global/head/topbar.php');
 echo '<nav class="menus">'.PHP_EOL;
+echo '<div class="wrapper">'.PHP_EOL;
 if (!empty($site_logo_header)) {
 	echo '<a class="menus__brand" href="'.get_bloginfo('url').'"><img alt="'.get_bloginfo('name').'" class="menus__brand--img img-fluid" src="'.esc_url($site_logo_header).'"></a>'.PHP_EOL;
 } else {
@@ -19,6 +20,7 @@ echo '<div class="menus__toggle">'.PHP_EOL;
 echo '<span class="menus__toggle--item"></span>'.PHP_EOL;
 echo '<span class="menus__toggle--item"></span>'.PHP_EOL;
 echo '<span class="menus__toggle--item"></span>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</nav>'.PHP_EOL;
 echo '<main>'.PHP_EOL;
