769032fd4b080fb3ec5af0a6950cf59924bef975
Ron Madriz <ron@firstcreative.com>
2020-07-30 12:11:49 -0500

    functions - modularization

--- fls_2019/functions/includes.php
+++ fls_2019/functions/includes.php
@@ -1,6 +1,6 @@
 <?php
-include (get_stylesheet_directory().'/includes/core/extended-cpts.php');
-include (get_stylesheet_directory().'/includes/core/extended-taxos.php');
-foreach (glob(get_stylesheet_directory().'/includes/cpt_files/*.php') as $filename) {include $filename;}
-foreach (glob(get_stylesheet_directory().'/includes/customizers/*.php') as $customizers) {include $customizers;}
-require_once (get_stylesheet_directory().'/includes/options/default.php');
+include (get_template_directory().'/includes/core/extended-cpts.php');
+include (get_template_directory().'/includes/core/extended-taxos.php');
+foreach (glob(get_template_directory().'/includes/cpt_files/*.php') as $filename) {include $filename;}
+foreach (glob(get_template_directory().'/includes/customizers/*.php') as $customizers) {include $customizers;}
+require_once (get_template_directory().'/includes/options/default.php');
