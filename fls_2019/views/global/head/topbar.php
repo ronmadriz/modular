f7030fb7709d04994adeb916cd4c75d3ec773d83
Ron Madriz <ron@firstcreative.com>
2020-07-30 13:09:02 -0500

    social icons optimized

--- fls_2019/views/global/head/topbar.php
+++ fls_2019/views/global/head/topbar.php
@@ -6,7 +6,7 @@ echo '<section id="topbar" class="topbar">'.PHP_EOL;
 echo '<div class="wrapper">'.PHP_EOL;
 echo '<span class="topbar__email"><a class="topbar__email--link" href="mailto:'.$topbar_email.'?Subject=Information about Flexible Lifeline Safety">'.$topbar_email.'</a></span>'.PHP_EOL;
 echo '<span class="topbar__phone"><a class="topbar__phone--link" href="tel:+1'.$topbar_phoneND.'" rel="noopener noreferrer">'.$topbar_phone.'</a></span>'.PHP_EOL;
-echo '<span class="topbar__social">'.PHP_EOL;
+echo '<span class="topbar__social social">'.PHP_EOL;
 social_media_icons();
 echo '</span>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
