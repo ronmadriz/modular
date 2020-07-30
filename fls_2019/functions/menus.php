87bbcbf02f6cb19992a2f28168ecf5ff94ceae9e
Ron Madriz <ron@firstcreative.com>
2020-07-29 19:27:48 -0500

    submenu - class

--- fls_2019/functions/menus.php
+++ fls_2019/functions/menus.php
@@ -10,8 +10,26 @@ function register_my_menu() {
 	register_nav_menu('footer__solution', __('footer__solution', 'footer__solution'));
 }
 
+// add custom class to submenu
 function menu__sub__class($classes) {
 	$classes[] = 'menu__sub';
 	return $classes;
 }
 add_filter('nav_menu_submenu_css_class', 'menu__sub__class');
+
+// find level in menu
+add_filter('wp_nav_menu_objects', 'tier__finder');
+function tier__finder($menu) {
+	$level = 0;
+	$stack = array('0');
+	foreach ($menu as $key => $item) {
+		while ($item->menu_item_parent != array_pop($stack)) {
+			$level--;
+		}
+		$level++;
+		$stack[]               = $item->menu_item_parent;
+		$stack[]               = $item->ID;
+		$menu[$key]->classes[] = 'tier__'.($level-1);
+	}
+	return $menu;
+}
