e52af4dfde60078bcb1669ecec90ffff338613a8
Ron Madriz <ron@firstcreative.com>
2020-07-30 10:33:30 -0500

    nav rebuild - topbar

--- fls_2019/views/global/head/head.php
+++ fls_2019/views/global/head/head.php
@@ -14,20 +14,20 @@ echo '>'.PHP_EOL;
 echo $ronmadriz_settings['ronmadriz_body_extra'];
 include (get_template_directory().'/views/global/head/topbar.php');
 echo '<nav class="menus">'.PHP_EOL;
-if (!empty($site_logo_header)):
-echo '<a href="'.get_bloginfo('url').'" class="menus__brand"><img src="'.esc_url($site_logo_header).'" alt="';
-bloginfo('name');
-echo '" class="menus__brand--img img-fluid"></a>'.PHP_EOL;
- else :
-echo '<a href="'.get_bloginfo('url').'" class="menus__brand">'.get_bloginfo('name');
-echo (!empty($slogan))?'<br><small>'.$slogan.'</small>':'';
-echo '</a>'.PHP_EOL;
-endif;
+if (!empty($site_logo_header)) {
+	echo '<a href="'.get_bloginfo('url').'" class="menus__brand"><img src="'.esc_url($site_logo_header).'" alt="';
+	bloginfo('name');
+	echo '" class="menus__brand--img img-fluid"></a>'.PHP_EOL;
+} else {
+	echo '<a href="'.get_bloginfo('url').'" class="menus__brand">'.get_bloginfo('name');
+	echo (!empty($slogan))?'<br><small>'.$slogan.'</small>':'';
+	echo '</a>'.PHP_EOL;
+}
+include (get_template_directory().'/views/components/navigation/mega.php');
 echo '<div class="menus__toggler">'.PHP_EOL;
 echo '<span class="menus__toggler--item"></span>'.PHP_EOL;
 echo '<span class="menus__toggler--item"></span>'.PHP_EOL;
 echo '<span class="menus__toggler--item"></span>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
-include (get_template_directory().'/views/components/navigation/mega.php');
 echo '</nav>'.PHP_EOL;
 echo '<main>'.PHP_EOL;
