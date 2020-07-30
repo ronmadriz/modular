c9c75772461383cb05659877d7d830ed6e30ab5c
Ron Madriz <ron@firstcreative.com>
2020-07-30 15:09:53 -0500

    topbar - email

--- fls_2019/views/global/head/head.php
+++ fls_2019/views/global/head/head.php
@@ -15,10 +15,10 @@ if (!empty($site_logo_header)) {
 	echo '<a class="menus__brand" href="'.get_bloginfo('url').'" title="'.get_bloginfo('name').'">'.get_bloginfo('name').'</a>'.PHP_EOL;
 }
 include (get_template_directory().'/views/components/navigation/mega.php');
-echo '<div class="menus__toggler">'.PHP_EOL;
-echo '<span class="menus__toggler--item"></span>'.PHP_EOL;
-echo '<span class="menus__toggler--item"></span>'.PHP_EOL;
-echo '<span class="menus__toggler--item"></span>'.PHP_EOL;
+echo '<div class="menus__toggle">'.PHP_EOL;
+echo '<span class="menus__toggle--item"></span>'.PHP_EOL;
+echo '<span class="menus__toggle--item"></span>'.PHP_EOL;
+echo '<span class="menus__toggle--item"></span>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</nav>'.PHP_EOL;
 echo '<main>'.PHP_EOL;
