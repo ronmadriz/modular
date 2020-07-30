2b2791050cb9346d55ff32554b9f575e8bc47239
Ron Madriz <ron@firstcreative.com>
2020-07-30 12:09:39 -0500

    functions - modularization

--- fls_2019/functions/menus.php
+++ fls_2019/functions/menus.php
@@ -2,10 +2,10 @@
 add_action('after_setup_theme', 'register_my_menu');
 function register_my_menu() {
 	register_nav_menu('new_menu', __('new_menu', 'new_menu'));
-	register_nav_menu('mega', __('mega', 'mega'));
 	register_nav_menu('solution', __('solution-pages', 'solution-pages'));
 	register_nav_menu('industry', __('industry-pages', 'industry-pages'));
 	register_nav_menu('primary', __('company-pages', 'company-pages'));
 	register_nav_menu('sidenav', __('Side Navigation'));
 	register_nav_menu('footer__solution', __('footer__solution', 'footer__solution'));
+	register_nav_menu('mega', __('mega', 'mega'));
 }
