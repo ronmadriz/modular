7b3bc53e5725ccb1fba5b3446a1ea58ef92a6f95
Ron Madriz <ron@firstcreative.com>
2020-08-03 12:05:56 -0500

    navigation - mobile first

--- fls_2019/functions/Modular_Walker.php
+++ fls_2019/functions/Modular_Walker.php
@@ -26,7 +26,23 @@ class Modular_Walker extends Walker_Nav_Menu {
 
 // add custom class to submenu
 function menus__sub__class($classes) {
-	$classes[] = 'menus__sub';
-	return $classes;
+
+	$parents = array();
+
+	// Collect menu items with parents.
+	foreach ($items as $item) {
+		if ($item->menu_item_parent && $item->menu_item_parent > 0) {
+			$parents[] = $item->menu_item_parent;
+		}
+	}
+
+	// Add class.
+	foreach ($items as $item) {
+		if (in_array($item->ID, $parents)) {
+			$item->classes[] = 'menus__sub';
+		}
+	}
+	return $items;
+
 }
 add_filter('nav_menu_submenu_css_class', 'menus__sub__class');
