e1c167f6815d1f11a577aefbcb5d2766c2942257
Ron Madriz <ron@firstcreative.com>
2020-08-03 11:58:15 -0500

    navigation - mobile first

--- fls_2019/functions/Modular_Walker.php
+++ fls_2019/functions/Modular_Walker.php
@@ -24,7 +24,7 @@ class Modular_Walker extends Walker_Nav_Menu {
 	}
 	function start_lvl(&$output, $depth = 0, $args = array()) {
 		if ($depth == 2) {
-			$output .= '<ul class="tertiary"'.PHP_EOL;
+			$output .= '<ul class="tertiary">';
 		}
 	}
 
