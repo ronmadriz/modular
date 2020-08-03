22d4ae2b2f4747dba58a41ad8f3390c02ff97ac3
Ron Madriz <ron@firstcreative.com>
2020-08-03 13:49:54 -0500

    navigation - mobile first

--- fls_2019/dist/scripts/main.js
+++ fls_2019/dist/scripts/main.js
@@ -341,3 +341,45 @@ const navSlide = () => {
   });
 }
 navSlide();
+const toggle = document.querySelector(".menus__toggle");
+const menu = document.querySelector(".menus__list");
+const items = document.querySelectorAll(".menus__item");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  if (menu.classList.contains("menus__active")) {
+    menu.classList.remove("menus__active");
+  } else {
+    menu.classList.add("menus__active");
+  }
+}
+
+/* Activate Submenu */
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+}
+
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+  let isClickInside = menu.contains(e.target);
+
+  if (!isClickInside && menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  }
+}
+/* Event Listeners */
+toggle.addEventListener("click", toggleMenu, false);
+for (let item of items) {
+  if (item.querySelector(".submenu")) {
+    item.addEventListener("click", toggleItem, false);
+  }
+  item.addEventListener("keypress", toggleItem, false);
+}
+document.addEventListener("click", closeSubmenu, false);
