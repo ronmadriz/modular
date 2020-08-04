7ee939e8207a0e6e4edd175195a67d45c6fb14dd
Ron Madriz <ron@firstcreative.com>
2020-08-04 11:33:23 -0500

    footer - structure - social

---
+++ fls_2019/views/components/navigation/mega-boiler.php
@@ -0,0 +1,20 @@
+<?php
+$mega_args = array(
+	'theme_location'  => 'mega_boiler',
+	'menu'            => 'mega_boiler',
+	'container'       => '',
+	'container_class' => '',
+	'container_id'    => '',
+	'echo'            => true,
+	'menu_class'      => 'footer__boiler',
+	'container'       => false,
+	'container'       => false,
+	'before'          => '',
+	'after'           => '',
+	'link_before'     => '',
+	'link_after'      => '',
+	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+	'walker'          => new Modular_Walker(),
+	'depth'           => 3,
+);
+wp_nav_menu($mega_args);
