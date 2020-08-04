7aa1bace92223d7452f747167dbcb46c4a4c2b35
Ron Madriz <ron@firstcreative.com>
2020-08-04 15:19:28 -0500

    footer - structure - complete

--- fls_2019/views/global/foot/foot.php
+++ fls_2019/views/global/foot/foot.php
@@ -36,7 +36,7 @@ include (get_template_directory().'/views/components/navigation/mega-boiler.php'
 echo '</span>'.PHP_EOL;
 echo '<span class="footer__boiler--info"><em class="footer__boiler--link footer__boiler--copyright">';
 _e('&copy; ', 'fc_core');
-echo date('Y').get_bloginfo('name').'</em> <em class="footer__boiler--link footer__boiler--rights">';
+echo date('Y').' '.get_bloginfo('name').'</em> <em class="footer__boiler--link footer__boiler--rights">';
 _e('All Rights Reserved', 'fc_core');
 echo '<a href="'.$privacy_policy.'" class="footer__boiler--link footer__boiler--privacy">';
 _e('Privacy Policy', 'fc_core');
