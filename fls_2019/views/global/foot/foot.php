ad566b795ebc96809293405a8fba461c81f255bf
Ron Madriz <ron@firstcreative.com>
2020-08-04 15:20:25 -0500

    footer - structure - complete

--- fls_2019/views/global/foot/foot.php
+++ fls_2019/views/global/foot/foot.php
@@ -38,7 +38,7 @@ echo '<span class="footer__boiler--info"><em class="footer__boiler--link footer_
 _e('&copy; ', 'fc_core');
 echo date('Y').' '.get_bloginfo('name').'</em> <em class="footer__boiler--link footer__boiler--rights">';
 _e('All Rights Reserved', 'fc_core');
-echo '<a href="'.$privacy_policy.'" class="footer__boiler--link footer__boiler--privacy">';
+echo '</em> <a href="'.$privacy_policy.'" class="footer__boiler--link footer__boiler--privacy">';
 _e('Privacy Policy', 'fc_core');
 echo '</a></span>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
