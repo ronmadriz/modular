5509b732dc5d542b59ead7f2c490392bd3c73e6b
Ron Madriz <ron@firstcreative.com>
2020-08-04 15:09:32 -0500

    footer - structure - complete

--- fls_2019/functions/social.php
+++ fls_2019/functions/social.php
@@ -58,3 +58,27 @@ function social_media_footer() {
 		}
 	}
 }
+
+// old
+
+function social_media_icons() {
+	$social_sites = ronmadriz_social_media_array();
+	foreach ($social_sites as $social_site) {
+		if (strlen(get_theme_mod($social_site)) > 0) {
+			$active_sites[] = $social_site;
+		}
+	}
+	if (!empty($active_sites)) {
+		echo '<ul class="social-media-icons list-inline">';
+		foreach ($active_sites as $active_site) {
+			$class = 'fab fa-'.$active_site;
+			if ($active_site == 'email') {
+			} else {
+				echo '<li class="list-inline-item"><a class="'.$active_site.' ?>" target="_blank" href="'.get_theme_mod($active_site).'" rel="noopener" rel="noreferrer"> <i class="'.esc_attr($class).'" title="';
+				printf(__('%s icon', 'text-domain'), $active_site);
+				echo '"></i> </a> </li>';
+			}
+		}
+		echo "</ul>";
+	}
+}
