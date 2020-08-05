ac7eb54d5e9632467ed90accb5ec0e3dbff18a19
Ron Madriz <ron@firstcreative.com>
2020-08-05 13:50:39 -0500

    header & footer cleanup

--- fls_2019/views/global/foot/columns.php
+++ fls_2019/views/global/foot/columns.php
@@ -4,8 +4,7 @@ $phone   = get_theme_mod('footer_phone');
 $fax     = get_theme_mod('footer_fax');
 $email   = get_theme_mod('footer_email');
 
-echo '<div class="footer__columns">'.PHP_EOL;
-echo '<span class="footer__column footer__menus">'.PHP_EOL;
+echo '<div class="footer__menus">'.PHP_EOL;
 echo '<ul id="footer__menu" class="footer__menu">'.PHP_EOL;
 include (get_template_directory().'/views/components/navigation/mega-foot.php');
 echo '<li class="footer__item footer__item--parent">'.PHP_EOL;
@@ -16,7 +15,6 @@ echo '<ul class="footer__sub">'.PHP_EOL;
 echo '<li class="footer__item"><address>'.$address.'</address></li>'.PHP_EOL;
 echo '<li class="footer__item"><strong>Phone:</strong> '.$fax.'</li><li class="footer__item"><strong>Fax:</strong> '.$fax.'</li><li class="footer__item"><strong>Email:</strong> <a class="footer__email" href="mailto:'.$email.'?Subject=Information Email from Website Footer">'.$email.'</a></li>'.PHP_EOL;
 echo '</ul></li></ul>'.PHP_EOL;
-echo '</span>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 
 /*
