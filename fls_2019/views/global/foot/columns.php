7848e8dd65bd73c94b0142002bf4f914be1bc63c
Ron Madriz <ron@firstcreative.com>
2020-08-05 13:12:38 -0500

    header & footer cleanup

--- fls_2019/views/global/foot/columns.php
+++ fls_2019/views/global/foot/columns.php
@@ -6,33 +6,13 @@ $email   = get_theme_mod('footer_email');
 
 echo '<div class="footer__columns">'.PHP_EOL;
 echo '<span class="footer__column footer__menus">'.PHP_EOL;
-$mega_args = array(
-	'theme_location'  => 'mega_footer',
-	'menu'            => 'mega_footer',
-	'container'       => '',
-	'container_class' => '',
-	'container_id'    => '',
-	'echo'            => true,
-	'menu_class'      => 'footer__menu',
-	'container'       => false,
-	'container'       => false,
-	'before'          => '',
-	'after'           => '',
-	'link_before'     => '',
-	'link_after'      => '',
-	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-	'walker'          => new Footer_Walker(),
-	'depth'           => 2,
-);
-wp_nav_menu($mega_args);
-echo '<ul id="hqinfo" class="footer__menu">'.PHP_EOL;
-echo '<li class="footer__item footer__item--parent">'.PHP_EOL;
-echo '<span class="footer__link">';
-_e('FLS Headquarters', 'fls_core');
+include (get_template_directory().'/views/components/navigation/mega-foot.php');
 echo '</span>'.PHP_EOL;
-echo '<ul class="footer__sub">'.PHP_EOL;
-echo '<li class="footer__item"><address>'.$address.'</address>'.PHP_EOL;
-echo '<li class="footer__item"><strong>Phone:</strong> '.$fax.'</li><li class="footer__item"><strong>Fax:</strong> '.$fax.'</li><li class="footer__item"><strong>Email:</strong> <a class="footer__email" href="mailto:'.$email.'?Subject=Information Email from Website Footer">'.$email.'</a></li>'.PHP_EOL;
-echo '</ul></li>'.PHP_EOL;
-echo '</ul></span>'.PHP_EOL;
+echo '<span class="footer__column footer__info">'.PHP_EOL;
+echo '<h4 class="footer__title">';
+_e('FLS Headquarters', 'fls_core');
+'</h4>'.PHP_EOL;
+echo '<address>'.$address.'</address>'.PHP_EOL;
+echo '<p><strong>Phone:</strong> '.$fax.'<br><strong>Fax:</strong> '.$fax.'<br><strong>Email:</strong> <a class="footer__email" href="mailto:'.$email.'?Subject=Information Email from Website Footer">'.$email.'</a></p>'.PHP_EOL;
+echo '</span>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
