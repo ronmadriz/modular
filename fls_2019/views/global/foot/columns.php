816113fbeb057648e8f778b0df4c032b5ad090dc
Ron Madriz <ron@firstcreative.com>
2020-08-05 13:45:10 -0500

    header & footer cleanup

--- fls_2019/views/global/foot/columns.php
+++ fls_2019/views/global/foot/columns.php
@@ -8,7 +8,6 @@ echo '<div class="footer__columns">'.PHP_EOL;
 echo '<span class="footer__column footer__menus">'.PHP_EOL;
 echo '<ul id="footer__menu" class="footer__menu">'.PHP_EOL;
 include (get_template_directory().'/views/components/navigation/mega-foot.php');
-echo '</span>'.PHP_EOL;
 echo '<li class="footer__item footer__item--parent">'.PHP_EOL;
 echo '<span class="footer__link">';
 _e('FLS Headquarters', 'fls_core');
