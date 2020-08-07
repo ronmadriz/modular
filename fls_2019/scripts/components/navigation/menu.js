a03e3c3439556334d34662385edf00fc2ef9f20d
Ron Madriz <ron@firstcreative.com>
2020-08-06 22:48:46 -0500

    add current class to nav item

--- fls_2019/scripts/components/navigation/menu.js
+++ fls_2019/scripts/components/navigation/menu.js
@@ -4,11 +4,4 @@ j(document).ready(function(){
 	j('.menus__toggle').click(function(){
 		j('.menus__list').toggleClass('menus__list--active');
 	});
-
-	j('.menus__sub').parent().addClass('menus__dropdown');
-	j('.menus__sub').addClass('dropdown-menu');
-	j('.menus__list li.menus__dropdown a').addClass('menus__dropdown--toggle');
-	j('.menus__sub li a').removeClass('menus__dropdown--toggle'); 
-	j('.menus__list .dropdown-toggle').append('<b class="caret"></b>');
-	j('a.dropdown-toggle').attr('data-toggle', 'dropdown');	
 });
