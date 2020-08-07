2812c3437512edde48958ee80caf51a36ec9133a
Ron Madriz <ron@firstcreative.com>
2020-08-07 18:24:08 -0500

    footer form fix

--- fls_2019/single-case_study.php
+++ fls_2019/single-case_study.php
@@ -3,6 +3,8 @@
 Template Name: Case Study
 Template Post Type: case_study
  */
+$pageID = get_the_id();
+$pageCF = get_post_custom($pageID);
 get_header();
 include (get_template_directory().'/views/components/banner/default.php');
 include (get_template_directory().'/views/components/content/default.php');
--- fls_2019/views/components/gallery/default.php
+++ fls_2019/views/components/gallery/default.php
@@ -1,14 +1,13 @@
 <?
 // GALLERY
-$post_ID           = get_the_ID();
-$solutions_gallery = get_field('solutions_gallery', $post_ID);
+$solutions_gallery = get_field('solutions_gallery', $pageID);
 if ($solutions_gallery['gallery_pics']) {
 	echo '<section class="galleries" id="galleries">'.PHP_EOL;
 	echo '<div class="wrapper">'.PHP_EOL;
 	echo '<div class="galleries__content">'.PHP_EOL;
 	while (have_rows('solutions_gallery')) {
 		the_row();
-		$gallery_pics = get_sub_field('gallery_pics', $post_ID);
+		$gallery_pics = get_sub_field('gallery_pics', $pageID);
 		if ($gallery_pics) {
 			echo '<ul class="galleries__list list-unstyled" id="lightSlider">'.PHP_EOL;
 			foreach ($gallery_pics as $gallery_pic) {
