98b38ff4cf3dd8e0363d0e2df1dfe73590aa4823
Ron Madriz <ron@firstcreative.com>
2020-08-06 22:24:24 -0500

    navigation - scripting

--- fls_2019/scripts/components/navigation/menu.js
+++ fls_2019/scripts/components/navigation/menu.js
@@ -1,16 +1,15 @@
 var j = jQuery.noConflict();
 j(document).ready(function(){
-  // toggle nav
-  j('.menus__toggle').click(function(){
-    j('.menus__list').toggleClass('menus__list--active');
-  });
-});
-
-j(function(){
-	var children=j('.menus__list li a').filter(function(){return j(this).nextAll().length>0})
-	j('<span class="menus__caret">+</span>').insertAfter(children)
-	j('.menus__list .menus__caret').click(function (e) {
-		j(this).next().slideToggle(300);
-		  return false;
+	// toggle nav
+	j('.menus__toggle').click(function(){
+		j('.menus__list').toggleClass('menus__list--active');
 	});
-})
+	j(function(){
+		var children=j('.menus__list li a').filter(function(){return j(this).nextAll().length>0})
+		j('<span class="menus__caret">+</span>').insertAfter(children)
+		j('.menus__list .menus__caret').click(function (e) {
+			j(this).next().slideToggle(300);
+			  return false;
+		});
+	})
+});
