8b00747f65b9c0f67e422063b2de577978e7510c
Ron Madriz <ron@firstcreative.com>
2020-08-06 22:50:23 -0500

    add current class to nav item

--- fls_2019/scripts/components/navigation/menu.js
+++ fls_2019/scripts/components/navigation/menu.js
@@ -4,4 +4,8 @@ j(document).ready(function(){
 	j('.menus__toggle').click(function(){
 		j('.menus__list').toggleClass('menus__list--active');
 	});
+
 });
+j(document).on('click', '.menus__link', function(event) {
+	console.log('clicked');
+}
