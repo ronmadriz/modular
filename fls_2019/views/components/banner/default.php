1039e2b917749e45e2ec08ebb8287923d07e96ce
Ron Madriz <ron@firstcreative.com>
2020-08-07 18:00:26 -0500

    footer form fix

--- fls_2019/views/components/banner/default.php
+++ fls_2019/views/components/banner/default.php
@@ -7,7 +7,7 @@ echo '<section id="banner"'.(empty($banner_img)?' class="no_img"':'').'>'.PHP_EO
 echo '<div class="container">'.PHP_EOL;
 echo ($banner_img != null?'<div class="row w-image justify-content-center align-content-center"><style type="text/css">section#banner{background-image:url('.$banner_img['url'].');}</style>':'<div class="row justify-content-center align-content-center">').PHP_EOL;
 echo '<div class="page_title col-12 col-md-10">'.PHP_EOL;
-echo '<h1>'(!empty($alternate_page_title)?$alternate_page_title:$default_Pagetitle).'</h1>'.PHP_EOL;
+echo '<h1>'.(!empty($alternate_page_title)?$alternate_page_title:$default_Pagetitle).'</h1>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
