c7e73661044e75dc2aa6fceb40ebaef237e83f73
Ron Madriz <ron@firstcreative.com>
2020-08-07 21:00:03 -0500

    hero fix

--- fls_2019/views/components/banner/default.php
+++ fls_2019/views/components/banner/default.php
@@ -4,7 +4,7 @@ $banner_img           = get_field('banner');
 $default_Pagetitle    = get_the_title();
 // BANNER
 echo ($banner_img != null?'<style type="text/css">.hero{background-image:url('.$banner_img['url'].');}</style>':'').PHP_EOL;
-echo '<section id="banner" class="hero'.(!empty($banner_img)?' has_bg':'').'">'.PHP_EOL;
+echo '<section id="hero" class="hero'.(!empty($banner_img)?' has_bg':'').'">'.PHP_EOL;
 echo '<div class="wrapper">'.PHP_EOL;
 echo '<span class="page_title"><h1>'.(!empty($alternate_page_title)?$alternate_page_title:$default_Pagetitle).'</h1></span>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
