f46229dc99a1985698f9d07abb05e9fc0622f534
Ron Madriz <ron@firstcreative.com>
2020-08-10 14:35:35 -0500

    literature - list item fix

--- fls_2019/views/components/navigation/solutions.php
+++ fls_2019/views/components/navigation/solutions.php
@@ -9,10 +9,11 @@ if (have_rows('affiliated_solutions')) {
 	while (have_rows('affiliated_solutions')) {
 		the_row();
 		$as_solution = get_sub_field('as_solution');
+		$as_summary  = get_field('summary', $as_solution->ID);
 		echo '<figure class="solutions__item">'.PHP_EOL;
 		echo '<a class="solutions__link" href="'.get_permalink($as_solution->ID).'">'.get_the_post_thumbnail($as_solution->ID, 'full', array('class' => 'solutions__image img-fluid')).'</a>'.PHP_EOL;
 		echo '<figcaption class="solutions__content"><h3 class="solutions__content--title"><a class="solutions__link" href="'.get_permalink($as_solution->ID).'">'.get_the_title($as_solution->ID).'</a></h3></div>'.PHP_EOL;
-		echo '<span class="solutions__content--desc"><a class="solutions__link" href="'.get_the_permalink($as_solution->ID).'">'.$subLink_summary.'</a></span></figcaption>'.PHP_EOL;
+		echo '<span class="solutions__content--desc"><a class="solutions__link" href="'.get_the_permalink($as_solution->ID).'">'.$as_summary.'</a></span></figcaption>'.PHP_EOL;
 		echo '</figure>'.PHP_EOL;
 	}
 	echo '</div>'.PHP_EOL;
