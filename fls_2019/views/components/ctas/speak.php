7e2ad880d8d1788b4867344889921f394abc73ad
Ron Madriz <ron@firstcreative.com>
2020-08-10 11:03:59 -0500

    hero holder

--- fls_2019/views/components/ctas/speak.php
+++ fls_2019/views/components/ctas/speak.php
@@ -1,3 +1,3 @@
 <?
 // CTA Speak
-echo (!empty(get_the_content())?'<section class="speak" id="speak"><div class="wrapper"><div class="speak__title"><span class="speak__title--text"><a class="speak__link" href="#cta_blue">Speak with a fall protection specialist <i class="im im-angle-right-circle"></i></a></span></div></div></section>'.PHP_EOL:'');
+echo (!empty(get_the_content())?'<section class="speak" id="speak"><div class="wrapper"><div class="speak__title"><span class="speak__title--text"><a class="speak__link" href="#cta_blue">Speak with a fall protection specialist <i class="speak__icon im im-angle-right-circle"></i></a></span></div></div></section>'.PHP_EOL:'');
