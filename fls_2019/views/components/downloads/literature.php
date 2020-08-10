d0294bd0615213fd0f340dd90ce7c9de65b5e37e
Ron Madriz <ron@firstcreative.com>
2020-08-10 12:40:11 -0500

    section title fix

--- fls_2019/views/components/downloads/literature.php
+++ fls_2019/views/components/downloads/literature.php
@@ -6,24 +6,24 @@ if (have_rows('download_literature')) {
 	echo '<div class="wrapper">'.PHP_EOL;
 	echo '<span class="literature__content section__title col-12"><h2>'.(!empty($download_literature_title)?$download_literature_title:'download literature').'</h2></span>'.PHP_EOL;
 	echo '<span class="literature__content">'.PHP_EOL;
-	echo '<ul class="literature__list">'.PHP_EOL;
+	echo '<div class="literature__list">'.PHP_EOL;
 	while (have_rows('download_literature')) {
 		the_row();
 		$lit_file  = get_sub_field('file');
 		$lit_img   = get_sub_field('Image');
 		$lit_title = get_sub_field('title');
-		echo '<li class="literature__item">';
+		echo '<figure class="literature__item">';
 		echo ($lit_file != null?'<a class="literature__link" href="'.$lit_file['url'].'">':'');
 		echo ($lit_img != null?'<img class="literature__img img-fluid" src="'.$lit_img['url'].'" alt="'.$lit_img['alt'].'">':'');
 		echo ($lit_file != null?'</a>'.PHP_EOL:'');
-		echo '<h4 class="literature__text">';
+		echo '<figcaption class="literature__text">';
 		echo ($lit_file != null?'<a class="literature__link" href="'.$lit_file['url'].'">':'');
 		echo $lit_title;
 		echo ($lit_file != null?'</a>':'');
-		echo '</h4>'.PHP_EOL;
-		echo '</li>'.PHP_EOL;
+		echo '</figcaption>'.PHP_EOL;
+		echo '</figure>'.PHP_EOL;
 	}
-	echo '</ul>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
 	echo '</span>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
 	echo '</section>'.PHP_EOL;
