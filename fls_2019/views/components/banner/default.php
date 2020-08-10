619c42f2b815d7072975bdd3d87c615d84375bc4
Ron Madriz <ron@firstcreative.com>
2020-08-10 09:58:42 -0500

    hero holder

--- fls_2019/views/components/banner/default.php
+++ fls_2019/views/components/banner/default.php
@@ -5,7 +5,7 @@ $default_Pagetitle    = get_the_title();
 // BANNER
 echo '<section id="hero" class="hero">'.PHP_EOL;
 echo '<div class="wrapper">'.PHP_EOL;
-echo '<span class="hero__holder">'.($banner_img != null?'<img src="'.$banner_img['url'].'" alt="">':'').'</span>'.PHP_EOL;
+echo '<span class="hero__holder">'.($banner_img != null?'<img alt="'.$banner_img['alt'].'" class="hero__img img-fluid" src="'.$banner_img['url'].'">':'').'</span>'.PHP_EOL;
 echo '<span class="page_title"><h1>'.(!empty($alternate_page_title)?$alternate_page_title:$default_Pagetitle).'</h1></span>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
