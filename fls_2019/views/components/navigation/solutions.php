bc74154879f4e67c9196a3c140556d0b956f39b9
Ron Madriz <ron@firstcreative.com>
2020-08-10 14:11:42 -0500

    literature - list item fix

--- fls_2019/views/components/navigation/solutions.php
+++ fls_2019/views/components/navigation/solutions.php
@@ -11,10 +11,10 @@ if (have_rows('solutions')) {
 		$solutions_desc            = get_sub_field('desc');
 		$solutions_links           = get_sub_field('links');
 		echo ''.PHP_EOL;
-		echo (!empty($solutions_title)?'<span class="solution__title section__title col-12"><h2>'.$solutions_title.'</h2></div>'.PHP_EOL:'');
-		echo ($solutions_has_description == 1?'<div class="row"><div class="content col-12">'.$solutions_desc.'</div></div>'.PHP_EOL:'');
+		echo (!empty($solutions_title)?'<span class="solutions__title section__title col-12"><h2>'.$solutions_title.'</h2></span>'.PHP_EOL:'');
+		echo ($solutions_has_description == 1?'<span class="solutions__desc">'.$solutions_desc.'</span>'.PHP_EOL:'');
 		if (have_rows('links')) {
-			echo '<div class="row">'.PHP_EOL;
+			echo '<span class="solutions__list">'.PHP_EOL;
 			while (have_rows('links')) {
 				the_row();
 				$solutions_link = get_sub_field('link');
@@ -22,14 +22,15 @@ if (have_rows('solutions')) {
 				$subLink = $solutions_link;
 				setup_postdata($subLink);
 				$subLink_summary = get_field('summary', $subLink->ID);
-				echo '<div class="item col-12 col-md-3">'.PHP_EOL;
-				echo '<a href="'.get_permalink($subLink->ID).'">'.get_the_post_thumbnail($subLink->ID, 'full', array('class' => 'img-fluid')).'</a>'.PHP_EOL;
-				echo '<div class="title"><h3><a href="'.get_permalink($subLink->ID).'">'.get_the_title($subLink->ID).'</a></h3></div>'.PHP_EOL;
-				echo '<div class="desc"><p><a href="'.get_the_permalink($subLink->ID).'">'.$subLink_summary.'</a></p></div>'.PHP_EOL;
-				echo '</div>'.PHP_EOL;
+				echo '<figure class="solutions__item">'.PHP_EOL;
+				echo '<a class="solutions__link" href="'.get_permalink($subLink->ID).'">'.get_the_post_thumbnail($subLink->ID, 'full', array('class' => 'solutions__image img-fluid')).'</a>'.PHP_EOL;
+				echo '<figcaption class="solutions__item--content"><h3 class="solutions__item--title"><a class="solutions__link" href="'.get_permalink($subLink->ID).'">'.get_the_title($subLink->ID).'</a></h3></div>'.PHP_EOL;
+				echo '<span class="solutions__item--desc"><a class="solutions__link" href="'.get_the_permalink($subLink->ID).'">'.$subLink_summary.'</a></span></figcaption>'.PHP_EOL;
+				echo '</figure>'.PHP_EOL;
 				endif;
 			}
 			wp_reset_query();
+			echo '</span>'.PHP_EOL;
 		}
 	}
 	wp_reset_query();
