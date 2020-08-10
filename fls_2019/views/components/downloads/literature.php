6c4492805f827cbfe3ec2d1e394cdedc83f0b47a
Ron Madriz <ron@firstcreative.com>
2020-08-10 11:42:59 -0500

    hero holder

--- fls_2019/views/components/downloads/literature.php
+++ fls_2019/views/components/downloads/literature.php
@@ -2,10 +2,8 @@
 $download_literature_title = get_field('download_literature_title');
 if (have_rows('download_literature')) {
 	echo '<section id="literature">'.PHP_EOL;
-	echo '<div class="container-fluid">'.PHP_EOL;
-	echo '<div class="row">'.PHP_EOL;
+	echo '<div class="wrapper">'.PHP_EOL;
 	echo '<div class="section_title col-12"><h2>'.(!empty($download_literature_title)?$download_literature_title:'download literature').'</h2></div>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
 	echo '<div class="row">'.PHP_EOL;
 	echo '<div class="content col-12">'.PHP_EOL;
 	echo '<ul class="d-flex align-items-start flex-wrap literature_list">'.PHP_EOL;
