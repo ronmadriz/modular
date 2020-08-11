3c6a157ed51d59854048853198edb07ba9a151cd
Ron Madriz <ron@firstcreative.com>
2020-08-11 11:58:01 -0500

    Solutions Alignment

--- fls_2019/includes/cpt_files/solutions_type.php
+++ fls_2019/includes/cpt_files/solutions_type.php
@@ -3,8 +3,8 @@ add_action('init', function () {
 		/*
 		register_extended_taxonomy( 'page_category', 'page', array() );
 		 */
-		register_extended_taxonomy('solution_type', array('solutions', 'industries'));
-		register_extended_taxonomy('industry', array('solutions', 'industries'));
+		register_extended_taxonomy('solution_type', array('solutions', 'industries', 'case_study'));
+		register_extended_taxonomy('industry', array('solutions', 'industries', 'case_study'));
 	});
 
 ?>
