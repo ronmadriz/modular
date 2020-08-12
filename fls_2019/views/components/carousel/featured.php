37b6c2ab39b2cd4c90f8230a89097c7449e9dcd0
Ron Madriz <ron@firstcreative.com>
2020-08-12 11:06:24 -0500

    case studies - styles

--- fls_2019/views/components/carousel/featured.php
+++ fls_2019/views/components/carousel/featured.php
@@ -10,14 +10,14 @@ if ($featured_query->have_posts()) {
 		$featured__img     = get_the_post_thumbnail_url(get_the_ID(), 'full');
 		$featured__title   = get_field('sidebar__title');
 		$featured__summary = get_field('sidebar__summary');
-		echo '<figure class="featured__item">'.PHP_EOL;
+		echo '<div class="featured__item">'.PHP_EOL;
 		echo '<a class="featured__image--link" href="'.get_the_permalink().'"><img alt="'.$featured__title.'" class="featured__image" src="'.$featured__img.'"></a>'.PHP_EOL;
-		echo '<figcaption class="featured__content">'.PHP_EOL;
+		echo '<div class="featured__content">'.PHP_EOL;
 		echo '<span class="featured__content--title">'.(!empty($featured__title)?$featured__title:get_the_title()).'</span>'.PHP_EOL;
 		echo '<span class="featured__content--desc">'.$featured__summary.'</span>'.PHP_EOL;
 		echo '<span class="featured__content--link"><a href="'.get_the_permalink().'">'.$featured__link__text.'</a></span>'.PHP_EOL;
-		echo '</figcaption>'.PHP_EOL;
-		echo '</figure>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
 	}
 	echo '</div>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
