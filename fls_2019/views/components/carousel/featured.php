f2c69e80db57f685582875d13a7cc349792765f9
Ron Madriz <ron@firstcreative.com>
2020-08-12 17:34:20 -0500

    case studies - styles

--- fls_2019/views/components/carousel/featured.php
+++ fls_2019/views/components/carousel/featured.php
@@ -5,17 +5,17 @@ if ($featured_query->have_posts()) {
 	echo '<div class="wrapper">'.PHP_EOL;
 	echo '<span class="featured__header"><h2 class="featured__header--text">Featured Projects</h2></span>'.PHP_EOL;
 	echo '<div class="featured__list">'.PHP_EOL;
+	echo '<a class="featured__nav--prev"><i class="featured__nav--icon"></i><span class="feature__nav--text">'.PHP_EOL;
+	_e('Previous', 'fc_core');
+	echo '</span></a>'.PHP_EOL;
+	echo '<a class="featured__nav--next"><i class="featured__nav--icon"></i><span class="feature__nav--text">'.PHP_EOL;
+	_e('Next', 'fc_core');
+	echo '</span></a>'.PHP_EOL;
 	while ($featured_query->have_posts()) {
 		$featured_query->the_post();
 		$featured__img     = get_the_post_thumbnail_url(get_the_ID(), 'full');
 		$featured__title   = get_field('sidebar__title');
 		$featured__summary = get_field('sidebar__summary');
-		echo '<a class="featured__nav--prev"><i class="featured__nav--icon"></i><span class="feature__nav--text">'.PHP_EOL;
-		_e('Previous', 'fc_core');
-		echo '</span></a>'.PHP_EOL;
-		echo '<a class="featured__nav--next"><i class="featured__nav--icon"></i><span class="feature__nav--text">'.PHP_EOL;
-		_e('Next', 'fc_core');
-		echo '</span></a>'.PHP_EOL;
 		echo '<div class="featured__item">'.PHP_EOL;
 		echo '<a class="featured__image--link" href="'.get_the_permalink().'"><img alt="'.$featured__title.'" class="featured__image" src="'.$featured__img.'"></a>'.PHP_EOL;
 		echo '<span class="featured__title"><a class="featured__title--link" href="'.get_the_permalink().'">'.(!empty($featured__title)?$featured__title:get_the_title()).'</a></span>'.PHP_EOL;
