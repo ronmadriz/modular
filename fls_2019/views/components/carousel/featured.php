d61942e370007a64a33821fca2f257d1e8feb8de
Ron Madriz <ron@firstcreative.com>
2020-08-12 17:24:31 -0500

    case studies - styles

--- fls_2019/views/components/carousel/featured.php
+++ fls_2019/views/components/carousel/featured.php
@@ -10,8 +10,12 @@ if ($featured_query->have_posts()) {
 		$featured__img     = get_the_post_thumbnail_url(get_the_ID(), 'full');
 		$featured__title   = get_field('sidebar__title');
 		$featured__summary = get_field('sidebar__summary');
-		echo '<a class="featured__nav--prev"><i class="featured__nav--icon"></i><span class="feature__nav--text"></span></a>'.PHP_EOL;
-		echo '<a class="featured__nav--next"><i class="featured__nav--icon"></i><span class="feature__nav--text"></span></a>'.PHP_EOL;
+		echo '<a class="featured__nav--prev"><i class="featured__nav--icon"></i><span class="feature__nav--text">'.PHP_EOL;
+		_e('', 'fc_core');
+		echo '</span></a>'.PHP_EOL;
+		echo '<a class="featured__nav--next"><i class="featured__nav--icon"></i><span class="feature__nav--text">'.PHP_EOL;
+		_e('', 'fc_core');
+		echo '</span></a>'.PHP_EOL;
 		echo '<div class="featured__item">'.PHP_EOL;
 		echo '<a class="featured__image--link" href="'.get_the_permalink().'"><img alt="'.$featured__title.'" class="featured__image" src="'.$featured__img.'"></a>'.PHP_EOL;
 		echo '<span class="featured__title"><a class="featured__title--link" href="'.get_the_permalink().'">'.(!empty($featured__title)?$featured__title:get_the_title()).'</a></span>'.PHP_EOL;
