e8e796f3a20d8982e4e03402e775bf1a03308c08
Ron Madriz <ron@firstcreative.com>
2020-08-13 00:16:32 -0500

    carousel - navigation

--- fls_2019/functions/scripts-styles.php
+++ fls_2019/functions/scripts-styles.php
@@ -4,7 +4,7 @@ function enqueue_my_scripts() {
 	wp_deregister_script('jquery');
 	wp_deregister_script('jquery-ui-core');
 	$pathTojQuery  = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
-	$pathToScripts = get_stylesheet_directory_uri()."/dist/scripts/main.min.js", false, false;
+	$pathToScripts = get_stylesheet_directory_uri()."/dist/scripts/main.min.js";
 	wp_enqueue_script('jquery', $pathTojQuery, array(), true, true);
 	wp_enqueue_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', array('jquery'), '', true, true);
 	wp_enqueue_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', array('jquery'), true, true);
@@ -12,7 +12,7 @@ function enqueue_my_scripts() {
 	wp_enqueue_script('lightgallery_js', 'https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.6.12/js/lightgallery.min.js', array('jquery'), '', true, true);
 	wp_enqueue_script('lightslide_js', 'https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/js/lightslider.min.js', array('jquery'), '', true, true);
 	wp_enqueue_script('update', get_stylesheet_directory_uri().'/dist/scripts/jquery.firstVisitPopup.min.js', array('jquery'), '', true, true);
-	wp_enqueue_script('site_script', $pathToScripts, array('jquery'), false);
+	wp_enqueue_script('site_script', $pathToScripts, array('jquery'), false, false);
 }
 add_action('wp_enqueue_scripts', 'enqueue_my_scripts');
 
