e543aeff0f07db2899f1edd974ac2a214377749b
Ron Madriz <ron@firstcreative.com>
2020-08-13 12:40:35 -0500

    carousel - navigation

--- fls_2019/views/components/blog/default.php
+++ fls_2019/views/components/blog/default.php
@@ -18,7 +18,7 @@ if ($blog__query->have_posts()) {
 		$blog__query->the_post();
 		$blog__thumb = get_the_post_thumbnail_url();
 		echo '<article class="blog__item">'.PHP_EOL;
-		echo '<span class="blog__thumb"><img alt="'.get_the_title().'" class="blog__thumb--img" src="'.$blog__thumb.'"></span>'.PHP_EOL;
+		echo (!empty($blog__thumb)?'<span class="blog__thumb"><img alt="'.get_the_title().'" class="blog__thumb--img" src="'.$blog__thumb.'"></span>'.PHP_EOL:'');
 		echo '<div class="blog__content">'.PHP_EOL;
 		echo '<h3 class="blog__title">'.get_the_title().'</h3>'.PHP_EOL;
 		echo '<p class="blog__desc">'.get_the_excerpt().'</p>'.PHP_EOL;
