7adb11819a2be8d831e091fabb43ecb7d4575f6c
Ron Madriz <ron@firstcreative.com>
2020-08-13 12:37:44 -0500

    carousel - navigation

--- fls_2019/views/components/banner/default.php
+++ fls_2019/views/components/banner/default.php
@@ -2,12 +2,14 @@
 $alternate_page_title = get_field('alternate_page_title');
 $banner_img           = get_field('banner');
 $default_Pagetitle    = get_the_title();
+$blogpage__title      = get_the_title(get_option('page_for_posts', true));
+;
 // BANNER
 echo '<section id="hero" class="hero">'.PHP_EOL;
 echo '<span class="hero__holder">'.($banner_img != null?'<img alt="'.$banner_img['alt'].'" class="hero__img img-fluid" src="'.$banner_img['url'].'">':'').'</span>'.PHP_EOL;
 echo '<div class="wrapper">'.PHP_EOL;
 if (is_home()) {
-	echo '<span class="hero__title"><h1 class="hero__title--text">'.single_post_title().'</h1></span>'.PHP_EOL;
+	echo '<span class="hero__title"><h1 class="hero__title--text">'.$blogpage__title.'</h1></span>'.PHP_EOL;
 } elseif (is_taxonomy()) {
 	echo '<span class="hero__title"><h1 class="hero__title--text">'.single_term_title().'</h1></span>'.PHP_EOL;
 } elseif (!empty($alternate_page_title)) {
