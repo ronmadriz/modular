196d81bd7231192274baaf5bfc79e4a92795ebdc
Ron Madriz <ron@firstcreative.com>
2020-08-13 11:03:57 -0500

    carousel - navigation

--- fls_2019/styles/components/blog/default.php
+++ fls_2019/styles/components/blog/default.php
@@ -1,11 +1,13 @@
-<div class="blog">
-	<div class="blog__list">
-		<article class="blog__item">
-			<span class="blog__image"></span>
-			<div class="blog__content">
-				<h3 class="blog__title"></h3>
-				<p class="blog__desc"></p>
-			</div>
-		</article>
-	</div>
-</div>
+<?
+echo '<div class="blog">'.PHP_EOL;
+echo '<div class="blog__list">'.PHP_EOL;
+echo '<article class="blog__item">'.PHP_EOL;
+echo '<span class="blog__thumb"><img alt="" class="" src=""></span>'.PHP_EOL;
+echo '<div class="blog__content">'.PHP_EOL;
+echo '<h3 class="blog__title"></h3>'.PHP_EOL;
+echo '<p class="blog__desc"></p>'.PHP_EOL;
+echo '<a class="blog__link" href="#">Read More</a>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</article>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
