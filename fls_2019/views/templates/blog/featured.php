b3afd081eaf343e0ae08a56e4c5efef3c1be74d4
Ron Madriz <ron@firstcreative.com>
2020-08-13 23:27:11 -0500

    blog - sidebar

--- fls_2019/views/templates/blog/featured.php
+++ fls_2019/views/templates/blog/featured.php
@@ -1,5 +1,10 @@
 <?php
-$featured_args        = array('post_type' => 'posts', 'post_status' => 'publish', 'posts_per_page' => -1, 'tag' => 'featured');
+$featured_args = array(
+	'post_type'      => 'post',
+	'post_status'    => 'publish',
+	'posts_per_page' => -1,
+	'tag'            => 'featured',
+);
 $section__title       = 'Must Read Stories';
 $featured__link__text = 'View Project';
 include (get_template_directory().'/views/components/carousel/featured.php');
