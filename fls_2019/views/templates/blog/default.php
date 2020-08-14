e411be817be29b52d6a9f0bd676e7ec317c830e3
Ron Madriz <ron@firstcreative.com>
2020-08-14 00:10:40 -0500

    blog - sidebar

--- fls_2019/views/templates/blog/default.php
+++ fls_2019/views/templates/blog/default.php
@@ -21,7 +21,7 @@ if ($blog__query->have_posts()) {
 		echo '<div class="blog__content">'.PHP_EOL;
 		echo '<h3 class="blog__title">'.get_the_title().'</h3>'.PHP_EOL;
 		echo '<span class="blog__meta"><a class="blog__author">FLS</a> &ndash; <date class="blog__date">October 8, 2020</date></span>'.PHP_EOL;
-		echo '<p class="blog__desc">'.get_the_excerpt().'</p>'.PHP_EOL;
+		echo '<span class="blog__desc">'.get_the_excerpt().'</span>'.PHP_EOL;
 		echo '<a class="blog__link button__solid" href="'.get_the_permalink().'">';
 		_e('Read More', 'fc_core');
 		echo '</a>'.PHP_EOL;
