939e74a09c0eefcf93eeb2ced4e764799fcb851d
Ron Madriz <ron@firstcreative.com>
2020-08-13 19:14:45 -0500

    blog - sidebar

--- fls_2019/home.php
+++ fls_2019/home.php
@@ -3,8 +3,11 @@ $pageID = get_the_id();
 $pageCF = get_post_custom($pageID);
 get_header();
 include (get_template_directory().'/views/components/banner/default.php');
+echo '<section id="blog" class="blog">'.PHP_EOL;
+echo '<div class="wrapper">'.PHP_EOL;
 include (get_template_directory().'/views/templates/blog/default.php');
 include (get_template_directory().'/views/templates/blog/side.php');
-
+echo '</div>'.PHP_EOL;
+echo '</section>'.PHP_EOL;
 get_footer();
 ?>
