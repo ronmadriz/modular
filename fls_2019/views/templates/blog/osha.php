7fc465c42fa9468ce53377ae1b0e517eb78f9198
Ron Madriz <ron@firstcreative.com>
2020-08-14 01:05:04 -0500

    blog - sidebar

--- fls_2019/views/templates/blog/osha.php
+++ fls_2019/views/templates/blog/osha.php
@@ -1,7 +1,7 @@
 <?
 echo '<div class="osha">'.PHP_EOL;
 echo '<h3 class="osha__title blog__side--title">';
-_e('osha news feed', 'fc_core')
+_e('osha news feed', 'fc_core');
 echo '</h3>'.PHP_EOL;
 echo do_shortcode('[wp-rss-aggregator sources="1119044"]');
 echo '</div>'.PHP_EOL;
