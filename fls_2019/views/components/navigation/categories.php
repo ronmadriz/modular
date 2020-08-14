37ae2eee4d2c413bdf4ecff9fab49ca406b542a0
Ron Madriz <ron@firstcreative.com>
2020-08-14 00:00:11 -0500

    blog - sidebar

--- fls_2019/views/components/navigation/categories.php
+++ fls_2019/views/components/navigation/categories.php
@@ -1,9 +1,12 @@
 <?php
 echo '<div class="categories">'.PHP_EOL;
+echo '<h3 class="categories__title">';
+_e('Discover', 'fc_core');
+echo '</h3>'.PHP_EOL;
 echo '<ul class="categories__list">'.PHP_EOL;
 wp_list_categories(array(
 		'orderby'    => 'name',
-		'title_li'   => 'discover',
+		'title_li'   => '',
 		'show_count' => 1,
 		'walker'     => new Walker_Categories_Template
 	));
