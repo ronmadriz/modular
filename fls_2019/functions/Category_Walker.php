c1b131852f3d02a68e3be6ce084ac63a49ed2c9a
Ron Madriz <ron@firstcreative.com>
2020-08-13 22:10:38 -0500

    blog - sidebar

--- fls_2019/functions/Category_Walker.php
+++ fls_2019/functions/Category_Walker.php
@@ -2,19 +2,19 @@
 // Categories walker
 class Walker_Categories_Template extends Walker_Category {
 
-	function start_lvl(&$output, $depth = 1, $args) {
+	function start_lvl(&$output, $depth = 1, $args = array()) {
 		$output .= "\n<ul class=\"categories__list\">\n";
 	}
 
-	function end_lvl(&$output, $depth = 0, $args) {
+	function end_lvl(&$output, $depth = 0, $args = array()) {
 		$output .= "</ul>\n";
 	}
 
-	function start_el(&$output, $item, $depth = 0, $arg) {
+	function start_el(&$output, $item, $depth = 0, $args = array() {
 		$output .= "<li class=\"categories__item\"><a class=\"categories__link\" href=\"".esc_url(get_category_link($item->term_id))."\" title=\"".esc_attr($item->name)."\"><span class=\"categories__count\">".esc_attr($item->count)."</span>".esc_attr($item->name);
 	}
 
-	function end_el(&$output, $item, $depth = 0, $args) {
+	function end_el(&$output, $item, $depth = 0, $args = array()) {
 		$output .= "</a></li>\n";
 	}
 }
