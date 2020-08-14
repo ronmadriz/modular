4d6b9b7b706085c2f8e150b0dfcd2a8bf5d4a5fb
Ron Madriz <ron@firstcreative.com>
2020-08-13 22:57:20 -0500

    blog - sidebar

--- fls_2019/views/templates/blog/side.php
+++ fls_2019/views/templates/blog/side.php
@@ -1,4 +1,4 @@
 <?
-echo '<aside class="blogside">'.PHP_EOL;
+echo '<aside class="blog__side">'.PHP_EOL;
 include (get_template_directory().'/views/components/navigation/categories.php');
 echo '</aside>'.PHP_EOL;
