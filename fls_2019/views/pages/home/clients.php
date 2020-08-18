c7a813fe7356ba3cbd0e5b20caa6126c2e8f8423
Ron Madriz <ron@firstcreative.com>
2020-08-18 15:30:02 -0500

    home - styles

--- fls_2019/views/pages/home/clients.php
+++ fls_2019/views/pages/home/clients.php
@@ -1,5 +1,16 @@
 <section id="home_clients" class="clients">
 	<div class="wrapper">
-
+		<span class="clients__title"><h2 class="clients__title--text">Our Work</h2></span>
+		<span class="clients__content">
+			We have designed and installed thousands of handrail, guardrail &amp; fall protection solutions over the years. View installations, watch videos, read case studies and more.
+		</span>
+		<span class="clients__button"><a class="clients__link" href="/about/case-studies/">View Portfolio</a></span>
+		<div class="clients__carousel">
+			<div class="clients__list">
+				<a class="clients__item" href="#"></a>
+				<a class="clients__item" href="#"></a>
+				<a class="clients__item" href="#"></a>
+			</div>
+		</div>
 	</div>
 </section>
--- fls_2019/views/pages/home/testimonials.php
+++ fls_2019/views/pages/home/testimonials.php
@@ -1,5 +1,21 @@
 <section id="home_testimonials" class="testimonials">
 	<div class="wrapper">
-
+		<div class="testimonials__list">
+			<div class="testimonials__item">
+				<blockquote class="testimonials__quote">“The crew did a great job, were very professional and easy to work with. The project was completed exactly on time. Flexible Lifeline Systems responded to my questions and comments in a very timely manner; giving me even more information than I was expecting.”</blockquote>
+				<span class="testimonials__city">Nang - Houston, TX</span>
+				<span class="testimonials__company">Marathon Oil Corporation</span>
+			</div>
+			<div class="testimonials__item">
+				<blockquote class="testimonials__quote">“The crew did a great job, were very professional and easy to work with. The project was completed exactly on time. Flexible Lifeline Systems responded to my questions and comments in a very timely manner; giving me even more information than I was expecting.”</blockquote>
+				<span class="testimonials__city">Nang - Houston, TX</span>
+				<span class="testimonials__company">Marathon Oil Corporation</span>
+			</div>
+			<div class="testimonials__item">
+				<blockquote class="testimonials__quote">“The crew did a great job, were very professional and easy to work with. The project was completed exactly on time. Flexible Lifeline Systems responded to my questions and comments in a very timely manner; giving me even more information than I was expecting.”</blockquote>
+				<span class="testimonials__city">Nang - Houston, TX</span>
+				<span class="testimonials__company">Marathon Oil Corporation</span>
+			</div>
+		</div>
 	</div>
 </section>
