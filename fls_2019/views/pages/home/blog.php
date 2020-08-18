3eec7dde19576299a5dc267523229bb29ede8851
Ron Madriz <ron@firstcreative.com>
2020-08-18 15:02:35 -0500

    home - styles

--- fls_2019/views/pages/home/blog.php
+++ fls_2019/views/pages/home/blog.php
@@ -1,5 +1,48 @@
 <section id="home_blog" class="blogs">
 	<div class="wrapper">
-
+		<div class="blogs__list">
+			<article class="blogs__item">
+				<header class="blogs__header">
+					<a href="blogs__image--link"><img alt="" class="blogs__image" src="https://via.placehold.com/430x275"></a>
+				</header>
+				<div class="blogs__content">
+					<span class="blogs__sub">Safety Recommendations</span>
+					<h3 class="blogs__title"><a class="blogs__title--link" href="#">Do I need a Lifeline or Guardrail for my Roof?</a></h3>
+					<span class="blogs__desc">We often have customers ask us to put a lifeline or a guardrail on their roof to protect their employees and contractors. Both lifelines and guardrails can be an effective means to protect people working at height and each has its' place and purpose. In this article we will be evaluating the criteria and considerations...</span>
+				</div>
+				<footer class="blogs__footer">
+					<time class="blogs__date">12/19/2017</time>
+					<span class="blogs__buttons"><a class="blogs__link" href="#">Contineu Reading</a></span>
+				</footer>
+			</article>
+			<article class="blogs__item">
+				<header class="blogs__header">
+					<a href="blogs__image--link"><img alt="" class="blogs__image" src="https://via.placehold.com/430x275"></a>
+				</header>
+				<div class="blogs__content">
+					<span class="blogs__sub">Safety Recommendations</span>
+					<h3 class="blogs__title"><a class="blogs__title--link" href="#">Do I need a Lifeline or Guardrail for my Roof?</a></h3>
+					<span class="blogs__desc">We often have customers ask us to put a lifeline or a guardrail on their roof to protect their employees and contractors. Both lifelines and guardrails can be an effective means to protect people working at height and each has its' place and purpose. In this article we will be evaluating the criteria and considerations...</span>
+				</div>
+				<footer class="blogs__footer">
+					<time class="blogs__date">12/19/2017</time>
+					<span class="blogs__buttons"><a class="blogs__link" href="#">Contineu Reading</a></span>
+				</footer>
+			</article>
+			<article class="blogs__item">
+				<header class="blogs__header">
+					<a href="blogs__image--link"><img alt="" class="blogs__image" src="https://via.placehold.com/430x275"></a>
+				</header>
+				<div class="blogs__content">
+					<span class="blogs__sub">Safety Recommendations</span>
+					<h3 class="blogs__title"><a class="blogs__title--link" href="#">Do I need a Lifeline or Guardrail for my Roof?</a></h3>
+					<span class="blogs__desc">We often have customers ask us to put a lifeline or a guardrail on their roof to protect their employees and contractors. Both lifelines and guardrails can be an effective means to protect people working at height and each has its' place and purpose. In this article we will be evaluating the criteria and considerations...</span>
+				</div>
+				<footer class="blogs__footer">
+					<time class="blogs__date">12/19/2017</time>
+					<span class="blogs__buttons"><a class="blogs__link" href="#">Contineu Reading</a></span>
+				</footer>
+			</article>
+		</div>
 	</div>
 </section>
