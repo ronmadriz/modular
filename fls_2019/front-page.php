a41cfaec5bbb3eeed469a0d8279a7fe843f94a27
Ron Madriz <ron@firstcreative.com>
2020-08-18 11:26:15 -0500

    home - banner

--- fls_2019/front-page.php
+++ fls_2019/front-page.php
@@ -3,53 +3,7 @@ $pageID = get_the_id();
 $pageCF = get_post_custom($pageID);
 get_header();
 
-// Banner Slider
-$banners = get_field('banners');
-if ($banners) {
-	while (have_rows('banners')):
-	the_row();
-	echo '<section id="home_banner">'.PHP_EOL;
-	echo '<div class="container-fluid">'.PHP_EOL;
-	echo '<div id="banner_slider" class="row justify-content-center align-content-center carousel slide" data-ride="carousel">'.PHP_EOL;
-	$banner = get_field('banner');
-	if (have_rows('banner')):
-	echo '<div class="carousel-inner">'.PHP_EOL;
-	while (have_rows('banner')):
-	the_row();
-	$banner_title = get_sub_field('title');
-	$banner_logo  = get_sub_field('logo');
-	$banner_image = get_sub_field('image');
-	$banner_link  = get_sub_field('link');
-	echo '<div class="carousel-item';
-	if ($count == 0) {echo ' active';};
-	echo '">'.PHP_EOL;
-	echo '<img src="'.$banner_logo['url'].'" alt="'.$$banner_logo['alt'].'" class="brand img-fluid">'.PHP_EOL;
-	echo '<h1>'.$banner_title.'</h1>'.PHP_EOL;
-	if ($banner_link):
-	echo '<a href="'.get_permalink($banner_link->ID).'" aria-label="'.$banner_title.'"></a>'.PHP_EOL;
-	endif;
-	echo '<img src="'.$banner_image['url'].'" alt="'.$$banner_image['alt'].'" class="background img-fluid">'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	$count++;
-	wp_reset_query();
-	endwhile;
-	echo '</div>'.PHP_EOL;
-	echo '<ol class="carousel-indicators d-none d-md-flex">'.PHP_EOL;
-	$active = ' class="active"';
-	$num    = 0;
-	while (have_rows('banner')):the_row();
-	echo '<li data-target="#banner_slider" data-slide-to="'.$num.'"'.$active.'></li>'.PHP_EOL;
-	$active = '';
-	$num += 1;
-	endwhile;
-	echo '</ol>'.PHP_EOL;
-	endif;
-	echo '</div>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	echo '</section>'.PHP_EOL;
-	endwhile;
-	wp_reset_query();
-}
+include (get_template_directory().'/views/components/banner/home.php');
 
 // Home Core Content
 
---
+++ fls_2019/views/components/banner/home.php
@@ -0,0 +1,21 @@
+<section id="hero" class="hero">
+	<div class="wrapper">
+		<div class="hero__content">
+			<h2 class="hero__content--title">Identify Your Hazards</h2>
+			<span class="hero__content--desc">Conduct a fall hazard assessment and explore potential solutions with one of our specialists. Virtual Online or In-Person Onsite.</span>
+			<span class="hero__content--button"><a class="hero__content--link" href="#">Read More</a></span>
+		</div>
+		<span class="hero__services">
+			<ul class="hero__services--list">
+				<li><a href="#" class="hero__services--link"><i class="hero__services--icon"></i><span class="hero__services--text">Rigid Rail</span></a></li>
+				<li><a href="#" class="hero__services--link"><i class="hero__services--icon"></i><span class="hero__services--text">Cavke Vased</span></a></li>
+				<li><a href="#" class="hero__services--link"><i class="hero__services--icon"></i><span class="hero__services--text">Guardrail &amp; Safety</span></a></li>
+				<li><a href="#" class="hero__services--link"><i class="hero__services--icon"></i><span class="hero__services--text">Skylight Hatches</span></a></li>
+				<li><a href="#" class="hero__services--link"><i class="hero__services--icon"></i><span class="hero__services--text">Access Solutions</span></a></li>
+				<li><a href="#" class="hero__services--link"><i class="hero__services--icon"></i><span class="hero__services--text">Anchor Points</span></a></li>
+				<li><a href="#" class="hero__services--link"><i class="hero__services--icon"></i><span class="hero__services--text">Portable Solutions</span></a></li>
+				<li><a href="#" class="hero__services--link"><i class="hero__services--icon"></i><span class="hero__services--text">Custom Solutions</span></a></li>
+			</ul>
+		</span>
+	</div>
+</section>
