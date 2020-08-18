8dc6e88171ce7ab6ff3890697bc529b550ea7c68
Ron Madriz <ron@firstcreative.com>
2020-08-18 13:53:10 -0500

    home - styles

--- fls_2019/front-page.php
+++ fls_2019/front-page.php
@@ -3,4 +3,5 @@ $pageID = get_the_id();
 $pageCF = get_post_custom($pageID);
 get_header();
 include (get_template_directory().'/views/components/banner/home.php');
+include (get_template_directory().'/views/pages/home/industries.php');
 get_footer();
---
+++ fls_2019/styles/pages/home/industries.php
@@ -0,0 +1,45 @@
+<section id="home_industries" class="industries">
+	<div class="wrapper">
+		<span class="industries__title"><h2 class="industries__title--text">Industry Experts</h2></span>
+		<span class="industries__content">Flexible Lifeline Systems goes the extra step to become experts at analyzing your hazards as well as your employee work habits. This has lead us to installing thousands of fall arrest and fall protection systems globally that exceed expectations. Our comprehensive range of products meet the requirements of even the most challenging environments.</span>
+		<ul class="industries__list">
+			<li class="industries__item">
+				<img alt="" class="industries__image" src="https://via.placeholder.com/430x275">
+				<a class="industries__link" href="#">
+					<span class="industries__link--text">Aircraft</span>
+				</a>
+			</li>
+			<li class="industries__item">
+				<img alt="" class="industries__image" src="https://via.placeholder.com/430x275">
+				<a class="industries__link" href="#">
+					<span class="industries__link--text">Rooftop</span>
+				</a>
+			</li>
+			<li class="industries__item">
+				<img alt="" class="industries__image" src="https://via.placeholder.com/430x275">
+				<a class="industries__link" href="#">
+					<span class="industries__link--text">Industrial</span>
+				</a>
+			</li>
+			<li class="industries__item">
+				<img alt="" class="industries__image" src="https://via.placeholder.com/430x275">
+				<a class="industries__link" href="#">
+					<span class="industries__link--text">Energy</span>
+				</a>
+			</li>
+			<li class="industries__item">
+				<img alt="" class="industries__image" src="https://via.placeholder.com/430x275">
+				<a class="industries__link" href="#">
+					<span class="industries__link--text">Railcar</span>
+				</a>
+			</li>
+			<li class="industries__item">
+				<img alt="" class="industries__image" src="https://via.placeholder.com/430x275">
+				<a class="industries__link" href="#">
+					<span class="industries__link--text">All Industries</span>
+				</a>
+			</li>
+		</ul>
+
+	</div>
+</section>
