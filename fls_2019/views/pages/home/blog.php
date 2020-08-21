56346028acb265fc4ab86104637243c5c9ab349e
Ron Madriz <ron@firstcreative.com>
2020-08-21 18:08:15 -0500

    home - industries

--- fls_2019/views/pages/home/blog.php
+++ fls_2019/views/pages/home/blog.php
@@ -12,7 +12,7 @@ if (have_rows('hm_blog')) {
 			setup_postdata($post);
 			$hm_blog_img   = get_the_post_thumbnail_url('full');
 			$hm_blog_title = get_the_title();
-			$hm_blog_date  = get_the_date();
+			$hm_blog_date  = get_the_date('m/d/Y');
 			$hm_blog_ex    = get_the_excerpt();
 			$hm_blog_link  = get_the_permalink();
 			echo '<article class="blogs__item">'.PHP_EOL;
