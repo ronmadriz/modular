3429e079d48628983886d0ada156caabab88eac3
Ron Madriz <ron@firstcreative.com>
2020-08-21 12:02:35 -0500

    industries

--- fls_2019/views/pages/home/testimonials.php
+++ fls_2019/views/pages/home/testimonials.php
@@ -1,6 +1,7 @@
 <?
 echo '<section id="home_testimonials" class="testimonials">'.PHP_EOL;
 echo '<div class="wrapper">'.PHP_EOL;
+echo '<div class="testimonials__carousel">'.PHP_EOL;
 echo '<div class="testimonials__list">'.PHP_EOL;
 echo '<article class="testimonials__item">'.PHP_EOL;
 echo '<blockquote class="testimonials__quote">“The crew did a great job, were very professional and easy to work with. The project was completed exactly on time. Flexible Lifeline Systems responded to my questions and comments in a very timely manner; giving me even more information than I was expecting.”</blockquote>'.PHP_EOL;
@@ -24,5 +25,6 @@ echo '</span></a>'.PHP_EOL;
 echo '<a class="featured__nav featured__nav--next" href="#featured__carousel"><i class="featured__icon featured__icon--next"></i><span class="featured__nav--text">'.PHP_EOL;
 _e('Next', 'fc_core');
 echo '</span></a>'.PHP_EOL;
+echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
