68163d469767b95072cd7f07df2dd141912b7b1e
Ron Madriz <ron@firstcreative.com>
2020-08-21 17:57:21 -0500

    home - industries

--- fls_2019/views/pages/home/blog.php
+++ fls_2019/views/pages/home/blog.php
@@ -1,48 +1,43 @@
-<section id="home_blog" class="blogs">
-	<div class="wrapper">
-		<div class="blogs__list">
-			<article class="blogs__item">
-				<header class="blogs__header">
-					<a href="blogs__image--link"><img alt="" class="blogs__image" src="https://via.placeholder.com/430x275"></a>
-				</header>
-				<div class="blogs__content">
-					<span class="blogs__sub">Safety Recommendations</span>
-					<h3 class="blogs__title"><a class="blogs__title--link" href="#">Do I need a Lifeline or Guardrail for my Roof?</a></h3>
-					<span class="blogs__desc">We often have customers ask us to put a lifeline or a guardrail on their roof to protect their employees and contractors. Both lifelines and guardrails can be an effective means to protect people working at height and each has its' place and purpose. In this article we will be evaluating the criteria and considerations...</span>
-				</div>
-				<footer class="blogs__footer">
-					<time class="blogs__date">12/19/2017</time>
-					<span class="blogs__buttons"><a class="blogs__link" href="#">Contineu Reading</a></span>
-				</footer>
-			</article>
-			<article class="blogs__item">
-				<header class="blogs__header">
-					<a href="blogs__image--link"><img alt="" class="blogs__image" src="https://via.placeholder.com/430x275"></a>
-				</header>
-				<div class="blogs__content">
-					<span class="blogs__sub">Safety Recommendations</span>
-					<h3 class="blogs__title"><a class="blogs__title--link" href="#">Do I need a Lifeline or Guardrail for my Roof?</a></h3>
-					<span class="blogs__desc">We often have customers ask us to put a lifeline or a guardrail on their roof to protect their employees and contractors. Both lifelines and guardrails can be an effective means to protect people working at height and each has its' place and purpose. In this article we will be evaluating the criteria and considerations...</span>
-				</div>
-				<footer class="blogs__footer">
-					<time class="blogs__date">12/19/2017</time>
-					<span class="blogs__buttons"><a class="blogs__link" href="#">Contineu Reading</a></span>
-				</footer>
-			</article>
-			<article class="blogs__item">
-				<header class="blogs__header">
-					<a href="blogs__image--link"><img alt="" class="blogs__image" src="https://via.placeholder.com/430x275"></a>
-				</header>
-				<div class="blogs__content">
-					<span class="blogs__sub">Safety Recommendations</span>
-					<h3 class="blogs__title"><a class="blogs__title--link" href="#">Do I need a Lifeline or Guardrail for my Roof?</a></h3>
-					<span class="blogs__desc">We often have customers ask us to put a lifeline or a guardrail on their roof to protect their employees and contractors. Both lifelines and guardrails can be an effective means to protect people working at height and each has its' place and purpose. In this article we will be evaluating the criteria and considerations...</span>
-				</div>
-				<footer class="blogs__footer">
-					<time class="blogs__date">12/19/2017</time>
-					<span class="blogs__buttons"><a class="blogs__link" href="#">Contineu Reading</a></span>
-				</footer>
-			</article>
-		</div>
-	</div>
-</section>
+<?
+if (have_rows('hm_blog')) {
+	echo '<section id="home_blog" class="blogs">'.PHP_EOL;
+	echo '<div class="wrapper">'.PHP_EOL;
+	echo '<div class="blogs__list">'.PHP_EOL;
+	while (have_rows('hm_blog')) {
+		the_row();
+		$hm_blog_post = get_sub_field('hm_blog_post');
+		if ($hm_blog_post) {
+			foreach ($hm_blog_post as $hm_post) {
+				setup_postdata($hm_post);
+				$hm_blog_img   = get_the_post_thumbnail_url('post-thumbnail');
+				$hm_blog_title = get_the_title();
+				$hm_blog_date  = get_the_date();
+				$hm_blog_link  = get_the_permalink();
+				echo '<article class="blogs__item">'.PHP_EOL;
+				echo '<header class="blogs__header">'.PHP_EOL;
+				echo '<a href="blogs__image--link"><img alt="'.$hm_blog_title.'" class="blogs__image" src="'.$hm_blog_img.'"></a>'.PHP_EOL;
+				echo '</header>'.PHP_EOL;
+				echo '<div class="blogs__content">'.PHP_EOL;
+				echo '<span class="blogs__sub">';
+				the_category(', ');
+				echo '</span>'.PHP_EOL;
+				echo '<h3 class="blogs__title"><a class="blogs__title--link" href="'.$hm_blog_link.'">'.$hm_blog_title.'</a></h3>'.PHP_EOL;
+				echo '<span class="blogs__desc">';
+				the_excerpt();
+				echo '</span>'.PHP_EOL;
+				echo '</div>'.PHP_EOL;
+				echo '<footer class="blogs__footer">'.PHP_EOL;
+				echo '<time class="blogs__date">'.$hm_blog_date.'</time>'.PHP_EOL;
+				echo '<span class="blogs__buttons"><a class="blogs__link" href="'.$hm_blog_link.'">';
+				_e('Continue Reading', 'fc_core');
+				echo '</a></span>'.PHP_EOL;
+				echo '</footer>'.PHP_EOL;
+				echo '</article>'.PHP_EOL;
+			}
+			wp_reset_postdata();
+		}
+	}
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
+}
