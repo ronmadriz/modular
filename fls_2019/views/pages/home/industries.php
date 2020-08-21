8ca7a19a3e3175eac26dce5b134c1db173b78244
Ron Madriz <ron@firstcreative.com>
2020-08-21 17:32:22 -0500

    home - industries

--- fls_2019/views/pages/home/industries.php
+++ fls_2019/views/pages/home/industries.php
@@ -18,12 +18,13 @@ if (have_rows('hm_ind')) {
 			echo '<ul class="industries__list">'.PHP_EOL;
 			while (have_rows('hm_ind_list')) {
 				the_row();
-				$hm_ind_industry = get_sub_field('hm_ind_industry');
-				$hm_ind_image    = get_sub_field('hm_ind_image');
+				$hm_ind_industry   = get_sub_field('hm_ind_industry');
+				$hm_ind_image      = get_sub_field('hm_ind_image');
+				$hm_ind_page_title = get_sub_field('hm_ind_page_title');
 				echo '<li class="industries__item">'.PHP_EOL;
-				echo '<img alt="'.esc_url($hm_ind_industry).'" class="industries__image" src="'.$hm_ind_image['url'].'">'.PHP_EOL;
+				echo '<img alt="'.esc_html($hm_ind_industry).'" class="industries__image" src="'.$hm_ind_image['url'].'">'.PHP_EOL;
 				echo '<a class="industries__link" href="'.esc_url($hm_ind_industry).'">'.PHP_EOL;
-				echo '<span class="industries__link--text">'.esc_html($hm_ind_industry).'</span>'.PHP_EOL;
+				echo '<span class="industries__link--text">'.$hm_ind_page_title.'</span>'.PHP_EOL;
 				echo '</a>'.PHP_EOL;
 				echo '</li>'.PHP_EOL;
 			}
