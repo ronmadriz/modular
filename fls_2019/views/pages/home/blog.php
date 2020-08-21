ab0f681b3fd30a7a8b59a321fe3b3d179e2dfbc3
Ron Madriz <ron@firstcreative.com>
2020-08-21 18:13:48 -0500

    home - industries

--- fls_2019/views/pages/home/blog.php
+++ fls_2019/views/pages/home/blog.php
@@ -16,7 +16,9 @@ if (have_rows('hm_blog')) {
 			$hm_blog_link  = get_the_permalink();
 			echo '<article class="blogs__item">'.PHP_EOL;
 			echo '<header class="blogs__header">'.PHP_EOL;
-			echo '<a href="blogs__image--link"><img alt="'.$hm_blog_title.'" class="blogs__image" src="'.get_the_post_thumbnail_url(get_the_ID(), 'full').'"></a>'.PHP_EOL;
+			echo '<a href="blogs__image--link">';
+			the_post_thumbnail('full', array('blogs__image'));
+			echo '</a>'.PHP_EOL;
 			echo '</header>'.PHP_EOL;
 			echo '<div class="blogs__content">'.PHP_EOL;
 			echo '<span class="blogs__sub">';
