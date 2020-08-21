a0eed857de97b4468a1dd7de3fc7a2312439bc1e
Ron Madriz <ron@firstcreative.com>
2020-08-21 15:03:41 -0500

    industries

--- fls_2019/functions/acf_icon_picker.php
+++ fls_2019/functions/acf_icon_picker.php
@@ -1,8 +1,24 @@
 <?php
-add_filter('acf_svg_icon_filepath', 'bea_svg_icon_filepath');
-function bea_svg_icon_filepath($filepath) {
-	if (is_file(get_stylesheet_directory().'/sprites/')) {
-		$filepath[] = get_stylesheet_directory().'/sprites/';
-	}
-	return $filepath;
+// SVG Icons
+add_filter('acf_icon_path_suffix', 'acf_icon_path_suffix');
+function acf_icon_path_suffix($path_suffix) {
+	return 'sprites/';
 }
+// modify the path to the above prefix
+add_filter('acf_icon_path', 'acf_icon_path');
+function acf_icon_path($path_suffix) {
+	return plugin_dir_path(__FILE__);
+}
+// modify the URL to the icons directory to display on the page
+add_filter('acf_icon_url', 'acf_icon_url');
+function acf_icon_url($path_suffix) {
+	return plugin_dir_url(__FILE__);
+}
+add_filter('wpcf7_form_elements', function ($content) {
+		$content = preg_replace('/<label><input type="(checkbox|radio)" name="(.*?)" value="(.*?)" \/><span class="wpcf7-list-item-label">/i', '<label class="custom-control custom-\1"><input type="\1" name="\2" value="\3" class="custom-control-input"><span class="wpcf7-list-item-label custom-control-label">', $content);
+
+		return $content;
+	});
+
+// for acf contact form 7 plugin
+add_filter('acf_cf7_object', '__return_true');
