c3be3006f5a3ccf22d31f1041e84921909663172
Ron Madriz <ron@firstcreative.com>
2020-08-22 13:15:25 -0500

    home - industries

--- fls_2019/views/pages/home/testimonials.php
+++ fls_2019/views/pages/home/testimonials.php
@@ -11,17 +11,19 @@ if ($tst_query->have_posts()) {
 	echo '<div class="wrapper">'.PHP_EOL;
 	echo '<div id="testimonials__carousel" class="testimonials__carousel carousel slide" data-ride="carousel">'.PHP_EOL;
 	echo '<div class="testimonials__list carousel-inner">'.PHP_EOL;
+	$tst_count = 0;
 	while ($tst_query->have_posts()) {
 		$tst_query->the_post();
 		$tst_content      = get_the_content();
 		$tst_title        = get_the_title();
 		$tst_city_st      = get_field('tst_city_state');
 		$tst_company_name = get_field('tst_company_name');
-		echo '<article class="carousel-item testimonials__item">'.PHP_EOL;
+		echo '<article class="carousel-item testimonials__item'.($tst_count == 0?' active':'').'">'.PHP_EOL;
 		echo '<blockquote class="testimonials__quote">&ldquo;'.$tst_content.'&rdquo;</blockquote>'.PHP_EOL;
 		echo '<span class="testimonials__city">'.$tst_title.(!empty($tst_city_st)?' - '.$tst_city_st:'').'</span>'.PHP_EOL;
 		echo '<span class="testimonials__company">'.$tst_company_name.'</span>'.PHP_EOL;
 		echo '</article>'.PHP_EOL;
+		$tst_count++;
 	}
 	wp_reset_postdata();
 	echo '</div>'.PHP_EOL;
