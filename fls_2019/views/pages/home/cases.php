8749556da90387153b8307e60ab39925ed8d9cd5
Ron Madriz <ron@firstcreative.com>
2020-08-22 14:24:59 -0500

    home - industries

--- fls_2019/views/pages/home/cases.php
+++ fls_2019/views/pages/home/cases.php
@@ -1,21 +1,26 @@
-<section id="home_cases" class="cases">
-	<style>
-		.cases {
-			background-image:url(/wp-content/uploads/2018/12/karbach-after-installation-fall-safety12-1024x767-1024x767.jpg);
-		}
-	</style>
-	<div class="wrapper">
-		<span class="cases__title"><h2 class="cases__title--text">Our Work</h2></span>
-		<span class="cases__content">
-			We have designed and installed thousands of handrail, guardrail &amp; fall protection solutions over the years. View installations, watch videos, read case studies and more.
-		</span>
-		<span class="cases__button"><a class="cases__link" href="/about/case-studies/">View Portfolio</a></span>
-		<div class="cases__carousel">
-			<div class="cases__list">
-				<a class="cases__item" href="#"></a>
-				<a class="cases__item" href="#"></a>
-				<a class="cases__item" href="#"></a>
-			</div>
-		</div>
-	</div>
-</section>
+<?
+if (have_rows('hm_work')) {
+	echo '<section id="home_cases" class="cases">'.PHP_EOL;
+	echo '<style>'.PHP_EOL;
+	echo '.cases {'.PHP_EOL;
+	echo 'background-image:url(/wp-content/uploads/2018/12/karbach-after-installation-fall-safety12-1024x767-1024x767.jpg);'.PHP_EOL;
+	echo '}'.PHP_EOL;
+	echo '</style>'.PHP_EOL;
+	echo '<div class="wrapper">'.PHP_EOL;
+	while (have_rows('hm_work')) {
+		the_row();
+		$hm_work_title = get_sub_field('hm_work_title');
+		$hm_work_desc  = get_sub_field('hm_work_desc');
+		$hm_work_btn   = get_sub_field('hm_work_btn');
+		echo '<span class="cases__title"><h2 class="cases__title--text">'.$hm_work_title.'</h2></span>'.PHP_EOL;
+		echo '<span class="cases__content">'.$hm_work_desc.'</span>'.PHP_EOL;
+		echo '<span class="cases__button"><a class="cases__link" href="'.$hm_work_btn['url'].'">'.$hm_work_btn['title'].'</a></span>'.PHP_EOL;
+		echo '<div class="cases__carousel">'.PHP_EOL;
+		echo '<div class="cases__list">'.PHP_EOL;
+		echo '<a class="cases__item" href="#"></a>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
+	}
+	echo '</div>'.PHP_EOL;
+	echo '</section>'.PHP_EOL;
+}
