aa64a7d50b70d6fa3a418aceef2516a77a6a5c5f
Ron Madriz <ron@firstcreative.com>
2020-08-22 15:15:59 -0500

    home - industries

--- fls_2019/views/pages/home/cases.php
+++ fls_2019/views/pages/home/cases.php
@@ -16,12 +16,14 @@ if (have_rows('hm_work')) {
 		echo '<span class="cases__content">'.$hm_work_desc.'</span>'.PHP_EOL;
 		echo '<span class="cases__button"><a class="cases__link" href="'.$hm_work_btn['url'].'">'.$hm_work_btn['title'].'</a></span>'.PHP_EOL;
 		if (have_rows('hm_work_cases')) {
-			echo '<div class="cases__carousel">'.PHP_EOL;
-			echo '<div class="cases__list">'.PHP_EOL;
+			echo '<div id="cases__carousel" class="cases__carousel" data-ride="carousel">'.PHP_EOL;
+			echo '<div class="cases__list carousel-inner">'.PHP_EOL;
+			$hm_work_count = 0;
 			while (have_rows('hm_work_cases')) {
 				the_row();
 				$hm_work_cases = get_sub_field('hm_work_cases');
-				echo '<span class="cases__item"><img class="cases__image" src="'.$hm_work_cases.'"></span>'.PHP_EOL;
+				echo '<span class="cases__item carousel-item'.($hm_work_count == 0?' active':'').'"><img class="cases__image" src="'.$hm_work_cases.'"></span>'.PHP_EOL;
+				$hm_work_count++;
 			}
 			echo '</div>'.PHP_EOL;
 			echo '</div>'.PHP_EOL;
