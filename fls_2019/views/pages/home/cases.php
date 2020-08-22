73e3b4ae07877f9d5b827c85d31bf9d405de8b66
Ron Madriz <ron@firstcreative.com>
2020-08-22 15:16:20 -0500

    home - industries

--- fls_2019/views/pages/home/cases.php
+++ fls_2019/views/pages/home/cases.php
@@ -1,17 +1,17 @@
 <?
 if (have_rows('hm_work')) {
 	echo '<section id="home_cases" class="cases">'.PHP_EOL;
-	echo '<style>'.PHP_EOL;
-	echo '.cases {'.PHP_EOL;
-	echo 'background-image:url('.$hm_work_bg_img.');'.PHP_EOL;
-	echo '}'.PHP_EOL;
-	echo '</style>'.PHP_EOL;
-	echo '<div class="wrapper">'.PHP_EOL;
 	while (have_rows('hm_work')) {
 		the_row();
 		$hm_work_title = get_sub_field('hm_work_title');
 		$hm_work_desc  = get_sub_field('hm_work_desc');
 		$hm_work_btn   = get_sub_field('hm_work_btn');
+		echo '<style>'.PHP_EOL;
+		echo '.cases {'.PHP_EOL;
+		echo 'background-image:url('.$hm_work_bg_img.');'.PHP_EOL;
+		echo '}'.PHP_EOL;
+		echo '</style>'.PHP_EOL;
+		echo '<div class="wrapper">'.PHP_EOL;
 		echo '<span class="cases__title"><h2 class="cases__title--text">'.$hm_work_title.'</h2></span>'.PHP_EOL;
 		echo '<span class="cases__content">'.$hm_work_desc.'</span>'.PHP_EOL;
 		echo '<span class="cases__button"><a class="cases__link" href="'.$hm_work_btn['url'].'">'.$hm_work_btn['title'].'</a></span>'.PHP_EOL;
@@ -28,7 +28,7 @@ if (have_rows('hm_work')) {
 			echo '</div>'.PHP_EOL;
 			echo '</div>'.PHP_EOL;
 		}
+		echo '</div>'.PHP_EOL;
 	}
-	echo '</div>'.PHP_EOL;
 	echo '</section>'.PHP_EOL;
 }
