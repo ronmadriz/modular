3ca6143f270d47475b83cf95d77138c7665ec3ff
Ron Madriz <ron@firstcreative.com>
2020-08-24 15:13:40 -0500

    load more

--- fls_2019/functions/loadmore.php
+++ fls_2019/functions/loadmore.php
@@ -1,55 +1,40 @@
 <?
 function misha_my_load_more_scripts() {
-
 	global $wp_query;
-
-	// In most cases it is already included on the page and this line can be removed
 	wp_enqueue_script('jquery');
-
-	// register our main script but do not enqueue it yet
 	wp_register_script('my_loadmore', get_stylesheet_directory_uri().'/dist/scripts/myloadmore.js', array('jquery'));
-
-	// now the most interesting part
-	// we have to pass parameters to myloadmore.js script but we can get the parameters values only in PHP
-	// you can define variables directly in your HTML but I decided that the most proper way is wp_localize_script()
 	wp_localize_script('my_loadmore', 'misha_loadmore_params', array(
 			'ajaxurl'      => site_url().'/wp-admin/admin-ajax.php', // WordPress AJAX
 			'posts'        => json_encode($wp_query->query_vars), // everything about your loop is here
 			'current_page' => get_query_var('paged')?get_query_var('paged'):1,
 			'max_page'     => $wp_query->max_num_pages
 		));
-
 	wp_enqueue_script('my_loadmore');
 }
-
 add_action('wp_enqueue_scripts', 'misha_my_load_more_scripts');
-
 function misha_loadmore_ajax_handler() {
-
-	// prepare our arguments for the query
 	$args                = json_decode(stripslashes($_POST['query']), true);
 	$args['paged']       = $_POST['page']+1;// we need next page to be loaded
 	$args['post_status'] = 'publish';
-
-	// it is always better to use WP_Query but not here
 	query_posts($args);
-
-	if (have_posts()):
-
-	// run the loop
-	while (have_posts()):the_post();
-
-	// look into your theme code how the posts are inserted, but you can use your own HTML of course
-	// do you remember? - my example is adapted for Twenty Seventeen theme
-	get_template_part('template-parts/post/content', get_post_format());
-	// for the test purposes comment the line above and uncomment the below one
-	// the_title();
-
-	endwhile;
-
-	endif;
+	if (have_posts()) {
+		while (have_posts()) {
+			the_post();
+			$blog_thumb_more = get_the_post_thumbnail_url();
+			echo '<article class="blog__item">'.PHP_EOL;
+			echo (!empty($blog__thumb_more)?'<span class="blog__thumb"><img alt="'.get_the_title().'" class="blog__thumb--img" src="'.$blog__thumb_more.'"></span>'.PHP_EOL:'');
+			echo '<div class="blog__content">'.PHP_EOL;
+			echo '<h3 class="blog__title">'.get_the_title().'</h3>'.PHP_EOL;
+			echo '<span class="blog__meta"><a class="blog__author">FLS</a> &ndash; <date class="blog__date">October 8, 2020</date></span>'.PHP_EOL;
+			echo '<span class="blog__desc">'.get_the_excerpt().'</span>'.PHP_EOL;
+			echo '<span class="blog__button"><a class="blog__link button__solid" href="'.get_the_permalink().'">';
+			_e('Read More', 'fc_core');
+			echo '</a></span>'.PHP_EOL;
+			echo '</div>'.PHP_EOL;
+			echo '</article>'.PHP_EOL;
+		}
+	}
 	die;// here we exit the script and even no wp_reset_query() required!
 }
-
 add_action('wp_ajax_loadmore', 'misha_loadmore_ajax_handler');// wp_ajax_{action}
 add_action('wp_ajax_nopriv_loadmore', 'misha_loadmore_ajax_handler');// wp_ajax_nopriv_{action}
