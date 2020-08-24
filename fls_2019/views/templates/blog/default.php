4ad543f23bd1b6c7271f108a390fa03ad6696990
Ron Madriz <ron@firstcreative.com>
2020-08-24 15:27:35 -0500

    load more

--- fls_2019/views/templates/blog/default.php
+++ fls_2019/views/templates/blog/default.php
@@ -17,18 +17,18 @@ if ($blog__query->have_posts()) {
 		$lname       = get_the_author_meta('last_name');
 		$full_name   = '';
 		if (empty($fname)) {
-			$full_name = $lname;
+			$full_name = '<a class="blog__author">'.$lname.'</a> - ';
 		} elseif (empty($lname)) {
-			$full_name = $fname;
+			$full_name = '<a class="blog__author">'.$fname.'</a> - ';
 		} else {
 			//both first name and last name are present
-			$full_name = "{$fname} {$lname}";
+			$full_name = '<a class="blog__author">'."{$fname} {$lname}" .'</a> - ';
 		}
 		echo '<article class="blog__item">'.PHP_EOL;
 		echo (!empty($blog__thumb)?'<span class="blog__thumb"><img alt="'.get_the_title().'" class="blog__thumb--img" src="'.$blog__thumb.'"></span>'.PHP_EOL:'');
 		echo '<div class="blog__content">'.PHP_EOL;
 		echo '<h3 class="blog__title">'.get_the_title().'</h3>'.PHP_EOL;
-		echo '<span class="blog__meta"><a class="blog__author">'.$full_name.'</a> &ndash; <date class="blog__date">'.get_the_date('F j,Y').'</date></span>'.PHP_EOL;
+		echo '<span class="blog__meta">'.$full_name.'<date class="blog__date">'.get_the_date('F j,Y').'</date></span>'.PHP_EOL;
 		echo '<span class="blog__desc">'.get_the_excerpt().'</span>'.PHP_EOL;
 		echo '<span class="blog__button"><a class="blog__link button__solid" href="'.get_the_permalink().'">';
 		_e('Read More', 'fc_core');
