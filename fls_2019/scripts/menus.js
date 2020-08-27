19c5a27e11c9c84ca18d2f9fc721f50f5b35f3fa
Ron Madriz <ron@firstcreative.com>
2020-08-27 13:42:14 -0500

    menu js

--- fls_2019/scripts/menus.js
+++ fls_2019/scripts/menus.js
@@ -5,7 +5,7 @@ j(document).ready(function(){
 	if (j(window).width() < 786) {
 		j('<em class="menus__caret"></em>').insertBefore('.menus__child');
 		j('.menus__caret').click( function(){
-			j(this).parent('.menus__caret').toggleClass('menus__active');
+			j(this).parent('.menus__parent').toggleClass('menus__active');
 			j(this).siblings('.menus__child').toggleClass('menus__open');
 			j(this).toggleClass('menus__caret--open');
 		} );	
