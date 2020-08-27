bed2ae4777cce22a149c270fa91e40537dee062d
Ron Madriz <ron@firstcreative.com>
2020-08-27 02:08:08 -0500

    menu js

--- fls_2019/dist/scripts/main.js
+++ fls_2019/dist/scripts/main.js
@@ -168,16 +168,20 @@ j(document).on('click', '[data-toggle="lightbox"]', function(event) {
   }  
   j( Carousel.init );
 })(window);
-var j = jQuery.noConflict();
 j(document).ready(function(){
+	j('.menus__toggle').click(function(){
+		j('.menus__list').toggleClass('menus__list--active');
+	});
+	j('.has__children').click(function() {
+		j('.has__children ul').toggleClass('show');
+	}); 
 	if (j(window).width() < 786) {
 		j('.menus__parent').siblings('.menus__child').hide();
 		j('.menus__parent').click( function(){
 			j(this).siblings('.menus__child').toggle();
 		} );	
-	}
+	}	
 });
-
 function normalizeSlideHeights() {
     j('.carousel').each(function(){
       var items = j('.carousel-item img.background', this);
--- fls_2019/scripts/menus.js
+++ fls_2019/scripts/menus.js
@@ -1,9 +1,14 @@
-var j = jQuery.noConflict();
 j(document).ready(function(){
+	j('.menus__toggle').click(function(){
+		j('.menus__list').toggleClass('menus__list--active');
+	});
+	j('.has__children').click(function() {
+		j('.has__children ul').toggleClass('show');
+	}); 
 	if (j(window).width() < 786) {
 		j('.menus__parent').siblings('.menus__child').hide();
 		j('.menus__parent').click( function(){
 			j(this).siblings('.menus__child').toggle();
 		} );	
-	}
+	}	
 });
