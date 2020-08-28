407156a74cbcb20bcf9faaf3a1466377651a66b0
Ron Madriz <ron@firstcreative.com>
2020-08-28 15:07:23 -0500

    mobile menu icons replaced

--- fls_2019/views/components/forms/home.php
+++ fls_2019/views/components/forms/home.php
@@ -1,7 +1,6 @@
 <?
 echo '<section id="contactFormCTA" class="homeForm">'.PHP_EOL;
 echo '<div class="wrapper">'.PHP_EOL;
-echo '<style>.homeForm {background-image: url("'.get_stylesheet_directory_uri().'/images/cfImage.jpg");}</style>'.PHP_EOL;
 echo '<div class="homeForm__content">'.do_shortcode('[contact-form-7 html_class="homeForm__form" id="1117037" title="Contact CTA"]').'</div>'.PHP_EOL;// id="cfCTA_form" class="col-12 col-md-7 col-lg-7 col-xl-6"
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
