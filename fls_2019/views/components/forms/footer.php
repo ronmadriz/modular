1dd44da75b353a511985b94d75df0ddc4f4a8c53
Ron Madriz <ron@firstcreative.com>
2020-08-28 13:59:04 -0500

    mobile menu icons replaced

--- fls_2019/views/components/forms/footer.php
+++ fls_2019/views/components/forms/footer.php
@@ -8,13 +8,9 @@ if (!is_front_page()) {
 echo '<section id="contactFormCTA" class="genForm">'.PHP_EOL;
 echo '<div class="wrapper">'.PHP_EOL;
 echo '<style>section#contactFormCTA {background-image: url("'.get_stylesheet_directory_uri().'/images/cfImage.jpg");}</style>'.PHP_EOL;
-echo '<div id="cfCTA_form" class="col-12 col-md-7 col-lg-7 col-xl-6">'.do_shortcode('[contact-form-7 html_class="genForm__form" id="1117830" title="Contact CTA"]').'</div>'.PHP_EOL;
+echo '<div class="genForm__content">'.do_shortcode('[contact-form-7 html_class="genForm__form" id="1117830" title="Contact CTA"]').'</div>'.PHP_EOL;// id="cfCTA_form" class="col-12 col-md-7 col-lg-7 col-xl-6"
 echo '</div>'.PHP_EOL;
-echo '</section>'.PHP_EOL;
-echo '<section id="foot_call_cta">'.PHP_EOL;
-echo '<div class="container-fluid">'.PHP_EOL;
-echo '<div class="row align-content-center justify-content-center">'.PHP_EOL;
-echo '<div class="col-12 col-md-8">Talk to Fall Protection Expert Now <a href="tel:'.$form_phone.'" rel="noopener noreferrer"><i class="im im-phone"></i> '.$form_phone.'</a></div>'.PHP_EOL;
+echo '<div class="genForm__cta">Talk to Fall Protection Expert Now <a href="tel:'.$form_phone.'" rel="noopener noreferrer"><i class="im im-phone"></i> '.$form_phone.'</a></div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
