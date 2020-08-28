833b200a246f99e6926e6720cd073729896edde3
Ron Madriz <ron@firstcreative.com>
2020-08-28 16:20:13 -0500

    mobile menu icons replaced

--- fls_2019/views/components/forms/footer.php
+++ fls_2019/views/components/forms/footer.php
@@ -2,7 +2,7 @@
 $form_phone = get_theme_mod('footer_phone');
 echo '<section id="contactFormCTA" class="genForm">'.PHP_EOL;
 echo '<div class="wrapper">'.PHP_EOL;
-echo '<div id="genForm__header"><h2 class="genForm__header--text">';
+echo '<div class="genForm__header"><h2 class="genForm__header--text">';
 _e('Tell Us About Your Fall Hazard', 'fc_core');
 echo '</h2></div>'.PHP_EOL;
 echo '<div class="genForm__content">'.do_shortcode('[contact-form-7 html_class="genForm__form" id="1117830" title="Contact CTA"]').'</div>'.PHP_EOL;// id="cfCTA_form" class="col-12 col-md-7 col-lg-7 col-xl-6"
