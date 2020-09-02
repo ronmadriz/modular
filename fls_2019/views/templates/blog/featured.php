4bb483d662e4286bb03fec540bb9e4c994713a92
Ron Madriz <ron@firstcreative.com>
2020-09-02 14:34:40 -0500

    Featured Blog Fix

--- fls_2019/views/templates/blog/featured.php
+++ fls_2019/views/templates/blog/featured.php
@@ -3,8 +3,13 @@ $featured_args = array(
 	'post_type'      => 'post',
 	'post_status'    => 'publish',
 	'posts_per_page' => -1,
-	'meta_key'       => 'blg_is_featured',
-	'meta_value'     => 1,
+	'meta_query'     => array(
+		array(
+			'key'     => 'blg_is_featured',
+			'value'   => '1',
+			'compare' => '==',
+		)
+	)
 );
 $section__title       = 'Must Read Stories';
 $featured__link__text = 'View Project';
