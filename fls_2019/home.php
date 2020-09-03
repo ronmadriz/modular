805d06bea3598d3537cb0d95a42e471ff413a130
Ron Madriz <ron@firstcreative.com>
2020-09-03 18:12:04 -0500

    fonts

--- fls_2019/home.php
+++ fls_2019/home.php
@@ -3,12 +3,12 @@ $pageID = get_the_id();
 $pageCF = get_post_custom($pageID);
 get_header();
 include (get_template_directory().'/views/components/banner/default.php');
+include (get_template_directory().'/views/templates/blog/featured.php');
 echo '<section id="blog" class="blog">'.PHP_EOL;
 echo '<div class="wrapper">'.PHP_EOL;
 include (get_template_directory().'/views/templates/blog/default.php');
 include (get_template_directory().'/views/templates/blog/side.php');
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
-include (get_template_directory().'/views/templates/blog/featured.php');
 get_footer();
 ?>
