3d21794e679fdb7a54608368b160637cb997ff96
Ron Madriz <ron@firstcreative.com>
2020-09-04 13:57:20 -0500

    carousel case studies

--- fls_2019/dist/scripts/main.js
+++ fls_2019/dist/scripts/main.js
@@ -1,3 +1,26 @@
+j(document).ready(function(){
+  j('.multi__carousel').on('slide.bs.carousel', function (e) {
+    // CC 2.0 License Iatek LLC 2018
+    // Attribution required
+    var je = j(e.relatedTarget);
+    var idx = je.index();
+    console.log("IDX :  " + idx);
+    var itemsPerSlide = 3;
+    var totalItems = j('.carousel-item').length;
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i=0; i<it; i++) {
+        // append slides to end
+        if (e.direction=="left") {
+            j('.carousel-item').eq(i).appendTo('.carousel-inner');
+        }
+        else {
+            j('.carousel-item').eq(0).appendTo('.carousel-inner');
+        }
+      }
+    }
+  });
+});
 var j = jQuery.noConflict();
 j(document).ready(function(){
    var ppp = 3; // Post per page
