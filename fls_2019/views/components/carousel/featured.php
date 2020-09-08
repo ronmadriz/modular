f49dc46c97de0d35c524291e306ee277cefccf03
Ron Madriz <ron@firstcreative.com>
2020-09-08 01:19:42 -0500

    homepage - navigation

--- fls_2019/views/components/carousel/featured.php
+++ fls_2019/views/components/carousel/featured.php
@@ -4,8 +4,8 @@ if ($featured_query->have_posts()) {
 	echo '<section id="featured" class="featured">'.PHP_EOL;
 	echo '<div class="wrapper">'.PHP_EOL;
 	echo '<span class="featured__header"><h2 class="featured__header--text">'.$section__title.'</h2></span>'.PHP_EOL;
-	echo '<div id="featured__carousel" class="featured__carousel">'.PHP_EOL;
-	echo '<div class="featured__list featured__list--transition">'.PHP_EOL;
+	echo '<div id="featured__carousel" class="featured__carousel multi__carousel slide carousel"  data-interval="false">'.PHP_EOL;
+	echo '<div class="featured__list carousel-inner">'.PHP_EOL;
 	while ($featured_query->have_posts()) {
 		$featured_query->the_post();
 		$featured__img     = get_the_post_thumbnail_url(get_the_ID(), 'full');
@@ -13,7 +13,7 @@ if ($featured_query->have_posts()) {
 		$raw__summary      = get_field('sidebar__summary');
 		$featured__excerpt = get_the_excerpt();
 		$featured__summary = (!empty($raw__summary)?$raw__summary:$featured__excerpt);
-		echo '<div class="featured__item">'.PHP_EOL;
+		echo '<div class="featured__item carousel-item'.($hm_blog_count == 0?' active':'').'">'.PHP_EOL;
 		echo '<a class="featured__image--link" href="'.get_the_permalink().'"><img alt="'.$featured__title.'" class="featured__image" src="'.$featured__img.'"></a>'.PHP_EOL;
 		echo '<span class="featured__title"><a class="featured__title--link" href="'.get_the_permalink().'">'.(!empty($featured__title)?$featured__title:get_the_title()).'</a></span>'.PHP_EOL;
 		echo '<span class="featured__desc">'.$featured__summary.'</span>'.PHP_EOL;
@@ -21,10 +21,10 @@ if ($featured_query->have_posts()) {
 		echo '</div>'.PHP_EOL;
 	}
 	echo '</div>'.PHP_EOL;
-	echo '<a class="featured__nav featured__nav--prev" href="#featured__carousel"><i class="featured__icon featured__icon--prev"></i><span class="featured__nav--text">'.PHP_EOL;
+	echo '<a class="featured__nav carousel__nav carousel__nav--prev carousel-control-prev" href="#featured__carousel" role="button" data-slide="prev"><i class="featured__icon carousel__icon carousel__icon--prev"></i><span class="featured__nav--text sr-only">'.PHP_EOL;
 	_e('Previous', 'fc_core');
 	echo '</span></a>'.PHP_EOL;
-	echo '<a class="featured__nav featured__nav--next" href="#featured__carousel"><i class="featured__icon featured__icon--next"></i><span class="featured__nav--text">'.PHP_EOL;
+	echo '<a class="featured__nav carousel__nav carousel__nav--next carousel-control-next" href="#featured__carousel" role="button" data-slide="next"><i class="featured__icon carousel__icon carousel__icon--next"></i><span class="featured__nav--text sr-only">'.PHP_EOL;
 	_e('Next', 'fc_core');
 	echo '</span></a>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
