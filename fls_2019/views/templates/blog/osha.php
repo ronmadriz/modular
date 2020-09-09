a38f3432408b6fb3f96c0c09cf89129c4a8c6623
Ron Madriz <ron@firstcreative.com>
2020-09-09 14:08:56 -0500

    featured carousel - case studies

--- fls_2019/views/templates/blog/osha.php
+++ fls_2019/views/templates/blog/osha.php
@@ -3,5 +3,6 @@ echo '<div class="osha">'.PHP_EOL;
 echo '<h3 class="osha__title blog__side--title">';
 _e('osha news feed', 'fc_core');
 echo '</h3>'.PHP_EOL;
-echo do_shortcode('[wp-rss-aggregator]');
+//echo do_shortcode('[wp-rss-aggregator]');
+echo do_shortcode('[wp_rss_retriever url="https://www.osha.gov/news/newsreleases.xml" items="2" excerpt="20" read_more="true" new_window="true" cache="12 hours"]');
 echo '</div>'.PHP_EOL;
