2878616f126038dc921e483acb8e0add71c30d29
Ron Madriz <ron@firstcreative.com>
2020-09-10 10:07:58 -0500

    featured carousel - case studies

--- fls_2019/views/components/banner/home.php
+++ fls_2019/views/components/banner/home.php
@@ -1,7 +1,6 @@
 <?
 echo '<section id="hero" class="hero">'.PHP_EOL;
-echo '<div class="wrapper">'.PHP_EOL;
-echo '<div id="blog__carousel" class="blog__carousel multi__carousel slide carousel"  data-interval="false">'.PHP_EOL;
+echo '<div id="hero__carousel" class="hero__carousel multi__carousel slide carousel"  data-interval="false">'.PHP_EOL;
 if (have_rows('hm_hero')) {
 	echo '<div class="blogs__list carousel-inner">'.PHP_EOL;
 	while (have_rows('hm_hero')) {
@@ -18,22 +17,27 @@ if (have_rows('hm_hero')) {
 			echo '</style>'.PHP_EOL;
 		}
 		echo '<div class="hero__item">'.PHP_EOL;
+		echo '<div class="wrapper">'.PHP_EOL;
+		echo '<div class="hero__content">'.PHP_EOL;
 		echo (!empty($hm_hero_title)?'<h2 class="hero__item--title">'.$hm_hero_title.'</h2>'.PHP_EOL:'');
 		echo (!empty($hm_hero_description)?'<span class="hero__item--desc">'.$hm_hero_description.'</span>'.PHP_EOL:'');
 		echo (!empty($hm_hero_link)?'<span class="hero__item--button"><a class="hero__item--link" href="'.$hm_hero_link['url'].'">'.$hm_hero_link['title'].'</a></span>'.PHP_EOL:'');
 		echo '</div>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
 	}
 	echo '</div>'.PHP_EOL;
 }
-// echo '<a class="blog__nav light carousel__nav carousel__nav--prev carousel-control-prev" href="#blog__carousel" role="button" data-slide="prev"><i class="blog__icon carousel__icon carousel__icon--prev"></i><span class="blog__nav--text sr-only">'.PHP_EOL;
+// echo '<a class="hero__nav light carousel__nav carousel__nav--prev carousel-control-prev" href="#hero__carousel" role="button" data-slide="prev"><i class="hero__icon carousel__icon carousel__icon--prev"></i><span class="hero__nav--text sr-only">'.PHP_EOL;
 // _e('Previous', 'fc_core');
 // echo '</span></a>'.PHP_EOL;
-// echo '<a class="blog__nav light carousel__nav carousel__nav--next carousel-control-next" href="#blog__carousel" role="button" data-slide="next"><i class="blog__icon carousel__icon carousel__icon--next"></i><span class="blog__nav--text sr-only">'.PHP_EOL;
+// echo '<a class="hero__nav light carousel__nav carousel__nav--next carousel-control-next" href="#hero__carousel" role="button" data-slide="next"><i class="hero__icon carousel__icon carousel__icon--next"></i><span class="hero__nav--text sr-only">'.PHP_EOL;
 // _e('Next', 'fc_core');
 // echo '</span></a>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 if (have_rows('hm_hero_solutions')) {
-	echo '<span class="hero__services">'.PHP_EOL;
+	echo '<div class="hero__services">'.PHP_EOL;
+	echo '<div class="wrapper">'.PHP_EOL;
 	echo '<ul class="hero__services--list">'.PHP_EOL;
 	while (have_rows('hm_hero_solutions')) {
 		the_row();
@@ -48,7 +52,8 @@ if (have_rows('hm_hero_solutions')) {
 		echo '</li>'.PHP_EOL;
 	}
 	echo '</ul>'.PHP_EOL;
-	echo '</span>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
+	echo '</div>'.PHP_EOL;
 }
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
