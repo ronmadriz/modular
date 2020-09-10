f3e249d0f5512a036b1003bf04d1cdbf12434eac
Ron Madriz <ron@firstcreative.com>
2020-09-10 11:44:49 -0500

    featured carousel - case studies

--- fls_2019/front-page.php
+++ fls_2019/front-page.php
@@ -2,7 +2,7 @@
 $pageID = get_the_id();
 $pageCF = get_post_custom($pageID);
 get_header();
-include (get_template_directory().'/views/components/banner/home.php');
+include (get_template_directory().'/views/components/carousel/home.php');
 include (get_template_directory().'/views/pages/home/industries.php');
 include (get_template_directory().'/views/pages/home/blog.php');
 include (get_template_directory().'/views/pages/home/testimonials.php');
