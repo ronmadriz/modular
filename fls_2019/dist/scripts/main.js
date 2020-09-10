ac12acd9d1a12aa7ca1a9a22eae21b3b1e61329e
Ron Madriz <ron@firstcreative.com>
2020-09-10 12:13:34 -0500

    featured carousel - case studies

--- fls_2019/dist/scripts/main.js
+++ fls_2019/dist/scripts/main.js
@@ -21,6 +21,10 @@ j(document).ready(function(){
       }
     }
   });
+  j('.crsl-items').carousel();
+  !function(t){t.fn.bcSwipe=function(e){var n={threshold:50};return e&&t.extend(n,e),this.each(function(){function e(t){1==t.touches.length&&(u=t.touches[0].pageX,c=!0,this.addEventListener("touchmove",o,!1))}function o(e){if(c){var o=e.touches[0].pageX,i=u-o;Math.abs(i)>=n.threshold&&(h(),t(this).carousel(i>0?"next":"prev"))}}function h(){this.removeEventListener("touchmove",o),u=null,c=!1}var u,c=!1;"ontouchstart"in document.documentElement&&this.addEventListener("touchstart",e,!1)}),this}}(jQuery);
+  j('.carousel').bcSwipe({ threshold: 50 });
+  j('#flsCarousel').carousel('pause');  
 });
 var j = jQuery.noConflict();
 j(document).ready(function(){
@@ -67,13 +71,9 @@ j(document).ready(function(){
 		  j(this).attr('target', '_blank');
 		};
 	});
-  j('.crsl-items').carousel();
 	j('#myModal').on('shown.bs.modal', function () {
 		j('#myInput').trigger('focus')
 	})	
-	!function(t){t.fn.bcSwipe=function(e){var n={threshold:50};return e&&t.extend(n,e),this.each(function(){function e(t){1==t.touches.length&&(u=t.touches[0].pageX,c=!0,this.addEventListener("touchmove",o,!1))}function o(e){if(c){var o=e.touches[0].pageX,i=u-o;Math.abs(i)>=n.threshold&&(h(),t(this).carousel(i>0?"next":"prev"))}}function h(){this.removeEventListener("touchmove",o),u=null,c=!1}var u,c=!1;"ontouchstart"in document.documentElement&&this.addEventListener("touchstart",e,!1)}),this}}(jQuery);
-	j('.carousel').bcSwipe({ threshold: 50 });
-  j('#flsCarousel').carousel('pause');
   j("a[target!='_blank'][href$='.pdf']").attr("target", "_blank");
 	j(window).scroll(function(){
 	    if (j(this).scrollTop() > 50) {
@@ -150,42 +150,6 @@ j(document).on('click', '[data-toggle="lightbox"]', function(event) {
 	event.preventDefault();
 	j(this).ekkoLightbox();
 });
-(function() {
-  var sliderWidth = j('.featured__list').width();
-  var Carousel = {
-    init: function() {
-      Carousel.bindEvents();    
-      j('.featured__list').css({marginLeft: -sliderWidth});
-      j('.featured__item:last-child').prependTo('.featured__list');
-    },
-    bindEvents: function() {
-      j('.featured__nav--next').on('click', Carousel.next);
-      j('.featured__nav--prev').on('click', Carousel.previous);
-      j(document).on('keydown', function(e) { // you need only one listener
-        var key = e.which; // Use Event.which instead of keyCode
-        if (key===37) Carousel.previous();
-        if (key===39) Carousel.next();
-      });
-    },
-    next: function() {
-      j('.featured__list').animate({
-        left: -sliderWidth
-      }, 350, function() {
-        j('.featured__item:first-child').appendTo('.featured__list');
-        j('.featured__list').css('left', 0);
-      });
-    },
-    previous: function() {
-      j('.featured__list').animate({
-        left: +sliderWidth
-      }, 350, function() {
-        j('.featured__item:last-child').prependTo('.featured__list');
-        j('.featured__list').css('left', 0);
-      });
-    }
-  }  
-  j( Carousel.init );
-})(window);
 j(document).ready(function(){
 	j('.menus__toggle').click(function(){
 		j(this).toggleClass('menus__toggle--open');
