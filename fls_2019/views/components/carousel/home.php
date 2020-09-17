33b230e4102ecc19c8e76d01f52432263bd8461a
Ron Madriz <ron@firstcreative.com>
2020-09-17 16:15:48 -0500

    homepage - link

--- fls_2019/views/components/carousel/home.php
+++ fls_2019/views/components/carousel/home.php
@@ -8,17 +8,16 @@ if (have_rows('hm_hero')) {
 		the_row();
 		$hm_hero_image       = get_sub_field('hm_hero_image');
 		$hm_hero_img         = esc_url($hm_hero_image['url']);
+		$hm_hero_title       = get_sub_field('hm_hero_title');
 		$hm_hero_description = get_sub_field('hm_hero_description');
-		$hm_hero_link_raw    = get_sub_field('hm_hero_link');
-		$hm_hero_title       = $hm_hero_link_raw['title'];
-		$hm_hero_link        = $hm_hero_link_raw['url'];
+		$hm_hero_link        = get_sub_field('hm_hero_link');
 		$hm_hero_solutions   = get_sub_field('hm_hero_solutions');
 		echo '<div class="hero__item carousel-item'.($hero_count == 0?' active':'').'"'.(empty($hm_hero_img)?'':' style="background-image:url('.$hm_hero_img.');"').'>'.PHP_EOL;
 		echo '<div class="wrapper">'.PHP_EOL;
 		echo '<div class="hero__content">'.PHP_EOL;
 		echo (!empty($hm_hero_title)?'<h2 class="hero__content--title">'.$hm_hero_title.'</h2>'.PHP_EOL:'');
 		echo (!empty($hm_hero_description)?'<span class="hero__content--desc">'.$hm_hero_description.'</span>'.PHP_EOL:'');
-		echo (!empty($hm_hero_link)?'<span class="hero__content--button"><a class="hero__content--link" href="'.$hm_hero_link.'">'.$hm_hero_link['title'].'</a></span>'.PHP_EOL:'');
+		echo (!empty($hm_hero_link)?'<span class="hero__content--button"><a class="hero__content--link" href="'.$hm_hero_link['url'].'">'.$hm_hero_link['title'].'</a></span>'.PHP_EOL:'');
 		echo '</div>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
 		echo '</div>'.PHP_EOL;
@@ -39,9 +38,10 @@ if (have_rows('hm_hero_solutions')) {
 	echo '<ul class="hero__services--list">'.PHP_EOL;
 	while (have_rows('hm_hero_solutions')) {
 		the_row();
-		$hm_hero_icon       = get_sub_field('hm_hero_icon');
-		$hm_hero_icon_title = get_sub_field('hm_hero_icon_title');
-		$hm_hero_icon_link  = get_sub_field('hm_hero_icon_link');
+		$hm_hero_icon          = get_sub_field('hm_hero_icon');
+		$hm_hero_icon_link_raw = get_sub_field('hm_hero_icon_link');
+		$hm_hero_icon_title    = $hm_hero_icon_link_raw['title'];
+		$hm_hero_icon_link     = $hm_hero_icon_link_raw['url'];
 		echo '<li class="hero__services--item">';
 		echo (!empty($hm_hero_icon_link)?'<a href="'.$hm_hero_icon_link.'" class="hero__services--link">':'');
 		echo (!empty($hm_hero_icon)?'<i class="hero__services--icon">'.file_get_contents(get_template_directory().'/sprites/'.$hm_hero_icon.'.svg').'</i>':'');
