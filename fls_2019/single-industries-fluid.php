7eecb31a41f73c6349aa95e9d60af462b6bdcd74
Ron Madriz <ron@firstcreative.com>
2020-09-28 11:42:28 -0500

    fluid layout - banner switch

--- fls_2019/single-industries-fluid.php
+++ fls_2019/single-industries-fluid.php
@@ -6,20 +6,7 @@ Template Post Type: industries
 $pageID = get_the_id();
 $pageCF = get_post_custom($pageID);
 get_header();
-
-$banner_img           = get_field('banner');
-$alternate_page_title = get_field('alternate_page_title');
-// BANNER
-echo '<section id="banner"'.(empty($banner_img)?' class="no_img"':'').'>'.PHP_EOL;
-echo '<div class="container-fluid">'.PHP_EOL;
-echo ($banner_img != null?'<div class="row w-image"><style type="text/css">section#banner{background-image:url('.$banner_img['url'].');}</style>':'<div class="row">').PHP_EOL;
-echo '<div class="page_title col-12">'.PHP_EOL;
-echo '<h1>'.(!empty($alternate_page_title)?$alternate_page_title:get_the_title()).'</h1>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</section>'.PHP_EOL;
-
+include (get_template_directory().'/views/components/banner/default.php');
 the_breadcrumb();
 
 // MAIN CONTENT
