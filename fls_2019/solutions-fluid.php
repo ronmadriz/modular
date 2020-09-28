3fb97bb328ec58197fc54319e37f89368d59cb94
Ron Madriz <ron@firstcreative.com>
2020-09-28 11:33:07 -0500

    fluid layout - banner switch

--- fls_2019/solutions-fluid.php
+++ fls_2019/solutions-fluid.php
@@ -7,19 +7,7 @@ global $post;
 $post_id = $post->ID;
 
 get_header();
-
-$banner_img           = get_field('banner');
-$alternate_page_title = get_field('alternate_page_title');
-// BANNER
-echo '<section id="banner"'.(empty($banner_img)?' class="no_img"':'').'>'.PHP_EOL;
-echo '<div class="container-fluid">'.PHP_EOL;
-echo ($banner_img != null?'<div class="row w-image"><style type="text/css">section#banner{background-image:url('.$banner_img['url'].');}</style>':'<div class="row">').PHP_EOL;
-
-echo '<div class="page_title col-12"><h1>'.(!empty($alternate_page_title)?$alternate_page_title:get_the_title()).'</h1></div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</section>'.PHP_EOL;
-
+include (get_template_directory().'/views/components/banner/default.php');
 the_breadcrumb();
 
 // MAIN CONTENT
