5f5aee99959fb774b03c36bfd61c22ffa31659ed
Ron Madriz <ron@firstcreative.com>
2020-09-28 11:49:24 -0500

    testimonials page - banner switch

--- fls_2019/page-testimonials.php
+++ fls_2019/page-testimonials.php
@@ -5,19 +5,7 @@ get_header();
 /**
  * Template Name: Testimonials
  */
-$banner_img = get_field('banner');
-// BANNER
-echo '<section id="banner"'.(empty($banner_img)?' class="no_img"':'').'>'.PHP_EOL;
-echo '<div class="container">'.PHP_EOL;
-echo ($banner_img != null?'<div class="row w-image"><style type="text/css">section#banner{background-image:url('.$banner_img['url'].');}</style>':'<div class="row">').PHP_EOL;
-
-echo '<div class="page_title col-12 col-md-7">'.PHP_EOL;
-echo '<h1>'.get_the_title().'</h1>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</section>'.PHP_EOL;
-
+include (get_template_directory().'/views/components/banner/default.php');
 echo '<div id="pagewrapper" class="container">'.PHP_EOL;
 echo '<div class="row">'.PHP_EOL;
 echo '<div id="columns_2" class="col-12 col-md-9">'.PHP_EOL;
