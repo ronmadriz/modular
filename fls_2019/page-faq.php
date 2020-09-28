df59db2794f12cd167ceb9a83c085555e9f2c73e
Ron Madriz <ron@firstcreative.com>
2020-09-28 11:53:03 -0500

    testimonials page - banner switch

--- fls_2019/page-faq.php
+++ fls_2019/page-faq.php
@@ -5,20 +5,7 @@ $pageCF = get_post_custom($pageID);
  * Template Name: FAQ
  */
 get_header();
-
-$banner_img = get_field('banner');
-// BANNER
-echo '<section id="banner"'.(empty($banner_img)?' class="no_img"':'').'>'.PHP_EOL;
-echo '<div class="container">'.PHP_EOL;
-echo ($banner_img != null?'<div class="row w-image"><style type="text/css">section#banner{background-image:url('.$banner_img['url'].');}</style>':'<div class="row">').PHP_EOL;
-echo '<div class="page_title col-12 col-md-7">'.PHP_EOL;
-$alternate_page_title = get_field('alternate_page_title');
-echo '<h1>'.(!empty($alternate_page_title)?$alternate_page_title:get_the_title()).'</h1>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</section>'.PHP_EOL;
-
+include (get_template_directory().'/views/components/banner/default.php');
 echo '<div id="pagewrapper" class="container">'.PHP_EOL;
 echo '<div class="row">'.PHP_EOL;
 echo '<div id="columns_2" class="col-12 col-md-9">'.PHP_EOL;
--- fls_2019/single-all-industries.php
+++ fls_2019/single-all-industries.php
@@ -6,16 +6,8 @@ Template Post Type: industries
 $pageID = get_the_id();
 $pageCF = get_post_custom($pageID);
 get_header();
-$banner_img = get_field('banner');
-// BANNER
-echo '<section id="banner"'.(empty($banner_img)?' class="no_img"':'').'>'.PHP_EOL;
-echo '<div class="container-fluid">'.PHP_EOL;
-echo ($banner_img != null?'<div class="row w-image justify-content-center align-content-center"><style type="text/css">section#banner{background-image:url('.$banner_img['url'].');}</style>':'<div class="row justify-content-center align-content-center">').PHP_EOL;
-$alternate_page_title = get_field('alternate_page_title');
-echo '<div class="page_title col-12"><h1>'.(!empty($alternate_page_title)?$alternate_page_title:get_the_title()).'</h1></div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</div>'.PHP_EOL;
-echo '</section>'.PHP_EOL;
+include (get_template_directory().'/views/components/banner/default.php');
+
 // INDUSTRY CONTENT
 if (have_posts()):while (have_posts()):the_post();
 echo '<section id="main-content">'.PHP_EOL;
