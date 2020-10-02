43d12c18853025d786a5850540e0b02497b1745c
Ron Madriz <ron@firstcreative.com>
2020-10-02 16:49:59 -0500

    image holder and image for banner

--- fls_2019/dist/scripts/main.js
+++ fls_2019/dist/scripts/main.js
@@ -1,6 +1,7 @@
 var j = jQuery.noConflict();
 j(document).ready(function(){
-  j('.multi__carousel').on('slide.bs.carousel', function (e) {
+
+/*  j('.multi__carousel').on('slide.bs.carousel', function (e) {
     // CC 2.0 License Iatek LLC 2018
     // Attribution required
     var je = j(e.relatedTarget);
@@ -20,12 +21,29 @@ j(document).ready(function(){
         }
       }
     }
-  });
+  }); */
+  j('.multi__carousel .item').each(function(){
+    var next = j(this).next();
+    if (!next.length) {
+      next = j(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    if (next.next().length>0) {
+      next.next().children(':first-child').clone().appendTo($(this));
+    } else {
+      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+  });  
   j('.crsl-items').carousel();
   // !function(t){t.fn.bcSwipe=function(e){var n={threshold:50};return e&&t.extend(n,e),this.each(function(){function e(t){1==t.touches.length&&(u=t.touches[0].pageX,c=!0,this.addEventListener("touchmove",o,!1))}function o(e){if(c){var o=e.touches[0].pageX,i=u-o;Math.abs(i)>=n.threshold&&(h(),t(this).carousel(i>0?"next":"prev"))}}function h(){this.removeEventListener("touchmove",o),u=null,c=!1}var u,c=!1;"ontouchstart"in document.documentElement&&this.addEventListener("touchstart",e,!1)}),this}}(jQuery);
   // j('.carousel').bcSwipe({ threshold: 50 });
   j('#flsCarousel').carousel('pause');  
 });
+
+
+
+
 var j = jQuery.noConflict();
 j(document).ready(function(){
    var ppp = 3; // Post per page
