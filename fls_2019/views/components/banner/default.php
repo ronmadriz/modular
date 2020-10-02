f58f849af878ff5851da788ada6f33105bf6ada3
Ron Madriz <ron@firstcreative.com>
2020-10-02 11:45:34 -0500

    fluid templates removal of padding on sections

--- fls_2019/views/components/banner/default.php
+++ fls_2019/views/components/banner/default.php
@@ -6,7 +6,7 @@ $blogpage__title      = get_the_title(get_option('page_for_posts', true));
 ;
 // BANNER
 echo '<section id="hero" class="hero">'.PHP_EOL;
-echo '<span class="hero__holder">'.($banner_img != null?'<img alt="'.$banner_img['alt'].'" class="hero__img img-fluid" src="'.$banner_img['url'].'">':'').'</span>'.PHP_EOL;
+echo '<span class="hero__holder">'.($banner_img != null?'<img alt="'.$banner_img['alt'].'" class="hero__img" src="'.$banner_img['url'].'">':'').'</span>'.PHP_EOL;
 echo '<div class="wrapper">'.PHP_EOL;
 if (is_home()) {
 	echo '<span class="hero__title"><h1 class="hero__title--text">'.$blogpage__title.'</h1></span>'.PHP_EOL;
