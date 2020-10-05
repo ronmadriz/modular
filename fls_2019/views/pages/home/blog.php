d1b64ce471065fa72691785ecaec035c5f823de9
Ron Madriz <ron@firstcreative.com>
2020-10-05 14:59:02 -0500

    blog posts on blog and case fix

--- fls_2019/views/pages/home/blog.php
+++ fls_2019/views/pages/home/blog.php
@@ -43,10 +43,10 @@ if (have_rows('hm_blog')) {
 		$hm_blog_count++;
 	}
 	echo '</div>'.PHP_EOL;
-	echo '<a class="blog__nav light carousel__nav carousel__nav--prev carousel-control-prev" href="#blog__carousel" role="button" data-slide="prev"><i class="blog__icon carousel__icon carousel__icon--prev"></i><span class="blog__nav--text sr-only">'.PHP_EOL;
+	echo '<a class="blogs__nav light carousel__nav carousel__nav--prev carousel-control-prev" href="#blog__carousel" role="button" data-slide="prev"><i class="blog__icon carousel__icon carousel__icon--prev"></i><span class="blog__nav--text sr-only">'.PHP_EOL;
 	_e('Previous', 'fc_core');
 	echo '</span></a>'.PHP_EOL;
-	echo '<a class="blog__nav light carousel__nav carousel__nav--next carousel-control-next" href="#blog__carousel" role="button" data-slide="next"><i class="blog__icon carousel__icon carousel__icon--next"></i><span class="blog__nav--text sr-only">'.PHP_EOL;
+	echo '<a class="blogs__nav light carousel__nav carousel__nav--next carousel-control-next" href="#blog__carousel" role="button" data-slide="next"><i class="blog__icon carousel__icon carousel__icon--next"></i><span class="blog__nav--text sr-only">'.PHP_EOL;
 	_e('Next', 'fc_core');
 	echo '</span></a>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
