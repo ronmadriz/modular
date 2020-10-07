6624c0d401fcc2e9ff2df166bed602db25115b32
Ron Madriz <ron@firstcreative.com>
2020-10-07 15:32:42 -0500

    featured overhaul

--- fls_2019/views/pages/home/blog.php
+++ fls_2019/views/pages/home/blog.php
@@ -3,8 +3,8 @@
 if (have_rows('hm_blog')) {
 	echo '<section id="home_blog" class="blogs">'.PHP_EOL;
 	echo '<div class="wrapper">'.PHP_EOL;
-	echo '<div id="blog__carousel" class="blog__carousel multi__carousel slide carousel" data-interval="false"  data-wrap="false">'.PHP_EOL;
-	echo '<div class="blogs__list carousel-inner">'.PHP_EOL;
+	echo '<div id="featured__carousel" class="featured__carousel">'.PHP_EOL;
+	echo '<div class="featured__slider">'.PHP_EOL;
 	$hm_blog_count = 0;
 	while (have_rows('hm_blog')) {
 		the_row();
@@ -20,33 +20,33 @@ if (have_rows('hm_blog')) {
 			$hm_blog_excerpt = get_the_excerpt();
 			$hm_blog_summary = (!empty($hm_blog_ex)?$hm_blog_ex:$hm_blog_excerpt);
 			$hm_blog_link    = get_the_permalink();
-			echo '<article class="blogs__item carousel-item'.($hm_blog_count == 0?' active':'').'">'.PHP_EOL;
-			echo '<header class="blogs__header">'.PHP_EOL;
-			echo '<a class="blogs__image--link" href="'.get_the_permalink().'"><img alt="'.$hm_blog_title.'" class="blogs__image" src="'.$hm_blog_img.'"></a>'.PHP_EOL;
-			echo '</header>'.PHP_EOL;
-			echo '<div class="blogs__content">'.PHP_EOL;
-			echo '<span class="blogs__sub">';
+			echo '<div class="featured__item">'.PHP_EOL;
+			echo '<div class="featured__item--wrapper">'.PHP_EOL;
+			echo '<header><a class="featured__image--link" href="'.get_the_permalink().'"><img alt="'.$hm_blog_title.'" class="featured__image" src="'.$hm_blog_img.'"></a></header>'.PHP_EOL;
+			echo '<div class="featured__content">'.PHP_EOL;
+			echo '<span class="featured__sub">';
 			the_category(', ');
 			echo '</span>'.PHP_EOL;
-			echo '<h3 class="blogs__title"><a class="blogs__title--link" href="'.$hm_blog_link.'">'.$hm_blog_title.'</a></h3>'.PHP_EOL;
-			echo '<span class="blogs__desc">'.$hm_blog_summary.'</span>'.PHP_EOL;
+			echo '<h3 class="featured__title"><a class="featured__title--link" href="'.$hm_blog_link.'">'.$hm_blog_title.'</a></h3>'.PHP_EOL;
+			echo '<span class="featured__desc">'.$hm_blog_summary.'</span>'.PHP_EOL;
 			echo '</div>'.PHP_EOL;
-			echo '<footer class="blogs__footer">'.PHP_EOL;
-			echo '<time class="blogs__date">'.$hm_blog_date.'</time>'.PHP_EOL;
-			echo '<span class="blogs__buttons"><a class="blogs__link" href="'.$hm_blog_link.'">';
+			echo '<footer class="featured__footer">'.PHP_EOL;
+			echo '<time class="featured__date">'.$hm_blog_date.'</time>'.PHP_EOL;
+			echo '<span class="featured__buttons"><a class="featured__link" href="'.$hm_blog_link.'">';
 			_e('Continue Reading', 'fc_core');
 			echo '</a></span>'.PHP_EOL;
 			echo '</footer>'.PHP_EOL;
-			echo '</article>'.PHP_EOL;
+			echo '</div>'.PHP_EOL;
+			echo '</div>'.PHP_EOL;
 			wp_reset_postdata();
 		}
 		$hm_blog_count++;
 	}
 	echo '</div>'.PHP_EOL;
-	echo '<a class="blogs__nav light carousel__nav carousel__nav--prev carousel-control-prev" href="#blog__carousel" role="button" data-slide="prev"><i class="blog__icon carousel__icon carousel__icon--prev"></i><span class="blog__nav--text sr-only">'.PHP_EOL;
+	echo '<a class="featured__nav light carousel__nav carousel__nav--prev carousel-control-prev" href="#featured__carousel" role="button" data-slide="prev"><i class="featured__icon carousel__icon carousel__icon--prev"></i><span class="featured__nav--text sr-only">'.PHP_EOL;
 	_e('Previous', 'fc_core');
 	echo '</span></a>'.PHP_EOL;
-	echo '<a class="blogs__nav light carousel__nav carousel__nav--next carousel-control-next" href="#blog__carousel" role="button" data-slide="next"><i class="blog__icon carousel__icon carousel__icon--next"></i><span class="blog__nav--text sr-only">'.PHP_EOL;
+	echo '<a class="featured__nav light carousel__nav carousel__nav--next carousel-control-next" href="#featured__carousel" role="button" data-slide="next"><i class="featured__icon carousel__icon carousel__icon--next"></i><span class="featured__nav--text sr-only">'.PHP_EOL;
 	_e('Next', 'fc_core');
 	echo '</span></a>'.PHP_EOL;
 	echo '</div>'.PHP_EOL;
