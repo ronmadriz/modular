32a1ec6ec5f705bb2a8b65d06da417d85946736d
Ron Madriz <ron@firstcreative.com>
2020-10-07 13:04:17 -0500

    featured overhaul

--- fls_2019/functions/scripts-styles.php
+++ fls_2019/functions/scripts-styles.php
@@ -10,6 +10,7 @@ function enqueue_my_scripts() {
 	wp_enqueue_script('lightbox', 'https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.js', array('jquery'), '', true, true);
 	wp_enqueue_script('lightgallery_js', 'https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.6.12/js/lightgallery.min.js', array('jquery'), '', true, true);
 	wp_enqueue_script('lightslide_js', 'https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/js/lightslider.min.js', array('jquery'), '', true, true);
+	wp_enqueue_script('slick_js', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array('jquery'), '', true, true);
 	wp_enqueue_script('update', get_stylesheet_directory_uri().'/dist/scripts/jquery.firstVisitPopup.min.js', array('jquery'), '', true, true);
 	wp_enqueue_script('site_script', get_stylesheet_directory_uri().'/dist/scripts/main.min.js', array('jquery'), false, false);
 }
@@ -22,6 +23,8 @@ function enqueue_my_styles() {
 	wp_enqueue_style('iconmonstr', 'https://cdn.iconmonstr.com/1.3.0/css/iconmonstr-iconic-font.min.css');
 	wp_enqueue_style('lightgallery_css', 'https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.6.12/css/lightgallery.min.css');
 	wp_enqueue_style('lightslide_css', 'https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/css/lightslider.min.css');
+	wp_enqueue_style('slick_css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css');
+	wp_enqueue_style('slickmin_css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css');
 	wp_enqueue_style('site-style', get_template_directory_uri().'/dist/css/style.min.css');
 	wp_enqueue_style('my-style', get_template_directory_uri().'/style.css');
 }
--- fls_2019/scripts/carousel.js
+++ fls_2019/scripts/carousel.js
@@ -39,7 +39,29 @@ j(document).ready(function(){
   // !function(t){t.fn.bcSwipe=function(e){var n={threshold:50};return e&&t.extend(n,e),this.each(function(){function e(t){1==t.touches.length&&(u=t.touches[0].pageX,c=!0,this.addEventListener("touchmove",o,!1))}function o(e){if(c){var o=e.touches[0].pageX,i=u-o;Math.abs(i)>=n.threshold&&(h(),t(this).carousel(i>0?"next":"prev"))}}function h(){this.removeEventListener("touchmove",o),u=null,c=!1}var u,c=!1;"ontouchstart"in document.documentElement&&this.addEventListener("touchstart",e,!1)}),this}}(jQuery);
   // j('.carousel').bcSwipe({ threshold: 50 });
   j('#flsCarousel').carousel('pause');  
+
+
+  j('.featured__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: $('.featured__nav--prev'),
+    nextArrow: $('.featured__nav--next'),
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });  
 });
-
-
-
