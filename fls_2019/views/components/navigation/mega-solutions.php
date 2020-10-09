ae875d1f25b87f06f8bca291accfcca607f45f08
Ron Madriz <ron@firstcreative.com>
2020-10-09 09:25:35 -0500

    featured overhaul

--- fls_2019/views/components/navigation/mega-solutions.php
+++ fls_2019/views/components/navigation/mega-solutions.php
@@ -1,80 +1,80 @@
 <div class="menus__solutions">
 	<div class="wrapper">
 		<ul class="menus__list">
-			<li class="menus__item menus__parent menus__item--parent""><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/rigid-rail/" class="menus__link">Rigid Rail</a>
-				<ul class="menus__sub menus__child">
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/rigid-rail/flexrail/" class="menus__sub--link">FlexRail</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/rigid-rail/flextraxx/" class="menus__sub--link">FlexTraxx</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/rigid-rail/flexbridge/" class="menus__sub--link">FlexBridge</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/rigid-rail/dual-overhead-rails/" class="menus__sub--link">Twin FlexRail</a></li>
+			<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/rigid-rail/" class="menus__link">Rigid Rail</a>
+				<ul>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/rigid-rail/flexrail/">FlexRail</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/rigid-rail/flextraxx/">FlexTraxx</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/rigid-rail/flexbridge/">FlexBridge</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/rigid-rail/dual-overhead-rails/">Twin FlexRail</a></li>
 				</ul>
 			</li>
-			<li class="menus__item menus__parent menus__item--parent""><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/lifeline-systems/" class="menus__link">Lifeline Systems</a>
-				<ul class="menus__sub menus__child">
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/lifeline-systems/overhead-lifeline-systems/" class="menus__sub--link">Overhead Lifeline Systems</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/lifeline-systems/horizontal-lifeline-systems/" class="menus__sub--link">Horizontal Lifeline Systems</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/lifeline-systems/vertical-lifeline-systems/" class="menus__sub--link">Vertical Lifeline Systems</a></li>
+			<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/lifeline-systems/" class="menus__link">Lifeline Systems</a>
+				<ul>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/lifeline-systems/overhead-lifeline-systems/">Overhead Lifeline Systems</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/lifeline-systems/horizontal-lifeline-systems/">Horizontal Lifeline Systems</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/lifeline-systems/vertical-lifeline-systems/">Vertical Lifeline Systems</a></li>
 				</ul>
 			</li>
-			<li class="menus__item menus__parent menus__item--parent""><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/guardrail/" class="menus__link">Guardrail</a>
-				<ul class="menus__sub menus__child">
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/flexguard/flexguard-permanent/" class="menus__sub--link">FlexGuard Permanent</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/flexguard/flexguard-portable/" class="menus__sub--link">FlexGuard Portable</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/flexguard/flexguard-parapet/" class="menus__sub--link">FlexGuard Parapet</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/flexguard/flexguard-topfix/" class="menus__sub--link">FlexGuard Topfix</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/warning-line/" class="menus__sub--link">FlexLine Warning System</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/guardrail/flexkit/" class="menus__sub--link">FlexKit</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/guardrail/flexguard-ground-based/" class="menus__sub--link">FlexGuard Ground Based</a></li>
+			<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/guardrail/" class="menus__link">Guardrail</a>
+				<ul>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/flexguard/flexguard-permanent/">FlexGuard Permanent</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/flexguard/flexguard-portable/">FlexGuard Portable</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/flexguard/flexguard-parapet/">FlexGuard Parapet</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/flexguard/flexguard-topfix/">FlexGuard Topfix</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/warning-line/">FlexLine Warning System</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/guardrail/flexkit/">FlexKit</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/guardrail/flexguard-ground-based/">FlexGuard Ground Based</a></li>
 				</ul>
 			</li>
-			<li class="menus__item menus__parent menus__item--parent""><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/skylight-hatch/" class="menus__link">Skylight/Hatch</a>
-				<ul class="menus__sub menus__child">
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/skylight-screens/" class="menus__sub--link">Skylight Screens</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/skylight-guardrail/" class="menus__sub--link">Skylight GuardRail</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/hatch-kits/" class="menus__sub--link">Hatch Kits</a></li>
+			<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/skylight-hatch/" class="menus__link">Skylight/Hatch</a>
+				<ul>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/skylight-screens/">Skylight Screens</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/skylight-guardrail/">Skylight GuardRail</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/hatch-kits/">Hatch Kits</a></li>
 				</ul>
 			</li>
-			<li class="menus__item menus__parent menus__item--parent""><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/access-solutions/" class="menus__link">Access Solutions</a>
-			<ul class="menus__sub menus__child">
-				<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/access-solutions/flexwalk-with-guardrail/" class="menus__sub--link">FlexWalk With Guardrail</a></li>
-				<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/flexwalk/" class="menus__sub--link">FlexWalk</a></li>
-				<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/industrial/catwalks/" class="menus__sub--link">Catwalks</a></li>
-				<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/access-solutions/rooftop-crossover-platforms/" class="menus__sub--link">Rooftop Crossover Platforms</a></li>
-				<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/access-solutions/permanent-access-solutions/" class="menus__sub--link">Permanent Access Platforms</a></li>
-				<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/access-solutions/self-closing-gates/" class="menus__sub--link">Self Closing Gates</a></li>
-				<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/access-solutions/work-platforms/" class="menus__sub--link">Work Platforms</a></li>
+			<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/access-solutions/" class="menus__link">Access Solutions</a>
+			<ul>
+				<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/access-solutions/flexwalk-with-guardrail/">FlexWalk With Guardrail</a></li>
+				<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/flexwalk/">FlexWalk</a></li>
+				<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/industrial/catwalks/">Catwalks</a></li>
+				<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/access-solutions/rooftop-crossover-platforms/">Rooftop Crossover Platforms</a></li>
+				<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/access-solutions/permanent-access-solutions/">Permanent Access Platforms</a></li>
+				<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/access-solutions/self-closing-gates/">Self Closing Gates</a></li>
+				<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/access-solutions/work-platforms/">Work Platforms</a></li>
 			</ul>
 		</li>
-			<li class="menus__item menus__parent menus__item--parent""><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/anchor-points/" class="menus__link">Anchor Points</a>
-				<ul class="menus__sub menus__child">
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/anchor-points/single-point-anchor/" class="menus__sub--link">Single Point Anchor</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/anchor-points/davit-arms-pedestals/" class="menus__sub--link">Davit Arms &#038; Pedestals</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/anchor-points/tieback-anchor/" class="menus__sub--link">Tieback Anchor</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/freestanding-anchor/" class="menus__sub--link">Freestanding Rooftop Anchor</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/anchor-points/tank-anchors/" class="menus__sub--link">Tank Anchors</a></li>
+			<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/anchor-points/" class="menus__link">Anchor Points</a>
+				<ul>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/anchor-points/single-point-anchor/">Single Point Anchor</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/anchor-points/davit-arms-pedestals/">Davit Arms &#038; Pedestals</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/anchor-points/tieback-anchor/">Tieback Anchor</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/freestanding-anchor/">Freestanding Rooftop Anchor</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/anchor-points/tank-anchors/">Tank Anchors</a></li>
 				</ul>
 			</li>
-			<li class="menus__item menus__parent menus__item--parent""><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/portable/" class="menus__link">Portable</a>
-				<ul class="menus__sub menus__child">
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/portable/horizontal-rail/" class="menus__sub--link">Horizontal Rail</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/portable/freestanding-ladder-stair-systems/" class="menus__sub--link">Freestanding Ladder & Stairs</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/portable/jibs/" class="menus__sub--link">Jibs</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/portable/counter-weighted-rail-systems/" class="menus__sub--link">Counter Weighted Rail Systems</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/portable/exosphere/" class="menus__sub--link">Exosphere</a></li>
+			<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/portable/" class="menus__link">Portable</a>
+				<ul>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/portable/horizontal-rail/">Horizontal Rail</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/portable/freestanding-ladder-stair-systems/">Freestanding Ladder & Stairs</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/portable/jibs/">Jibs</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/portable/counter-weighted-rail-systems/">Counter Weighted Rail Systems</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/portable/exosphere/">Exosphere</a></li>
 				</ul>
 			</li>
 			<li class=""><a href="https://sandbox.fall-arrest.com/fall-protection-solutions/custom/" class="menus__link">Custom</a></li>
-			<li class="menus__item menus__parent menus__item--parent""><span class="menus__link">Search By Industry</span>
-				<ul class="menus__sub menus__child">
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/aircraft/" class="menus__sub--link">Aircraft</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/" class="menus__sub--link">Rooftop</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/railcar/" class="menus__sub--link">Railcar</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/utilities-energy/" class="menus__sub--link">Energy</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/manufacturing/" class="menus__sub--link">Manufacturing</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/industrial/" class="menus__sub--link">Industrial</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/oil-gas-chemical/" class="menus__sub--link">Oil &#038; Gas/Chemical</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/trucks-trailers/" class="menus__sub--link">Trucks &#038; Trailers</a></li>
-					<li class="menus__sub--item"><a href="https://sandbox.fall-arrest.com/fall-protection-industries/all/" class="menus__sub--link">[All]</a></li>
+			<li><span class="menus__link">Search By Industry</span>
+				<ul>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/aircraft/">Aircraft</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/rooftop/">Rooftop</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/railcar/">Railcar</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/utilities-energy/">Energy</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/manufacturing/">Manufacturing</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/industrial/">Industrial</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/oil-gas-chemical/">Oil &#038; Gas/Chemical</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/trucks-trailers/">Trucks &#038; Trailers</a></li>
+					<li><a href="https://sandbox.fall-arrest.com/fall-protection-industries/all/">[All]</a></li>
 				</ul>
 			</li>
 		</ul>
