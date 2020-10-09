17d239424f0ea070fe48df7c2b94703f13ed312e
Ron Madriz <ron@firstcreative.com>
2020-10-09 13:57:25 -0500

    solutions list overhaul

--- fls_2019/dist/scripts/main.js
+++ fls_2019/dist/scripts/main.js
@@ -227,13 +227,17 @@ function normalizeSlideHeights() {
     }) 
 }
 j(document).ready(function(){
-    j('#menus__item--1').hover(
-        function(){
-            j('.menus__solutions').toggleClass('show')
-        }, 
-    );
+    // j('#menus__item--1').hover(function(){j('.menus__solutions').toggleClass('show') }, );
+    j('#menus__item--1').mouseover(function() {
+        clearTimeout(this.timer)
+        j(this).children('.menus__solutions').addClass('show');
+    })
+    .mouseleave(function() {
+        this.timer = setTimeout(function() {
+          j(this).children('.menus__solutions').removeClass('show');
+        }.bind(this), 500);
+    });    
 });
-
 // Solutions Filter
 var $btns = j('.btn_filter').click(function() {
   if (this.id == 'all') {
