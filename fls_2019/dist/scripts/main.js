7454801f4dce3b4ca1f2d0c832ef8754090d811d
Ron Madriz <ron@firstcreative.com>
2020-10-09 14:04:18 -0500

    solutions list overhaul

--- fls_2019/dist/scripts/main.js
+++ fls_2019/dist/scripts/main.js
@@ -231,8 +231,8 @@ j(document).ready(function(){
     j('#menus__item--1').mouseover(function() {
         clearTimeout(this.timer)
         j('.menus__solutions').addClass('show');
-    })
-    .mouseleave(function() {
+    });
+    j('.menu__parent').not('#menus__item--1').mouseover(function() {
         this.timer = setTimeout(function() {
           j('.menus__solutions').removeClass('show');
         }.bind(this), 500);
--- fls_2019/scripts/solutions.js
+++ fls_2019/scripts/solutions.js
@@ -3,8 +3,8 @@ j(document).ready(function(){
     j('#menus__item--1').mouseover(function() {
         clearTimeout(this.timer)
         j('.menus__solutions').addClass('show');
-    })
-    .mouseleave(function() {
+    });
+    j('.menu__parent').not('#menus__item--1').mouseover(function() {
         this.timer = setTimeout(function() {
           j('.menus__solutions').removeClass('show');
         }.bind(this), 500);
