235418e9fab3a27ef05d58302c3806fccf31554c
Ron Madriz <ron@firstcreative.com>
2020-10-09 13:29:14 -0500

    solutions list overhaul

--- fls_2019/scripts/menus.js
+++ fls_2019/scripts/menus.js
@@ -11,5 +11,7 @@ j(document).ready(function(){
 			j(this).siblings('.menus__child').toggleClass('menus__open');
 		} );	
 	}	
-	// j('#menus__item--1').hover(function(){ j('.menus__solutions').addClass('show') },);
+	j('#menus__item--1').hover(
+	       function(){ j('.menus__solutions').toggleClass('show') },
+	);
 });
