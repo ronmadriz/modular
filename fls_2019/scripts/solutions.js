0169b6ee4eeac0f6f88d3be30c9ff6624b21e125
Ron Madriz <ron@firstcreative.com>
2020-10-09 13:45:58 -0500

    solutions list overhaul

--- fls_2019/scripts/solutions.js
+++ fls_2019/scripts/solutions.js
@@ -1,7 +1,19 @@
 j(document).ready(function(){
-    j('#menus__item--1').hover(
-        function(){
-            j('.menus__solutions').toggleClass('show')
-        }, 
-    );
+    // j('#menus__item--1').hover(function(){j('.menus__solutions').toggleClass('show') }, ); 
+    j('#menus__item--1').on("mouseover", function() {
+        clearTimeout(timer);
+        openSubmenu();
+    }).on("mouseleave", function() {
+        timer = setTimeout(
+        closeSubmenu
+        , 1000);
+    });
+
+    function openSubmenu() {
+      j(".menus__solutions").addClass("open");
+    }
+    function closeSubmenu() {
+      j(".menus__solutions").removeClass("open");
+    }
+
 });
