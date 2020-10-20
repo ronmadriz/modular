ab527a0999ab8b8612ab17db8e12859f38bbcac3
Jon Butler <jon@thejonbutler.com>
2020-10-19 21:42:52 -0400

    test deploy

--- fls_2019/scripts/solutions.js
+++ fls_2019/scripts/solutions.js
@@ -1,9 +1,9 @@
 j(document).ready(function(){
     // j('#menus__item--1').hover(function(){j('.menus__solutions').toggleClass('show') }, );
-    j('#menus__item--1').mouseover(function() {
-        j('.menus__solutions').addClass('show');
-    });
-    j('.menu__parent').mouseleave(function() {
+    // j('#menus__item--1').mouseover(function() {
+    //     j('.menus__solutions').addClass('show');
+    // });
+    j('.menu__parent').not('#menus__item--1').mouseover(function() {
         j('.menus__solutions').removeClass('show');
     });
 });
