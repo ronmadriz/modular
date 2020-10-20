5c78d63275393ed8079916849293903cc79b9aa1
Jon Butler <jon@thejonbutler.com>
2020-10-19 23:28:19 -0400

    test logging to console

--- fls_2019/scripts/solutions.js
+++ fls_2019/scripts/solutions.js
@@ -1,5 +1,6 @@
 j(document).ready(function(){
     // j('#menus__item--1').hover(function(){j('.menus__solutions').toggleClass('show') }, );
+    console.log("test");
     // j('#menus__item--1').mouseover(function() {
     //     j('.menus__solutions').addClass('show');
     // });
