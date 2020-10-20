fa9c93150b9a8ba2aefb0fd6df421c44b9b0476d
Jon Butler <jon@thejonbutler.com>
2020-10-19 23:30:17 -0400

    test mouse leave

--- fls_2019/scripts/solutions.js
+++ fls_2019/scripts/solutions.js
@@ -1,10 +1,10 @@
 j(document).ready(function(){
     // j('#menus__item--1').hover(function(){j('.menus__solutions').toggleClass('show') }, );
     console.log("test");
-    // j('#menus__item--1').mouseover(function() {
-    //     j('.menus__solutions').addClass('show');
-    // });
-    j('.menu__parent').not('#menus__item--1').mouseover(function() {
+    j('#menus__item--1').mouseover(function() {
+       j('.menus__solutions').addClass('show');
+    });
+    j('.menu__parent').mouseleave(function() {
         j('.menus__solutions').removeClass('show');
     });
 });
