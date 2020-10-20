d63c34912dbb483039cd2a6e4aba29e51d6785c6
Jon Butler <jon@thejonbutler.com>
2020-10-19 23:55:12 -0400

    hover

--- fls_2019/scripts/solutions.js
+++ fls_2019/scripts/solutions.js
@@ -4,7 +4,7 @@ j(document).ready(function(){
     j('#menus__item--1').mouseover(function() {
        j('.menus__solutions').addClass('show');
     });
-    j('body').not('#menu-mega_solution, #menus__item--1').mouseover(function() {
+    j('body').not('#menu-mega_solution, #menus__item--1').hover(function() {
         j('.menus__solutions').removeClass('show');
     });
 });
