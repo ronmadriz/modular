d7e2529dbdab3919a7abbcb07b13a84aaf85df57
Jon Butler <jon@thejonbutler.com>
2020-10-20 00:54:00 -0400

    code cleanup

--- fls_2019/scripts/solutions.js
+++ fls_2019/scripts/solutions.js
@@ -1,5 +1,5 @@
 j(document).ready(function(){
-    //Show dropdown on mouseenter
+    //Show dropdown on mouse
     j('#menus__item--1').mouseenter(function() {
        j('.menus__solutions').addClass('show');
     });
@@ -9,7 +9,7 @@ j(document).ready(function(){
     });
     // Also remove dropdown when mouse leaves navbar, but not when it goes into dropdown
     j('#menus__item--1').mouseleave(function() {
-        if(!j('.menus__solutions:hover')){
+        if(!j('.menus__solutions:hover').length != 0){
             j('.menus__solutions').removeClass('show');
         }
     });
