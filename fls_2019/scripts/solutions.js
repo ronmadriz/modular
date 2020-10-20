c74954f24947145211a783d4a1618706c617a20d
Jon Butler <jon@thejonbutler.com>
2020-10-20 00:38:59 -0400

    test remove

--- fls_2019/scripts/solutions.js
+++ fls_2019/scripts/solutions.js
@@ -4,13 +4,18 @@ j(document).ready(function(){
        j('.menus__solutions').addClass('show');
     });
     //Remove dropdown when mouse leaves dropdown box
-    j('.menus__solutions').mouseleave(function() {
-        j('.menus__solutions').removeClass('show');
-    });
+    // j('.menus__solutions').mouseleave(function() {
+    //     j('.menus__solutions').removeClass('show');
+    // });
     // Also remove dropdown when mouse leaves navbar
+    // j('#menus__item--1').mouseleave(function() {
+    //     if(!j('.menus__solutions:hover').length != 0){
+    //         j('.menus__solutions').removeClass('show');
+    //     }
+    // });
+
+
     j('#menus__item--1').mouseleave(function() {
-        if(!j('.menus__solutions:hover').length != 0){
             j('.menus__solutions').removeClass('show');
-        }
     });
 });
