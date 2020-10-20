fa6289ef2d625dded8ac99364cc0ce6939fd1ded
Jon Butler <jon@thejonbutler.com>
2020-10-19 23:35:19 -0400

    add show

--- fls_2019/scripts/solutions.js
+++ fls_2019/scripts/solutions.js
@@ -4,7 +4,7 @@ j(document).ready(function(){
     j('#menus__item--1').mouseover(function() {
        j('.menus__solutions').addClass('show');
     });
-    j('#menus__item--1').mouseleave(function() {
+    j('#menus__item--1', '.show').mouseleave(function() {
         j('.menus__solutions').removeClass('show');
     });
 });
