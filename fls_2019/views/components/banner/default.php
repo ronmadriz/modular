e2e2bf50ead9e81eca539722299251233e4f2d19
Charlie Barzilla <charlie.barzilla@flexiblelifeline.com>
2020-10-27 15:24:56 +0100

    SR testing

--- fls_2019/views/components/banner/default.php
+++ fls_2019/views/components/banner/default.php
@@ -17,6 +17,5 @@ if (is_home()) {
 }
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
-echo '<p>TESTING</p>'.PHP_EOL;
-echo do_shortcode("[rev_slider alias="basic-hero-collection1"]");
+echo do_shortcode("[rev_slider alias="basic-hero-collection1"]").PHP_EOL;
 echo '</section>'.PHP_EOL;
