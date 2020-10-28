c07377b0cb58b492097262808c3d9b9836d44d18
Ron Madriz <ron@firstcreative.com>
2020-10-28 16:15:09 -0600

    carousel fix

--- fls_2019/dist/scripts/main.js
+++ fls_2019/dist/scripts/main.js
@@ -1,3 +1,31 @@
+var j = jQuery.noConflict();
+j(document).ready(function(){
+
+
+  j('.featured__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: j('.carousel__nav--prev'),
+    nextArrow: j('.carousel__nav--next'),
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });  
+});
 
 var j = jQuery.noConflict();
 j(document).ready(function(){
