4063c2615527708237767849a84f3ecf8f1044e5
Jon Butler <jon@thejonbutler.com>
2020-10-28 19:27:15 -0400

    proposed solution

--- fls_2019/scripts/solutions.js
+++ fls_2019/scripts/solutions.js
@@ -1,16 +1,46 @@
 j(document).ready(function(){
-    //Show dropdown on mouse
+    //Show dropdown on mouse enter solutions tab
     j('#menus__item--1').mouseenter(function() {
        j('.menus__solutions').addClass('show');
     });
+
     //Remove dropdown when mouse leaves dropdown box
     j('.menus__solutions').mouseleave(function() {
         j('.menus__solutions').removeClass('show');
     });
-    // Also remove dropdown when mouse leaves navbar, but not when it goes into dropdown
-    j('#menus__item--1').mouseleave(function() {
+
+    // Also remove dropdown when mouse leaves solutions tab, but not when it goes into dropdown
+    j('#menus__item--1').mouseleave(function(e) {
         if(j('.menus__solutions:hover').length == 0){
-            j('.menus__solutions').removeClass('show');
+            var solutionsTab = j('#menus__item--1').getBoundingClientRect();
+            var mouseX = e.pageX;
+            var mouseY = e.pageY;
+            //If mouse is in gap (FireFox only) don't remove
+            if (!inGapArea(mouseX,mouseY,solutionsTab)){
+                j('.menus__solutions').removeClass('show');
+            }
         }
     });
 });
+
+
+function inGapArea(mouseX, mouseY, solutionsTab){
+    if (mouseX < solutionsTab.left){
+        // Left
+        return false;
+    };
+    if (mouseX > solutionsTab.right){
+        // Right
+        return false;
+    };
+    if (solutionsTab.bottom +9 < mouseY){
+        // Below assuming a max 9 px gap
+        return false;
+    };
+    if (solutionsTab.bottom > mouseY){
+        // Above
+        return false;
+    };
+
+    return true;
+}
