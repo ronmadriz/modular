adb7aac79f9ce4900c4cbd34844cfb0a95be2386
Charlie Barzilla <charlie.barzilla@flexiblelifeline.com>
2020-10-29 14:57:01 +0100

    Formatting

--- fls_2019/views/components/carousel/home.php
+++ fls_2019/views/components/carousel/home.php
@@ -1,4 +1,4 @@
 <?
 echo '<section id="hero" class="hero">'.PHP_EOL;
-echo do_shortcode("[rev_slider alias="basic-hero-collection1"]");
+echo do_shortcode("[rev_slider alias='basic-hero-collection1']");
 echo '</section>'.PHP_EOL;
