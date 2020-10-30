afc7e19705c1f39156a590909784962abb9eea00
Jon Butler <jon@thejonbutler.com>
2020-10-30 19:08:49 -0400

    revert all changes

--- fls_2019/dist/scripts/myloadmore.js
+++ fls_2019/dist/scripts/myloadmore.js
@@ -1,1 +1,29 @@
+jQuery(function(j){
+	j('.misha_loadmore').click(function(){
+ 		var button = j(this),
+		    data = {
+			'action': 'loadmore',
+			'query': misha_loadmore_params.posts,
+			'page' : misha_loadmore_params.current_page
+		};
+ 		j.ajax({
+			url : misha_loadmore_params.ajaxurl,
+			data : data,
+			type : 'POST',
+			beforeSend : function ( xhr ) {
+				button.text('Loading...');
+			},
+			success : function( data ){
+				if( data ) {
+					button.text( 'More posts' ).prev().before(data);
+					misha_loadmore_params.current_page++;
 
+					if ( misha_loadmore_params.current_page == misha_loadmore_params.max_page )
+						button.remove();
+ 				} else {
+					button.remove();
+				}
+			}
+		});
+	});
+});
