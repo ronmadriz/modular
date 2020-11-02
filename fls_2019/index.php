6690f09563608b43324d7b4d94a5598db6d1c44f
Ron Madriz <ron@firstcreative.com>
2020-11-02 09:31:04 -0700

    hero logic

--- fls_2019/index.php
+++ fls_2019/index.php
@@ -8,38 +8,38 @@ $alternate_page_title = get_field('alternate_page_title', 'fls-blog');
 echo '<section id="banner"'.(empty($banner_img)?' class="no_img"':'').'>'.PHP_EOL;
 echo '<div class="container">'.PHP_EOL;
 echo ($banner_img != null?'<div class="row w-image"><style type="text/css">section#banner{background-image:url('.$banner_img['url'].');}</style>':'<div class="row">').PHP_EOL;
-
 echo '<div class="page_title col-12 col-md-7">'.PHP_EOL;
 echo '<h1>'.$alternate_page_title.'</h1>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
 echo '</section>'.PHP_EOL;
-
 echo '<div id="pagewrapper" class="container">'.PHP_EOL;
 echo '<div class="row">'.PHP_EOL;
 echo '<div id="columns_2" class="col-12 col-md-9">'.PHP_EOL;
-
 // MAIN CONTENT
 if (have_posts()) {
 	echo '<section id="main-content">'.PHP_EOL;
 	echo '<div class="container">'.PHP_EOL;
 	echo '<div class="row">'.PHP_EOL;
-	while (have_posts()):the_post();
-	$post_th = get_the_post_thumbnail('full', array('class' => 'img-fluid'));
-	echo '<div class="col-12"><h3><a href="'.get_the_permalink().'">'.get_the_title().'</a></h3></div>'.PHP_EOL;
-	if (!empty($post_th)):
-	echo '<div class="col-12 col-md-3 post_th">';
-	echo '</div>'.PHP_EOL;
-	echo '<div class="col-12 col-md-9 post_desc">'.PHP_EOL;
-	 else :
-	echo '<div class="col-12 post_desc">'.PHP_EOL;
-	endif;
-	echo '<p>Posted on '.get_the_date().'</p>'.PHP_EOL;
-	echo '<div class="content">'.get_the_excerpt().'</div>'.PHP_EOL;
-	echo '<a href="'.get_the_permalink().'" class="btn btn-dark">Read More</a>'.PHP_EOL;
-	echo '</div>'.PHP_EOL;
-	endwhile;
+	while (have_posts()) {
+		the_post();
+		$post_th = get_the_post_thumbnail('full', array('class' => 'img-fluid'));
+		echo '<div class="col-12"><h3><a href="'.get_the_permalink().'">'.get_the_title().'</a></h3></div>'.PHP_EOL;
+		if (!empty($post_th)) {
+			echo '<div class="col-12 col-md-3 post_th">';
+			echo '</div>'.PHP_EOL;
+			echo '<div class="col-12 col-md-9 post_desc">'.PHP_EOL;
+		} else {
+			echo '<div class="col-12 post_desc">'.PHP_EOL;
+		}
+		echo '<p>Posted on '.get_the_date().'</p>'.PHP_EOL;
+		echo '<div class="content">'.get_the_excerpt().'</div>'.PHP_EOL;
+		echo '<a href="'.get_the_permalink().'" class="btn btn-dark">';
+		_e('Read More', 'fc_core');
+		echo '</a>'.PHP_EOL;
+		echo '</div>'.PHP_EOL;
+	}
 	echo '</div>'.PHP_EOL;
 	echo '<div class="row">';
 	echo '<div class="col-12 text-center">';
@@ -52,10 +52,8 @@ if (have_posts()) {
 	echo '</section>'.PHP_EOL;
 }
 // End Industry Main Column
-
 // Sidebar
 echo '</div>'.PHP_EOL;
-
 echo '<aside class="sidebar d-none d-sm-block col-md-3">'.PHP_EOL;
 $secondary_logo = get_field('secondary_logo');
 echo (!empty($secondary_logo)?'<div id="sidebar_logo" class="d-block text-center"><img src="'.$secondary_logo['url'].'" alt="'.$secondary_logo['alt'].'"></div>':'<img src="'.get_bloginfo('url').'/wp-content/uploads/2019/02/fls-works.svg" alt="Fall Safety Protection - Works">').PHP_EOL;
@@ -69,11 +67,13 @@ $fls_blog_query = new WP_Query($fls_blog_args);
 if ($fls_blog_query) {
 	echo '<nav>'.PHP_EOL;
 	echo '<ul>'.PHP_EOL;
-	echo '<li> Recent Blog Posts'.PHP_EOL;
+	echo '<li>';
+	_e('Recent Blog Posts', 'fc_core');
 	echo '<ul>'.PHP_EOL;
-	while ($fls_blog_query->have_posts()):$fls_blog_query->the_post();
-	echo '<li><a href="'.get_the_permalink().'">'.get_the_title().'</a></li>'.PHP_EOL;
-	endwhile;
+	while ($fls_blog_query->have_posts()) {
+		$fls_blog_query->the_post();
+		echo '<li><a href="'.get_the_permalink().'">'.get_the_title().'</a></li>'.PHP_EOL;
+	}
 	wp_reset_query();
 	echo '</ul></li>'.PHP_EOL;
 	echo '</ul>'.PHP_EOL;
@@ -81,8 +81,6 @@ if ($fls_blog_query) {
 }
 get_sidebar();
 echo '</aside>'.PHP_EOL;
-
 echo '</div>'.PHP_EOL;
 echo '</div>'.PHP_EOL;
-
 get_footer();?>
--- fls_2019/views/components/banner/default.php
+++ fls_2019/views/components/banner/default.php
@@ -5,7 +5,7 @@ $default_Pagetitle    = get_the_title();
 $blogpage__title      = get_the_title(get_option('page_for_posts', true));
 ;
 // BANNER
-echo '<section id="hero" class="hero">'.PHP_EOL;
+echo '<section id="hero" class="hero'.($banner_img != null?'':' no_img').'">'.PHP_EOL;
 echo '<span class="hero__holder">'.($banner_img != null?'<img alt="'.$banner_img['alt'].'" class="hero__img" src="'.$banner_img['url'].'">':'').'</span>'.PHP_EOL;
 echo '<div class="wrapper">'.PHP_EOL;
 if (is_home()) {
